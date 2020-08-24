5cee2329f1c3b6aca46a0fcd0941e86722503fb8
lyxyz <cy-l@live.com>
2020-08-24 13:47:15 +0800

    add the build script

---
+++ build.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# Copyright 2019-2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+set -e
+BASEPATH=$(cd "$(dirname $0)"; pwd)
+export BUILD_PATH="${BASEPATH}/build/"
+OUTPUT_PATH="${BUILD_PATH}/output"
+CANN_PATH="${BUILD_PATH}/cann"
+
+# print usage message
+usage() {
+  echo "Usage:"
+  echo "sh build.sh [-j[n]] [-h] [-v] [-s] [-t] [-u] [-c]"
+  echo ""
+  echo "Options:"
+  echo "    -h Print usage"
+  echo "    -u Only compile ut, not execute"
+  echo "    -s Build st"
+  echo "    -j[n] Set the number of threads used to build CANN, default is 8"
+  echo "    -t Build and execute ut"
+  echo "    -c Build ut with coverage tag"
+  echo "    -v Verbose"
+  echo "to be continued ..."
+}
+
+logging() {
+  echo "[INFO] $@"
+}
+
+# parse and set optionss
+checkopts() {
+  VERBOSE=""
+  THREAD_NUM=8
+  # ENABLE_CANN_UT_ONLY_COMPILE="off"
+  ENABLE_CANN_UT="off"
+  ENABLE_CANN_ST="off"
+  ENABLE_CANN_COV="off"
+  CANN_ONLY="on"
+  # Process the options
+  while getopts 'ustchj:v' opt
+  do
+    OPTARG=$(echo ${OPTARG} | tr '[A-Z]' '[a-z]')
+    case "${opt}" in
+      u)
+        # ENABLE_CANN_UT_ONLY_COMPILE="on"
+        ENABLE_CANN_UT="on"
+        CANN_ONLY="off"
+        ;;
+      s)
+        ENABLE_CANN_ST="on"
+        ;;
+      t)
+	      ENABLE_CANN_UT="on"
+	      CANN_ONLY="off"
+	      ;;
+      c)
+        ENABLE_CANN_COV="on"
+        CANN_ONLY="off"
+        ;;
+      h)
+        usage
+        exit 0
+        ;;
+      j)
+        THREAD_NUM=$OPTARG
+        ;;
+      v)
+        VERBOSE="VERBOSE=1"
+        ;;
+      *)
+        logging "Undefined option: ${opt}"
+        usage
+        exit 1
+    esac
+  done
+}
+
+# mkdir directory
+mk_dir() {
+  local create_dir="$1"
+  mkdir -pv "${create_dir}"
+  logging "Created ${create_dir}"
+}
+
+# create build path
+build_cann() {
+  logging "Create build directory and build CANN"
+  mk_dir "${CANN_PATH}"
+  # CMAKE_ARGS="-DBUILD_PATH=$BUILD_PATH -DCANN_ONLY=$CANN_ONLY"
+  CMAKE_ARGS=""
+
+  if [[ "X$ENABLE_CANN_COV" = "Xon" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DENABLE_CANN_COV=ON"
+  fi
+
+  if [[ "X$ENABLE_CANN_UT" = "Xon" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DENABLE_CANN_UT=ON"
+  fi
+  if [[ "X$ENABLE_CANN_ST" = "Xon" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DENABLE_CANN_ST=ON"
+  fi
+
+  logging "CMake Args: ${CMAKE_ARGS}"
+  cd "${BUILD_PATH}/cann" && cmake ${CMAKE_ARGS} ../..
+  make ${VERBOSE} -j${THREAD_NUM}
+  logging "CANN build success!"
+}
+
+release_cann() {
+  logging "Create output directory"
+  mk_dir "${OUTPUT_PATH}"
+}
+
+main() {
+  checkopts "$@"
+  # CANN build start
+  logging "---------------- CANN build start ----------------"
+  g++ -v
+  build_cann
+  release_cann
+  logging "---------------- CANN build finished ----------------"
+}
+
+main "$@"
