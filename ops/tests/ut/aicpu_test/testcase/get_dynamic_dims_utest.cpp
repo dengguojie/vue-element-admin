840f29605510fd47a1568dfb2bfb3326ee1bb3d4
yuzhiheng <zhiheng.yu@pactera.com>
2020-11-25 11:42:45 +0800

    Public Functions ut 5

--- ops/tests/ut/aicpu_test/testcase/get_dynamic_dims_utest.cpp
+++ ops/tests/ut/aicpu_test/testcase/get_dynamic_dims_utest.cpp
@@ -17,26 +17,59 @@ class GET_DYNAMIC_DIMS_KERNEL_UT : public testing::Test {};
 
 TEST_F(GET_DYNAMIC_DIMS_KERNEL_UT, INT32_Success)
 {
-  auto node_def = CpuKernelUtils::CreateNodeDef();
+    auto nodeDef = CpuKernelUtils::CreateNodeDef();
+    nodeDef->SetOpType("GetDynamicDims");
 
-  std::vector<int32_t> x1{ 3, 2, 4, 1 };
-  std::vector<int32_t> x2{ 1, 2, 1 };
-  std::vector<int32_t> x3{ 16, 112, 112, 3, 4 };
-  std::vector<int64_t> dims(3);
-  constexpr int64_t n_attr = 3;
-  std::vector<int64_t> shape_info_attr(
-      { 4, 3, 2, -1, 1, 3, 1, 2, 1, 5, 16, -1, -1, 3, 4 });
+    // set input1
+    auto x1Tensor = nodeDef->AddInputs();
+    EXPECT_NE(x1Tensor, nullptr);
+    std::vector<int32_t> x1{ 3, 2, 4, 1 };
+    auto x1Shape = x1Tensor->GetTensorShape();
+    x1Shape->SetDimSizes({4});
+    x1Tensor->SetDataType(DT_INT32);
+    x1Tensor->SetData(x1.data());
 
-  NodeDefBuilder(node_def.get(), "GetDynamicDims", "GetDynamicDims")
-    .Input({"x1", DT_INT32, {4}, x1.data()})
-    .Input({"x2", DT_INT32, {3}, x2.data()})
-    .Input({"x3", DT_INT32, {5}, x3.data()})
-    .Output({"dims", DT_INT64, {3}, dims.data()})
-    .Attr("N", n_attr)
-    .Attr("shape_info", shape_info_attr);
+    // set input2
+    auto x2Tensor = nodeDef->AddInputs();
+    EXPECT_NE(x2Tensor, nullptr);
+    std::vector<int32_t> x2{ 1, 2, 1 };
+    auto x2Shape = x2Tensor->GetTensorShape();
+    x2Shape->SetDimSizes({3});
+    x2Tensor->SetDataType(DT_INT32);
+    x2Tensor->SetData(x2.data());
+
+    // set input3
+    auto x3Tensor = nodeDef->AddInputs();
+    EXPECT_NE(x3Tensor, nullptr);
+    std::vector<int32_t> x3{ 16, 112, 112, 3, 4 };
+    auto x3Shape = x3Tensor->GetTensorShape();
+    x3Shape->SetDimSizes({5});
+    x3Tensor->SetDataType(DT_INT32);
+    x3Tensor->SetData(x3.data());
+
+    // set attrs
+    int64_t N = 3;
+    auto NAttr = CpuKernelUtils::CreateAttrValue();
+    NAttr->SetInt(N);
+    nodeDef->AddAttrs("N", NAttr.get());
+
+    vector<int64_t> shape_info{ 4, 3, 2, -1, 1, 3, 1, 2, 1, 5, 16, -1, -1, 3, 4 };
+    auto shapeAttrs = CpuKernelUtils::CreateAttrValue();
+    shapeAttrs->SetListInt(shape_info);
+    nodeDef->AddAttrs("shape_info", shapeAttrs.get());
+
+    // set output
+    auto dimsTensor = nodeDef->AddOutputs();
+    EXPECT_NE(dimsTensor, nullptr);
+    std::vector<int64_t> dims(3);
+    auto dimsShape = dimsTensor->GetTensorShape();
+    dimsShape->SetDimSizes({3});
+    dimsTensor->SetDataType(DT_INT64);
+    dimsTensor->SetData(dims.data());
+    dimsTensor->SetDataSize(3 * sizeof(int64_t));
 
     CpuKernelContext ctx(HOST);
-    EXPECT_EQ(ctx.Init(node_def.get()), KERNEL_STATUS_OK);
+    EXPECT_EQ(ctx.Init(nodeDef.get()), KERNEL_STATUS_OK);
     uint32_t ret = CpuKernelRegister::Instance().RunCpuKernel(ctx);
     EXPECT_EQ(ret, KERNEL_STATUS_OK);
 
