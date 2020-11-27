f07e3e12fcf91ac01b4ef7441ec8c7998db1ab57
liyang <liyang70@huawei.com>
2020-11-27 10:12:11 +0800

    !469  [Ops] Delete unused test files after moving ops/tests to ops/built-in/tests * [Ops] Delete unused test files after moving ops/tests to ops/built-in/tests

--- ops/tests/common/python/build_whl.sh
+++
@@ -1,7 +0,0 @@
-python_cmd=$1
-whl_path=$2
-
-echo ${whl_path}
-cd ${whl_path}
-
-${python_cmd} setup.py  bdist_wheel
--- ops/tests/common/python/op_test_frame/utils/shape_utils.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-
-def calc_shape_size(shape):
-    if not shape:
-        return 0
-    reduce(lambda x, y: x * y, shape)
