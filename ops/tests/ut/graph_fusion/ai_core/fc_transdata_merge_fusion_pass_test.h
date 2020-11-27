8b999ef022e25f6dfd3eb6b1376c060b382ee7f6
liyang <liyang70@huawei.com>
2020-11-27 09:48:23 +0800

    !441 [Ops] Move utests of fusion_pass and op_tiling to ops/built-in/tests/ut * [Ops] Move utests of fusion_pass and op_tiling to ops/built-in/tests/ut

--- ops/tests/ut/graph_fusion/ai_core/fc_transdata_merge_fusion_pass_test.h
+++
@@ -1,9 +0,0 @@
-#include "graph/operator_reg.h"
-
-namespace ge {
-
-REG_OP(ReFormat)
-    .INPUT(x, TensorType({DT_FLOAT16}))
-    .OUTPUT(y, TensorType({DT_FLOAT16}))
-    .OP_END_FACTORY_REG(ReFormat)
-}
