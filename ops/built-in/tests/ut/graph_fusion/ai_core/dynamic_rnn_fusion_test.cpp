dd36f50fc13231b3077100c908dd7b9451d1760f
liyang <liyang70@huawei.com>
2020-11-27 17:37:51 +0800

    !501 [Ops] Fix commented testcases * [Ops] Fix commentted testcases

--- ops/built-in/tests/ut/graph_fusion/ai_core/dynamic_rnn_fusion_test.cpp
+++ ops/built-in/tests/ut/graph_fusion/ai_core/dynamic_rnn_fusion_test.cpp
@@ -21,49 +21,49 @@ protected:
     }
 };
 
-// TEST_F(dynamic_rnn_fusion_test, dynamic_rnn_fusion_test_1) {
-//     ge::Graph graph("dynamic_rnn_fusion_test_1");
-// 
-//     auto xData = op::Data("xData");
-//     std::vector<int64_t> dims_x{128, 25, 512};
-//     ge::Shape shape_x(dims_x);
-//     ge::TensorDesc tensorDescX(shape_x, FORMAT_NHWC,  DT_FLOAT);
-//     xData.update_input_desc_x(tensorDescX);
-//     xData.update_output_desc_y(tensorDescX);
-// 
-//     auto wData = op::Data("wData");
-//     std::vector<int64_t> dims_w{768, 1024};
-//     ge::Shape shape_w(dims_w);
-//     ge::TensorDesc tensorDescW(shape_w, FORMAT_HWCN,  DT_FLOAT);
-//     wData.update_input_desc_x(tensorDescW);
-//     wData.update_output_desc_y(tensorDescW);
-// 
-//     auto bData = op::Data("bData");
-//     std::vector<int64_t> dims_b{1024,};
-//     ge::Shape shape_b(dims_b);
-//     ge::TensorDesc tensorDescB(shape_b, FORMAT_NHWC,  DT_FLOAT);
-//     bData.update_input_desc_x(tensorDescB);
-//     bData.update_output_desc_y(tensorDescB);
-// 
-//     auto dynamicRNNOp = op::DynamicRNN("DynamicRNN_1");
-//     dynamicRNNOp.set_input_x(xData)
-//          .set_input_w(wData)
-//          .set_input_b(bData)
-//          .set_attr_time_major(false);
-// 
-//     std::vector<Operator> inputs{xData, wData, bData};
-//     std::vector<Operator> outputs{dynamicRNNOp};
-// 
-//     graph.SetInputs(inputs).SetOutputs(outputs);
-//     ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
-//     fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
-//     fe::FusionPassTestUtils::RunGraphFusionPass("DynamicRNNInsertTransposePass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
-// 
-//     bool findTranspose = false;
-//     for (auto node: compute_graph_ptr->GetAllNodes()) {
-//         if (node->GetType() == "TransposeD") {
-//             findTranspose = true;
-//         }
-//     }
-//     EXPECT_EQ(findTranspose, true);
-// }
+TEST_F(dynamic_rnn_fusion_test, dynamic_rnn_fusion_test_1) {
+    ge::Graph graph("dynamic_rnn_fusion_test_1");
+
+    auto xData = op::Data("xData");
+    std::vector<int64_t> dims_x{128, 25, 512};
+    ge::Shape shape_x(dims_x);
+    ge::TensorDesc tensorDescX(shape_x, FORMAT_NHWC,  DT_FLOAT);
+    xData.update_input_desc_x(tensorDescX);
+    xData.update_output_desc_y(tensorDescX);
+
+    auto wData = op::Data("wData");
+    std::vector<int64_t> dims_w{768, 1024};
+    ge::Shape shape_w(dims_w);
+    ge::TensorDesc tensorDescW(shape_w, FORMAT_HWCN,  DT_FLOAT);
+    wData.update_input_desc_x(tensorDescW);
+    wData.update_output_desc_y(tensorDescW);
+
+    auto bData = op::Data("bData");
+    std::vector<int64_t> dims_b{1024,};
+    ge::Shape shape_b(dims_b);
+    ge::TensorDesc tensorDescB(shape_b, FORMAT_NHWC,  DT_FLOAT);
+    bData.update_input_desc_x(tensorDescB);
+    bData.update_output_desc_y(tensorDescB);
+
+    auto dynamicRNNOp = op::DynamicRNN("DynamicRNN_1");
+    dynamicRNNOp.set_input_x(xData)
+         .set_input_w(wData)
+         .set_input_b(bData)
+         .set_attr_time_major(false);
+
+    std::vector<Operator> inputs{xData, wData, bData};
+    std::vector<Operator> outputs{dynamicRNNOp};
+
+    graph.SetInputs(inputs).SetOutputs(outputs);
+    ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+    fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+    fe::FusionPassTestUtils::RunGraphFusionPass("DynamicRNNInsertTransposePass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
+
+    bool findTranspose = false;
+    for (auto node: compute_graph_ptr->GetAllNodes()) {
+        if (node->GetType() == "TransposeD") {
+            findTranspose = true;
+        }
+    }
+    EXPECT_EQ(findTranspose, true);
+}
--- ops/built-in/tests/ut/graph_fusion/ai_core/prior_box_fusion_pass_test.cpp
+++ ops/built-in/tests/ut/graph_fusion/ai_core/prior_box_fusion_pass_test.cpp
@@ -21,151 +21,151 @@ protected:
     }
 };
 
-// TEST_F(prior_box_fusion_pass_test, prior_box_fusion_pass_test_1) {
-//     ge::Graph graph("prior_box_fusion_pass_test_1");
-//     auto feature_input_data = op::Data("feature_input_data");
-//     auto img_input_data = op::Data("img_input_data");
-//     auto boxes_input_data = op::Data("boxes_input_data");
-//     std::vector<int64_t> feature_dims{2, 16, 5, 5};
-//     std::vector<int64_t> img_dims{2, 16, 300, 300};
-//     std::vector<int64_t> boxes_dims{1, 2, 2400, 2400};
-//     ge::Shape feature_shape(feature_dims);
-//     ge::Shape img_shape(img_dims);
-//     ge::Shape boxes_shape(boxes_dims);
-//     ge::TensorDesc featureTensorDesc(feature_shape);
-//     ge::TensorDesc imgTensorDesc(img_shape);
-//     ge::TensorDesc boxesTensorDesc(boxes_shape);
-//     feature_input_data.update_input_desc_x(featureTensorDesc);
-//     feature_input_data.update_output_desc_y(featureTensorDesc);
-//     img_input_data.update_input_desc_x(imgTensorDesc);
-//     img_input_data.update_output_desc_y(imgTensorDesc);
-//     boxes_input_data.update_input_desc_x(boxesTensorDesc);
-//     boxes_input_data.update_output_desc_y(boxesTensorDesc);
-// 
-//     vector<float> min_size;
-//     vector<float> max_size;
-//     vector<float> asp;
-//     vector<float> var;
-//     min_size.push_back(162.0);
-//     max_size.push_back(213.0);
-//     asp.push_back(2);
-//     asp.push_back(3);
-//     var.push_back(0.1);
-// 
-//     auto prior_op = op::PriorBox("input_0");
-//     prior_op.set_input_x(feature_input_data)
-//             .set_input_img(img_input_data)
-//             //.set_input_boxes(boxes_input_data)
-//             .set_attr_min_size(min_size)
-//             .set_attr_max_size(max_size)
-//             .set_attr_aspect_ratio(asp)
-//             .set_attr_flip(true)
-//             .set_attr_clip(false)
-//             .set_attr_variance(var)
-//             .set_attr_img_h(300)
-//             .set_attr_img_w(300)
-//             .set_attr_step_h(64)
-//             .set_attr_step_w(64)
-//             .set_attr_offset(0.5);
-// 
-//     std::vector<Operator> inputs{feature_input_data, img_input_data, boxes_input_data};
-//     std::vector<Operator> outputs{prior_op};
-//     graph.SetInputs(inputs).SetOutputs(outputs);
-//     ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
-//     fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
-//     //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_before");
-//     fe::FusionPassTestUtils::RunGraphFusionPass("PriorBoxPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
-//     //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_after");
-// 
-//     bool findNode = false;
-//     bool shapeMatch = false;
-//     //vector<int64_t> expectShape{3, 32, 3, 32};
-//     for (auto node: compute_graph_ptr->GetAllNodes()) {
-//         if (node->GetType() == "PriorBoxDV2") {
-//             findNode = true;
-//             auto inputDesc = node->GetOpDesc()->GetInputDesc(2);
-//             std::vector<int64_t> dims = inputDesc.GetShape().GetDims();
-//             if (dims == boxes_dims) {
-//                 shapeMatch = true;
-//             }
-//         }
-//     }
-//     EXPECT_EQ(findNode, true);
-//     //EXPECT_EQ(shapeMatch, true);
-// }
-// 
-// TEST_F(prior_box_fusion_pass_test, prior_box_fusion_pass_test_2) {
-//     ge::Graph graph("prior_box_fusion_pass_test_2");
-//     auto feature_input_data = op::Data("feature_input_data");
-//     auto img_input_data = op::Data("img_input_data");
-//     auto boxes_input_data = op::Data("boxes_input_data");
-//     std::vector<int64_t> feature_dims{2, 16, 5, 5, 1};
-//     std::vector<int64_t> img_dims{2, 16, 300, 300, 1};
-//     std::vector<int64_t> boxes_dims{1, 2, 2400, 2400};
-//     ge::Shape feature_shape(feature_dims);
-//     ge::Shape img_shape(img_dims);
-//     ge::Shape boxes_shape(boxes_dims);
-//     ge::TensorDesc featureTensorDesc(feature_shape);
-//     ge::TensorDesc imgTensorDesc(img_shape);
-//     ge::TensorDesc boxesTensorDesc(boxes_shape);
-//     feature_input_data.update_input_desc_x(featureTensorDesc);
-//     feature_input_data.update_output_desc_y(featureTensorDesc);
-//     img_input_data.update_input_desc_x(imgTensorDesc);
-//     img_input_data.update_output_desc_y(imgTensorDesc);
-//     boxes_input_data.update_input_desc_x(boxesTensorDesc);
-//     boxes_input_data.update_output_desc_y(boxesTensorDesc);
-// 
-//     vector<float> min_size;
-//     vector<float> max_size;
-//     vector<float> asp;
-//     vector<float> var;
-//     min_size.push_back(162.0);
-//     max_size.push_back(213.0);
-//     asp.push_back(1.00000001);
-//     asp.push_back(1.00000021);
-//     var.push_back(0.1);
-//     var.push_back(0.1);
-//     var.push_back(0.1);
-//     var.push_back(0.1);
-// 
-//     auto prior_op = op::PriorBox("input_0");
-//     prior_op.set_input_x(feature_input_data)
-//             .set_input_img(img_input_data)
-//             //.set_input_boxes(boxes_input_data)
-//             .set_attr_min_size(min_size)
-//             .set_attr_max_size(max_size)
-//             .set_attr_aspect_ratio(asp)
-//             .set_attr_flip(true)
-//             .set_attr_clip(true)
-//             .set_attr_variance(var)
-//             .set_attr_img_h(0)
-//             .set_attr_img_w(0)
-//             .set_attr_step_h(0)
-//             .set_attr_step_w(0)
-//             .set_attr_offset(0.5);
-// 
-//     std::vector<Operator> inputs{feature_input_data, img_input_data, boxes_input_data};
-//     std::vector<Operator> outputs{prior_op};
-//     graph.SetInputs(inputs).SetOutputs(outputs);
-//     ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
-//     fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
-//     //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_before");
-//     fe::FusionPassTestUtils::RunGraphFusionPass("PriorBoxPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
-//     //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_after");
-// 
-//     bool findNode = false;
-//     bool shapeMatch = false;
-//     //vector<int64_t> expectShape{3, 32, 3, 32};
-//     for (auto node: compute_graph_ptr->GetAllNodes()) {
-//         if (node->GetType() == "PriorBoxDV2") {
-//             findNode = true;
-//             auto inputDesc = node->GetOpDesc()->GetInputDesc(2);
-//             std::vector<int64_t> dims = inputDesc.GetShape().GetDims();
-//             if (dims == boxes_dims) {
-//                 shapeMatch = true;
-//             }
-//         }
-//     }
-//     EXPECT_EQ(findNode, true);
-//     //EXPECT_EQ(shapeMatch, true);
-// }
+TEST_F(prior_box_fusion_pass_test, prior_box_fusion_pass_test_1) {
+    ge::Graph graph("prior_box_fusion_pass_test_1");
+    auto feature_input_data = op::Data("feature_input_data");
+    auto img_input_data = op::Data("img_input_data");
+    auto boxes_input_data = op::Data("boxes_input_data");
+    std::vector<int64_t> feature_dims{2, 16, 5, 5};
+    std::vector<int64_t> img_dims{2, 16, 300, 300};
+    std::vector<int64_t> boxes_dims{1, 2, 2400, 2400};
+    ge::Shape feature_shape(feature_dims);
+    ge::Shape img_shape(img_dims);
+    ge::Shape boxes_shape(boxes_dims);
+    ge::TensorDesc featureTensorDesc(feature_shape);
+    ge::TensorDesc imgTensorDesc(img_shape);
+    ge::TensorDesc boxesTensorDesc(boxes_shape);
+    feature_input_data.update_input_desc_x(featureTensorDesc);
+    feature_input_data.update_output_desc_y(featureTensorDesc);
+    img_input_data.update_input_desc_x(imgTensorDesc);
+    img_input_data.update_output_desc_y(imgTensorDesc);
+    boxes_input_data.update_input_desc_x(boxesTensorDesc);
+    boxes_input_data.update_output_desc_y(boxesTensorDesc);
+
+    vector<float> min_size;
+    vector<float> max_size;
+    vector<float> asp;
+    vector<float> var;
+    min_size.push_back(162.0);
+    max_size.push_back(213.0);
+    asp.push_back(2);
+    asp.push_back(3);
+    var.push_back(0.1);
+
+    auto prior_op = op::PriorBox("input_0");
+    prior_op.set_input_x(feature_input_data)
+            .set_input_img(img_input_data)
+            //.set_input_boxes(boxes_input_data)
+            .set_attr_min_size(min_size)
+            .set_attr_max_size(max_size)
+            .set_attr_aspect_ratio(asp)
+            .set_attr_flip(true)
+            .set_attr_clip(false)
+            .set_attr_variance(var)
+            .set_attr_img_h(300)
+            .set_attr_img_w(300)
+            .set_attr_step_h(64)
+            .set_attr_step_w(64)
+            .set_attr_offset(0.5);
+
+    std::vector<Operator> inputs{feature_input_data, img_input_data, boxes_input_data};
+    std::vector<Operator> outputs{prior_op};
+    graph.SetInputs(inputs).SetOutputs(outputs);
+    ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+    fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+    //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_before");
+    fe::FusionPassTestUtils::RunGraphFusionPass("PriorBoxPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
+    //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_after");
+
+    bool findNode = false;
+    bool shapeMatch = false;
+    //vector<int64_t> expectShape{3, 32, 3, 32};
+    for (auto node: compute_graph_ptr->GetAllNodes()) {
+        if (node->GetType() == "PriorBoxDV2") {
+            findNode = true;
+            auto inputDesc = node->GetOpDesc()->GetInputDesc(2);
+            std::vector<int64_t> dims = inputDesc.GetShape().GetDims();
+            if (dims == boxes_dims) {
+                shapeMatch = true;
+            }
+        }
+    }
+    EXPECT_EQ(findNode, true);
+    //EXPECT_EQ(shapeMatch, true);
+}
+
+TEST_F(prior_box_fusion_pass_test, prior_box_fusion_pass_test_2) {
+    ge::Graph graph("prior_box_fusion_pass_test_2");
+    auto feature_input_data = op::Data("feature_input_data");
+    auto img_input_data = op::Data("img_input_data");
+    auto boxes_input_data = op::Data("boxes_input_data");
+    std::vector<int64_t> feature_dims{2, 16, 5, 5, 1};
+    std::vector<int64_t> img_dims{2, 16, 300, 300, 1};
+    std::vector<int64_t> boxes_dims{1, 2, 2400, 2400};
+    ge::Shape feature_shape(feature_dims);
+    ge::Shape img_shape(img_dims);
+    ge::Shape boxes_shape(boxes_dims);
+    ge::TensorDesc featureTensorDesc(feature_shape);
+    ge::TensorDesc imgTensorDesc(img_shape);
+    ge::TensorDesc boxesTensorDesc(boxes_shape);
+    feature_input_data.update_input_desc_x(featureTensorDesc);
+    feature_input_data.update_output_desc_y(featureTensorDesc);
+    img_input_data.update_input_desc_x(imgTensorDesc);
+    img_input_data.update_output_desc_y(imgTensorDesc);
+    boxes_input_data.update_input_desc_x(boxesTensorDesc);
+    boxes_input_data.update_output_desc_y(boxesTensorDesc);
+
+    vector<float> min_size;
+    vector<float> max_size;
+    vector<float> asp;
+    vector<float> var;
+    min_size.push_back(162.0);
+    max_size.push_back(213.0);
+    asp.push_back(1.00000001);
+    asp.push_back(1.00000021);
+    var.push_back(0.1);
+    var.push_back(0.1);
+    var.push_back(0.1);
+    var.push_back(0.1);
+
+    auto prior_op = op::PriorBox("input_0");
+    prior_op.set_input_x(feature_input_data)
+            .set_input_img(img_input_data)
+            //.set_input_boxes(boxes_input_data)
+            .set_attr_min_size(min_size)
+            .set_attr_max_size(max_size)
+            .set_attr_aspect_ratio(asp)
+            .set_attr_flip(true)
+            .set_attr_clip(true)
+            .set_attr_variance(var)
+            .set_attr_img_h(0)
+            .set_attr_img_w(0)
+            .set_attr_step_h(0)
+            .set_attr_step_w(0)
+            .set_attr_offset(0.5);
+
+    std::vector<Operator> inputs{feature_input_data, img_input_data, boxes_input_data};
+    std::vector<Operator> outputs{prior_op};
+    graph.SetInputs(inputs).SetOutputs(outputs);
+    ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+    fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+    //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_before");
+    fe::FusionPassTestUtils::RunGraphFusionPass("PriorBoxPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
+    //    GE_DUMP(compute_graph_ptr, "diag_fusion_test_1_after");
+
+    bool findNode = false;
+    bool shapeMatch = false;
+    //vector<int64_t> expectShape{3, 32, 3, 32};
+    for (auto node: compute_graph_ptr->GetAllNodes()) {
+        if (node->GetType() == "PriorBoxDV2") {
+            findNode = true;
+            auto inputDesc = node->GetOpDesc()->GetInputDesc(2);
+            std::vector<int64_t> dims = inputDesc.GetShape().GetDims();
+            if (dims == boxes_dims) {
+                shapeMatch = true;
+            }
+        }
+    }
+    EXPECT_EQ(findNode, true);
+    //EXPECT_EQ(shapeMatch, true);
+}
