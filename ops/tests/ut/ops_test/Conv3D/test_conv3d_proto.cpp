baa27d70ebe31cbd637c81f58d86b2e24711c6ce
zheng-baihui <zhengbaihui1@hisilicon.com>
2020-11-28 16:46:25 +0800

    fix ut proto

--- ops/tests/ut/ops_test/Conv3D/test_conv3d_proto.cpp
+++ ops/tests/ut/ops_test/Conv3D/test_conv3d_proto.cpp
@@ -202,7 +202,7 @@ TEST_F(Conv3DProtoTest, conv3d_Strides_Length_Failed){
     op.SetAttr("pads", {0, 0, 0, 0, 0, 0});
 
     auto status = op.VerifyAllAttr(true);
-    EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+    EXPECT_EQ(status, ge::GRAPH_FAILED);
     auto ret = op.InferShapeAndType();
     EXPECT_EQ(ret, ge::GRAPH_FAILED);
 }
