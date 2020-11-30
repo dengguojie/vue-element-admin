cb8f540ce2a29543b4068377a718477da92bc981
张韦全 <zhangweiquan1@huawei.com>
2020-11-30 09:23:57 +0800

    !545 fix bug of max_pool_with_argmax.py

--- ops/built-in/tbe/impl/max_pool_with_argmax.py
+++ ops/built-in/tbe/impl/max_pool_with_argmax.py
@@ -1398,7 +1398,7 @@ class MaxPoolWithargmax():
         if self.window_h >= self.stride_h:
             cut_h_size = ((img2col_h // (((self.in_size_w + self.pad[0] + self.pad[1])) // self.stride_w + 1)) - 1) * \
                          self.stride_h + self.window_h - self.stride_h
-            if cut_h_size < self.window_h:
+            if cut_h_size < self.window_h or (cut_h_size - self.pad[2]) >= self.in_size_h:
                 cut_h_size = self.window_h
             cut_stride = cut_h_size - (self.window_h - self.stride_h)
         else:
