333529b6cdbb7decabdd972928406c510d3ec846
tbe <tanshengshun@huawei.com>
2020-12-01 17:30:04 +0800

    modify mul op_select format

--- ops/built-in/tests/ut/ops_test/Mul/test_Mul_impl.py
+++ ops/built-in/tests/ut/ops_test/Mul/test_Mul_impl.py
@@ -80,11 +80,10 @@ def test_op_select_format(test_arg):
                      {"shape": (96, 1, 56, 56, 16), "dtype": "float16", "format": "NC1HWC0", "ori_shape": (96, 56, 56, 8),"ori_format": "NHWC"})
     op_select_format({"shape": (), "dtype": "float32", "format": "NHWC", "ori_shape": (),"ori_format": "NHWC"},
                      {"shape": (96, 256), "dtype": "float32", "format": "FRACTAL_NZ", "ori_shape": (96, 256),"ori_format": "NHWC"},
-                     {"shape": (96, 256), "dtype": "float32", "format": "FRACTAL_NZ", "ori_shape": (96, 256),"ori_format": "NHWC"}
+                     {"shape": (96, 256), "dtype": "float32", "format": "FRACTAL_NZ", "ori_shape": (96, 256),"ori_format": "NHWC"})
     op_select_format({"shape": (25, 1, 16, 16), "dtype": "float32", "format": "FRACTAL_Z", "ori_shape": (6, 1, 5, 5),"ori_format": "NCHW"},
                      {"shape": (), "dtype": "float32", "format": "NCHW", "ori_shape": (),"ori_format": "NCHW"},
-                     {"shape": (25, 1, 16, 16), "dtype": "float32", "format": "FRACTAL_Z", "ori_shape": (6, 1, 5, 5),"ori_format": "NCHW"})
-
+                     {"shape": (25, 1, 16, 16), "dtype": "float32", "format": "FRACTAL_Z", "ori_shape": (6, 1, 5, 5,"ori_format": "NCHW"})
 
 ut_case.add_case(["Ascend310", "Ascend710", "Ascend910"], case1)
 ut_case.add_case(["Ascend310", "Ascend710", "Ascend910"], case2)
