88320b2f22652d40de73062b192443d6e5f7e814
huangqiang <huangqiang3@huawei.com>
2020-12-02 13:09:30 +0800

    infer profiling test

--- ops/built-in/tests/ut/graph_fusion/ai_core/test_concat_offset_proto.cpp
+++ ops/built-in/tests/ut/graph_fusion/ai_core/test_concat_offset_proto.cpp
@@ -25,31 +25,31 @@ TEST_F(concat_offset_test, concat_offset_test_1){
   auto shape_x1 = vector<int64_t>({-1,});
   TensorDesc desc_x1(ge::Shape(shape_x1), FORMAT_ND, DT_INT32);
   std::vector<std::pair<int64_t, int64_t>>x1_range;
-  x1_range.push_back(std::pair<int64_t, int64_t>){1,9};
+  x1_range.push_back(std::pair<int64_t, int64_t>{1,9});
   desc_x1.SetShapeRange(x1_range);
 
   auto data_x1 = op::Data("x1");
-  data_x1.update_input_desc(desc_x1);
+  data_x1.update_input_desc_x(desc_x1);
   data_x1.update_output_desc_y(desc_x1);
 
   auto shape_x2 = vector<int64_t>({-1,});
   TensorDesc desc_x2(ge::Shape(shape_x2), FORMAT_ND, DT_INT32);
   std::vector<std::pair<int64_t, int64_t>>x2_range;
-  x2_range.push_back(std::pair<int64_t, int64_t>){2,10};
+  x2_range.push_back(std::pair<int64_t, int64_t>{2,10});
   desc_x2.SetShapeRange(x2_range);
 
   auto data_x2 = op::Data("x2");
-  data_x2.update_input_desc(desc_x2);
+  data_x2.update_input_desc_x(desc_x2);
   data_x2.update_output_desc_y(desc_x2);
 
   auto shape_x3 = vector<int64_t>({-1,});
   TensorDesc desc_x3(ge::Shape(shape_x3), FORMAT_ND, DT_INT32);
   std::vector<std::pair<int64_t, int64_t>>x3_range;
-  x3_range.push_back(std::pair<int64_t, int64_t>){3,12};
+  x3_range.push_back(std::pair<int64_t, int64_t>{3,12});
   desc_x3.SetShapeRange(x3_range);
 
   auto data_x3 = op::Data("x3");
-  data_x3.update_input_desc(desc_x3);
+  data_x3.update_input_desc_x(desc_x3);
   data_x3.update_output_desc_y(desc_x3);
 
   //init concat_dim
@@ -111,262 +111,5 @@ TEST_F(concat_offset_test, concat_offset_test_1){
   EXPECT_EQ(ShapeMatch1, true);
   EXPECT_EQ(ShapeMatch2, true);
   EXPECT_EQ(ShapeMatch3, true);
-}
-
-TEST_F(concat_offset_test, concat_offset_test_2){
-  ge::Graph graph("concat_offset_2");
-  auto shape_x1 = vector<int64_t>({-1,});
-  TensorDesc desc_x1(ge::Shape(shape_x1), FORMAT_ND, DT_INT32);
-  std::vector<std::pair<int64_t, int64_t>>x1_range;
-  x1_range.push_back(std::pair<int64_t, int64_t>){1,9};
-  desc_x1.SetShapeRange(x1_range);
-
-  auto data_x1 = op::Data("x1");
-  data_x1.update_input_desc(desc_x1);
-  data_x1.update_output_desc_y(desc_x1);
-
-  auto shape_x2 = vector<int64_t>({-1,});
-  TensorDesc desc_x2(ge::Shape(shape_x2), FORMAT_ND, DT_INT32);
-
-
-  auto data_x2 = op::Data("x2");
-  data_x2.update_input_desc(desc_x2);
-  data_x2.update_output_desc_y(desc_x2);
-
-  auto shape_x3 = vector<int64_t>({5,});
-  TensorDesc desc_x3(ge::Shape(shape_x3), FORMAT_ND, DT_INT32);
-
-  auto data_x3 = op::Data("x3");
-  data_x3.update_input_desc(desc_x3);
-  data_x3.update_output_desc_y(desc_x3);
-
-  //init concat_dim
-  Tensor input_concat_dim_tensor;
-  input_concat_dim_tensor.SetTensorDesc(TensorDesc(ge::Shape({1}), ge::FORMAT_ND, ge::DT_INT32));
-  unit32_t* input_concat_dim_value = new uint32_t[1]{1};
-  input_concat_dim_tensor.SetData((uint8_t*)input_concat_dim_value, 1*sizeof(uint32_t));
-  auto input_concat_dim_data = op::Const("concat_dim").set_attr_value(input_concat_dim_tensor);
-  delete []input_concat_dim_value;
-
-  //new op
-  auto test_layer = op::ConcatOffset("ConcatOffset_2");
-  test_layer.set_input_concat_dim(input_concat_dim_data)
-            .create_dynamic_input_x(3)
-            .set_dynamic_input_x(0,data_x1)
-            .set_dynamic_input_x(1,data_x2)
-            .set_dynamic_input_x(2,data_x3)
-            .set_attr_N(3)
-            .create_dynamic_output_y(3);
-  std::vector<Operator> inputs{input_concat_dim_data, data_x1, data_x2,data_x3};
-  std::vector<Operator> outputs{test_layer};
-  graph.SetInputs(inputs).SetOutputs(outputs);
-
-  ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
-  fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
-
-  bool findOp = false;
-  bool ShapeMatch1 = false;
-  bool ShapeMatch2 = false;
-  bool ShapeMatch3 = false;
-  bool ShapeRangeMatch1 = false;
-  bool ShapeRangeMatch2 = false;
-  bool ShapeRangeMatch3 = false;
-  //except_shape
-  std::vector<std::pair<int64_t, int64_t>>expect_range1;
-  std::vector<std::pair<int64_t, int64_t>>expect_range2;
-  std::vector<std::pair<int64_t, int64_t>>expect_range3;
-
-  auto expect_shape1 = vector<int64_t>({5,});
-  auto expect_shape2 = vector<int64_t>({5,});
-  auto expect_shape3 = vector<int64_t>({5,});
-  expect_range1.push_back(std::pair<int64_t,int64_t>{5,5});
-  expect_range2.push_back(std::pair<int64_t,int64_t>{5,5});
-  expect_range3.push_back(std::pair<int64_t,int64_t>{5,5});
-
-  for(auto node: compute_graph_ptr->GetAllNodes()){
-      if(node->GetType() == "ConcatOffset"){
-          std::cout<< "ConcatOffset test_2!!!"<<std::endl;
-          findOp = true;
-          auto outputDesc1 = node->GetOpDesc()->GetOutputDesc(0);
-          auto outputDesc2 = node->GetOpDesc()->GetOutputDesc(1);
-          auto outputDesc3 = node->GetOpDesc()->GetOutputDesc(2);
-          std::vector<int64_t> output_shape1 = outputDesc1.GetShape().GetDims();
-          std::vector<int64_t> output_shape2 = outputDesc2.GetShape().GetDims();
-          std::vector<int64_t> output_shape3 = outputDesc3.GetShape().GetDims();
-          std::vector<std::pair<int64_t,int64_t>> output_range1;
-          std::vector<std::pair<int64_t,int64_t>> output_range2;
-          std::vector<std::pair<int64_t,int64_t>> output_range3;
-          outputDesc1.GetShapeRange(output_range1);
-          outputDesc2.GetShapeRange(output_range2);
-          outputDesc3.GetShapeRange(output_range3);
-          if(output_shape1 == expect_shape1){
-              shapeMatch1 = true;
-          }
-          if(output_shape2 == expect_shape2){
-              shapeMatch1 = true;
-          }
-          if(output_shape3 == expect_shape3){
-              shapeMatch1 = true;
-          }
-          if(output_range1 == expect_range1){
-              shapeRangeMatch1 = true;
-          }
-          if(output_range2 == expect_range2){
-              shapeRangeMatch1 = true;
-          }
-          if(output_range3 == expect_range3){
-              shapeRangeMatch1 = true;
-          }
-      }
-  }
-  EXPECT_EQ(findOp, true);
-  EXPECT_EQ(ShapeMatch1, true);
-  EXPECT_EQ(ShapeMatch2, true);
-  EXPECT_EQ(ShapeMatch3, true);
-  EXPECT_EQ(ShapeRangeMatch1, true);
-  EXPECT_EQ(ShapeRangeMatch2, true);
-  EXPECT_EQ(ShapeRangeMatch3, true);
-}
-
-TEST_F(concat_offset_test, concat_offset_test_3{
-  ge::Graph graph("concat_offset_3");
-  auto shape_x1 = vector<int64_t>({3,});
-  TensorDesc desc_x1(ge::Shape(shape_x1), FORMAT_ND, DT_INT32);
-
-  auto data_x1 = op::Data("x1");
-  data_x1.update_input_desc(desc_x1);
-  data_x1.update_output_desc_y(desc_x1);
-
-  auto shape_x2 = vector<int64_t>({3,});
-  TensorDesc desc_x2(ge::Shape(shape_x2), FORMAT_ND, DT_INT32);
-
-  auto data_x2 = op::Data("x2");
-  data_x2.update_input_desc(desc_x2);
-  data_x2.update_output_desc_y(desc_x2);
-
-  //init concat_dim
-  Tensor input_concat_dim_tensor;
-  input_concat_dim_tensor.SetTensorDesc(TensorDesc(ge::Shape({1}), ge::FORMAT_ND, ge::DT_INT32));
-  unit32_t* input_concat_dim_value = new uint32_t[1]{1};
-  input_concat_dim_tensor.SetData((uint8_t*)input_concat_dim_value, 1*sizeof(uint32_t));
-  auto input_concat_dim_data = op::Const("concat_dim").set_attr_value(input_concat_dim_tensor);
-  delete []input_concat_dim_value;
-
-  //new op
-  auto test_layer = op::ConcatOffset("ConcatOffset_2");
-  test_layer.set_input_concat_dim(input_concat_dim_data)
-            .create_dynamic_input_x(2)
-            .set_dynamic_input_x(0,data_x1)
-            .set_dynamic_input_x(1,data_x2)
-            .set_attr_N(2)
-            .create_dynamic_output_y(2);
-  std::vector<Operator> inputs{input_concat_dim_data, data_x1, data_x2};
-  std::vector<Operator> outputs{test_layer};
-  graph.SetInputs(inputs).SetOutputs(outputs);
-
-  ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
-  fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
-
-  bool findOp = false;
-  bool ShapeMatch1 = false;
-  bool ShapeMatch2 = false;
-  bool ShapeRangeMatch1 = false;
-  bool ShapeRangeMatch2 = false;
-  //except_shape
-  std::vector<std::pair<int64_t, int64_t>>expect_range1;
-  std::vector<std::pair<int64_t, int64_t>>expect_range2;
-
-  auto expect_shape1 = vector<int64_t>({3,});
-  auto expect_shape2 = vector<int64_t>({3,});
-  for(auto node: compute_graph_ptr->GetAllNodes()){
-      if(node->GetType() == "ConcatOffset"){
-          std::cout<< "ConcatOffset test_3!!!"<<std::endl;
-          findOp = true;
-          auto outputDesc1 = node->GetOpDesc()->GetOutputDesc(0);
-          auto outputDesc2 = node->GetOpDesc()->GetOutputDesc(1);
-          std::vector<int64_t> output_shape1 = outputDesc1.GetShape().GetDims();
-          std::vector<int64_t> output_shape2 = outputDesc2.GetShape().GetDims();
-          if(output_shape1 == expect_shape1){
-              shapeMatch1 = true;
-          }
-          if(output_shape2 == expect_shape2){
-              shapeMatch1 = true;
-          }
-      }
-  }
-  EXPECT_EQ(findOp, true);
-  EXPECT_EQ(ShapeMatch1, true);
-  EXPECT_EQ(ShapeMatch2, true);
-}
-
-TEST_F(concat_offset_test, concat_offset_test_4) {
-  ge::Graph graph("concat_offset_3");
-  auto shape_x1 = vector<int64_t>({3,});
-  TensorDesc desc_x1(ge::Shape(shape_x1), FORMAT_ND, DT_INT32);
-
-  auto data_x1 = op::Data("x1");
-  data_x1.update_input_desc(desc_x1);
-  data_x1.update_output_desc_y(desc_x1);
-
-  auto shape_x2 = vector<int64_t>({3,});
-  TensorDesc desc_x2(ge::Shape(shape_x2), FORMAT_ND, DT_INT32);
-
-  auto data_x2 = op::Data("x2");
-  data_x2.update_input_desc(desc_x2);
-  data_x2.update_output_desc_y(desc_x2);
-
-  //init concat_dim
-  Tensor input_concat_dim_tensor;
-  input_concat_dim_tensor.SetTensorDesc(TensorDesc(ge::Shape({1}), ge::FORMAT_ND, ge::DT_INT32));
-  unit32_t* input_concat_dim_value = new uint32_t[1]{1};
-  input_concat_dim_tensor.SetData((uint8_t*)input_concat_dim_value, 1*sizeof(uint32_t));
-  auto input_concat_dim_data = op::Const("concat_dim").set_attr_value(input_concat_dim_tensor);
-  delete []input_concat_dim_value;
-
-  //new op
-  auto test_layer = op::ConcatOffset("ConcatOffset_2");
-  test_layer.set_input_concat_dim(input_concat_dim_data)
-            .create_dynamic_input_x(2)
-            .set_dynamic_input_x(0,data_x1)
-            .set_dynamic_input_x(1,data_x2)
-            .set_attr_N(2)
-            .create_dynamic_output_y(2);
-  std::vector<Operator> inputs{input_concat_dim_data, data_x1, data_x2};
-  std::vector<Operator> outputs{test_layer};
-  graph.SetInputs(inputs).SetOutputs(outputs);
-
-  ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
-  fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
-
-  bool findOp = false;
-  bool ShapeMatch1 = false;
-  bool ShapeMatch2 = false;
-  bool ShapeRangeMatch1 = false;
-  bool ShapeRangeMatch2 = false;
-  //except_shape
-  std::vector<std::pair<int64_t, int64_t>>expect_range1;
-  std::vector<std::pair<int64_t, int64_t>>expect_range2;
-
-  auto expect_shape1 = vector<int64_t>({4,});
-  auto expect_shape2 = vector<int64_t>({3,});
-  for(auto node: compute_graph_ptr->GetAllNodes()){
-      if(node->GetType() == "ConcatOffset"){
-          std::cout<< "ConcatOffset test_3!!!"<<std::endl;
-          findOp = true;
-          auto outputDesc1 = node->GetOpDesc()->GetOutputDesc(0);
-          auto outputDesc2 = node->GetOpDesc()->GetOutputDesc(1);
-          std::vector<int64_t> output_shape1 = outputDesc1.GetShape().GetDims();
-          std::vector<int64_t> output_shape2 = outputDesc2.GetShape().GetDims();
-          if(output_shape1 == expect_shape1){
-              shapeMatch1 = true;
-          }
-          if(output_shape2 == expect_shape2){
-              shapeMatch1 = true;
-          }
-      }
-  }
-  EXPECT_EQ(findOp, true);
-  EXPECT_EQ(ShapeMatch1, true);
-  EXPECT_EQ(ShapeMatch2, true);
 }
 
