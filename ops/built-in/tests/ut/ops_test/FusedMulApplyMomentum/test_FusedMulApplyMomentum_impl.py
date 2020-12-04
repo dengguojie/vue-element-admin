edd9ce4a03a5f8fe07fe65cbbdd2073bf3bf7c78
tbe <tanshengshun@huawei.com>
2020-12-04 16:25:47 +0800

    get split_info for lx_fusion

--- ops/built-in/tests/ut/ops_test/FusedMulApplyMomentum/test_FusedMulApplyMomentum_impl.py
+++ ops/built-in/tests/ut/ops_test/FusedMulApplyMomentum/test_FusedMulApplyMomentum_impl.py
@@ -66,7 +66,7 @@ def test_get_op_support_info(test_arg):
                         {"shape": (1,), "dtype": "float16", "format":"ND", "ori_format":"ND", "ori_shape":(1,)},
                         {"shape": (1,), "dtype": "float16", "format":"ND", "ori_format":"ND", "ori_shape":(1,)},
                         {"shape": (144, 16, 16, 16), "dtype": "float16","format":"ND", "ori_format":"ND", "ori_shape":(144, 16, 16, 16)},
-                        {"shape": (144, 16, 16, 16), "dtype": "float16","format":"ND", "ori_format":"ND", "ori_shape":(144, 16, 16, 16)})
+                        {"shape": (144, 16, 16, 16), "dtype": "float16","format":"ND", "ori_format":"ND", "ori_shape":(144, 16, 16, 16)}, True)
 
 ut_case.add_case(["Ascend710", "Ascend910"], case1)
 ut_case.add_case(["Ascend710", "Ascend910"], case2)
