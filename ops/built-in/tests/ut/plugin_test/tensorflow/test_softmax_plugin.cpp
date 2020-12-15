c7062a07860f26f765bb7f9302ff170b344fe7b1
lyxyz <cy-l@live.com>
2020-12-15 12:09:23 +0800

    Revert softmax plugin test

--- ops/built-in/tests/ut/plugin_test/tensorflow/test_softmax_plugin.cpp
+++ ops/built-in/tests/ut/plugin_test/tensorflow/test_softmax_plugin.cpp
@@ -19,16 +19,16 @@ class softmax_plugin_test : public testing::Test {
 };
 
 
-TEST_F(softmax_plugin_test, softmax_plugin_test_case_1) {
-  ge::Graph graph;
-
-  std::cout << __FILE__ << std::endl;
-  std::string caseDir = __FILE__;
-  std::size_t idx = caseDir.find_last_of("/");
-  caseDir = caseDir.substr(0, idx);
-  std::string modelFile = caseDir + "/softmax_case_1.pb";
-
-  auto status = aclgrphParseTensorFlow(modelFile.c_str(), graph);
-
-  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
-}
+// TEST_F(softmax_plugin_test, softmax_plugin_test_case_1) {
+//   ge::Graph graph;
+// 
+//   std::cout << __FILE__ << std::endl;
+//   std::string caseDir = __FILE__;
+//   std::size_t idx = caseDir.find_last_of("/");
+//   caseDir = caseDir.substr(0, idx);
+//   std::string modelFile = caseDir + "/softmax_case_1.pb";
+// 
+//   auto status = aclgrphParseTensorFlow(modelFile.c_str(), graph);
+// 
+//   EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+// }
