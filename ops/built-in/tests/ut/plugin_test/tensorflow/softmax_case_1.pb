2ed5c7db1ae8bbbbcd2968a7c01b0f3057571b14
王星 <ntkingstar@163.com>
2020-12-15 09:51:48 +0800

    !1119 add softmax plugin set origin format Merge pull request !1119 from 王星/development

---
+++ ops/built-in/tests/ut/plugin_test/tensorflow/softmax_case_1.pb
@@ -0,0 +1,17 @@
+
+8
+PlaceholderPlaceholder*
+dtype0*
+shape:
+:
+
+Placeholder_1Placeholder*
+shape:*
+dtype0
+/
+AddAddPlaceholder
+Placeholder_1*
+T0
+'
+softmax_test_1SoftmaxAdd*
+T0"�
--- ops/built-in/tests/ut/plugin_test/tensorflow/test_softmax_pb_gen.py
+++ ops/built-in/tests/ut/plugin_test/tensorflow/test_softmax_pb_gen.py
@@ -7,5 +7,6 @@ pb_file_path = os.getcwd()
 with tf.Session(graph=tf.Graph()) as sess:
     x = tf.placeholder(dtype="float16", shape=(1,))
     y = tf.placeholder(dtype="float16", shape=(1,))
-    op = tf.add(x, y, name='add_test_1')
-    tf.io.write_graph(sess.graph, logdir="./", name="add_case_1.pb", as_text=False)
+    add_op = tf.add(x, y)
+    op = tf.nn.softmax(add_op, name='softmax_test_1')
+    tf.io.write_graph(sess.graph, logdir="./", name="softmax_case_1.pb", as_text=False)
