03d3f35fa41a7a92e76eec0547f8717ea6d626df
lyxyz <cy-l@live.com>
2020-12-19 18:12:46 +0800

    Revert "!1339 conv2dtranspose and deconvlution support dynamic infershape"
    This reverts commit c1328d259884e5e43fef5b070c3ca1948f5544bd.

--- ops/built-in/tests/ut/ops_test/Conv2DTranspose/test_Conv2DTranspose_proto.cpp
+++ ops/built-in/tests/ut/ops_test/Conv2DTranspose/test_Conv2DTranspose_proto.cpp
@@ -58,37 +58,6 @@ TEST_F(Conv2DTransposeProtoTest, conv2dTransposeVerifyfilterTest1) {
     EXPECT_EQ(ret, ge::GRAPH_FAILED);
 }
 
-//check dynamic mode
-TEST_F(Conv2DTransposeProtoTest, conv2dTransposeVerifyfilterTestDynamic1) {
-    ge::op::Conv2DTranspose op;
-    op.UpdateInputDesc("x", create_desc_shape_range({128, 512, -1, -1},
-                                                    ge::DT_FLOAT16,
-                                                    ge::FORMAT_NCHW,
-                                                    {128, 512, -1, -1},
-                                                    ge::FORMAT_NCHW,
-                                                    {{128, 128}, {512, 512}, {6, 26}, {6, 26}}));
-    op.UpdateInputDesc("input_size", create_desc_shape_range({128, 512, -1, -1},
-                                                    ge::DT_INT64,
-                                                    ge::FORMAT_NCHW,
-                                                    {128, 512, -1, -1},
-                                                    ge::FORMAT_NCHW,
-                                                    {{128, 128}, {512, 512}, {12, 52}, {12, 52}}));                            
-    op.UpdateInputDesc("filter", create_desc({512, 256, 1, 1}, ge::DT_FLOAT16));
-    op.UpdateInputDesc("bias", create_desc({128, 256, 14, 14}, ge::DT_FLOAT16));
-    op.SetAttr("strides", {1, 1, 2, 2});
-    op.SetAttr("pads", {0, 0, 0, 0});
-    op.SetAttr("dilations", {1, 1, 1, 1});
-    op.SetAttr("groups", true);
-    op.SetAttr("data_format","NCHW");
-    op.SetAttr("output_padding", {0, 0, 0, 0});
-    op.SetAttr("offset_x", 0);
-
-    auto status = op.VerifyAllAttr(true);
-    EXPECT_EQ(status, ge::GRAPH_SUCCESS);
-    auto ret = op.InferShapeAndType();
-    EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
 //check filter out of size 4
 TEST_F(Conv2DTransposeProtoTest, conv2dTransposeVerifyfilterTest2) {
     ge::op::Conv2DTranspose op;
