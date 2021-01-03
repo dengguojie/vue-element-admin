39d3de6db7cefa0d4472a381ff8abfe19acae331
李旭龙 <lixulong@huawei.com>
2021-01-03 15:31:16 +0800

    !1920 high performance for l2_loss Merge pull request !1920 from 李旭龙/development

--- ops/built-in/tbe/impl/l2_loss.py
+++ ops/built-in/tbe/impl/l2_loss.py
@@ -16,10 +16,45 @@
 l2_loss
 """
 import te.lang.cce as tbe
+import te.platform as tbe_platform
 from te import tvm
 from te.utils import para_check
 from te.utils import shape_util
 from te.utils.error_manager import error_manager_vector
+
+
+# pylint: disable=invalid-name, unused-argument
+@tbe_platform.fusion_manager.fusion_manager.register("l2_loss")
+def l2_loss_compute(x, y, kernel_name="l2_loss"):
+    """
+     Reduce a tensor on a certain axis, and scale output with coeff
+
+     Parameters
+     ----------
+     shape : shape of data
+     dtype : source data type, only support float16, float32
+     kernel_name : kernel name, default value is "l2_loss"
+
+     Returns
+     -------
+     res: TVM tensor
+        the result of compute
+     """
+    coeff_dtype = x.dtype
+    _, axis = shape_util.simplify_axis_shape(x.shape, range(len(x.shape)))
+    if x.dtype == "float16" and tbe_platform.api_check_support("tik.set_atomic_add")\
+            and tbe_platform.api_check_support("te.lang.cce.vmul", "float32"):
+        x = tbe.cast_to(x, "float32")
+        coeff_dtype = "float32"
+
+    coeff_sqrt = tvm.const(1.0 / (2 ** (0.5)), dtype=coeff_dtype)
+
+    data_mul = tbe.vmuls(x, coeff_sqrt)
+    data_sqr = tbe.vmul(data_mul, data_mul)
+    res = tbe.sum(data_sqr, axis)
+
+    return res
+
 
 # pylint: disable=invalid-name
 @para_check.check_op_params(para_check.REQUIRED_INPUT, para_check.REQUIRED_OUTPUT,
@@ -31,15 +66,12 @@ def l2_loss(x, y, kernel_name="l2_loss"):
     Parameters
     ----------
     shape : shape of data
-
     dtype : source data type, only support float16, float32
-
     kernel_name : kernel name, default value is "l2_loss"
 
     Returns
     -------
     None
-
     """
     shape = x.get("shape")
     dtype = x.get("dtype")
@@ -56,11 +88,7 @@ def l2_loss(x, y, kernel_name="l2_loss"):
     inp_dtype = dtype.lower()
     data_input = tvm.placeholder(shape, name="data_input", dtype=inp_dtype)
 
-    coeff_sqrt = tvm.const(1.0 / (2**(0.5)), dtype=inp_dtype)
-
-    data_mul = tbe.vmuls(data_input, coeff_sqrt)
-    data_sqr = tbe.vmul(data_mul, data_mul)
-    res = tbe.sum(data_sqr, axis)
+    res = l2_loss_compute(data_input, y, kernel_name)
 
     with tvm.target.cce():
         sch = tbe.auto_schedule(res)
@@ -68,3 +96,4 @@ def l2_loss(x, y, kernel_name="l2_loss"):
     config = {"name": kernel_name,
               "tensor_list": [data_input, res]}
     tbe.cce_build_code(sch, config)
+
