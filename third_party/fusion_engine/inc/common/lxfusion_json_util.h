425c38248849a04ca88b7948264c197b561c89b6
LiuZhenyu <liuzhenyu11@huawei.com>
2021-02-03 14:07:24 +0800

    !3081 更新metadef Merge pull request !3081 from LiuZhenyu/development

--- third_party/fusion_engine/inc/common/lxfusion_json_util.h
+++ third_party/fusion_engine/inc/common/lxfusion_json_util.h
@@ -21,8 +21,8 @@
 #include "op_slice_info.h"
 
 namespace fe {
-    void SetOpSliceInfoToJson(fe::OpCalcInfo &op_calc_info, std::string &op_calc_info_str, const string &slice_mode = fe::OP_SLICE_INFO);
+    void SetOpSliceInfoToJson(fe::OpCalcInfo &op_calc_info, std::string &op_calc_info_s);
 
-    void GetOpSliceInfoFromJson(fe::OpCalcInfo &op_calc_info, std::string &op_calc_info_str, const string &slice_mode = fe::OP_SLICE_INFO);
+    void GetOpSliceInfoFromJson(fe::OpCalcInfo &op_calc_info, std::string &op_calc_info_str);
 } // namespace fe
 #endif // FUSION_ENGINE_INC_COMMON_LXFUSION_JSON_UTIL_H
