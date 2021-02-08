a5d8f22753fd438e9d8fc898cc2c234a10c03c8f
傅煌培 <fuhuangpei@hisilicon.com>
2021-02-08 20:03:02 +0800

    !3294 fix gemm check func Merge pull request !3294 from 傅煌培/development

--- ops/built-in/tbe/impl/gemm.py
+++ ops/built-in/tbe/impl/gemm.py
@@ -63,12 +63,14 @@ def _check_param(input_x1, input_x2, bias, trans_a, trans_b):
     kn_shape, n_shape = (n_shape, kn_shape) if input_x2.get("ori_format") == "FRACTAL_NZ" else (kn_shape, n_shape)
     block_out = tbe_platform.BLOCK_OUT
     block_in = tbe_platform.BLOCK_IN
-    need_align_bias = ((input_x1.get("ori_format") == "FRACTAL_NZ")
-        and (input_x2.get("ori_format") in ("FRACTAL_NZ", "FRACTAL_Z"))
-        and (bias.get("ori_format") == "ND"))
-    if need_align_bias:
-        bias_shape[0] = (bias_shape[0] + block_in - 1) // block_in
-        bias_shape[1] = (bias_shape[1] + block_out - 1) // block_out
+    block_reduce = tbe_platform.BLOCK_REDUCE if input_x1.get("dtype") == "float16" else tbe_platform.BLOCK_REDUCE_INT8
+    km_shape = ((km_shape + block_reduce - 1) // block_reduce) if input_x1.get("ori_format") == "ND" else km_shape
+    kn_shape = ((kn_shape + block_reduce - 1) // block_reduce) if input_x2.get("ori_format") == "ND" else kn_shape
+    m_shape = ((m_shape + block_in - 1) // block_in) if input_x1.get("ori_format") == "ND" else m_shape
+    n_shape = ((n_shape + block_out - 1) // block_out) if input_x2.get("ori_format") == "ND" else n_shape
+    bias_shape = list(bias_shape)
+    bias_shape[0] = ((bias_shape[0] + block_in - 1) // block_in) if bias.get("ori_format") == "ND" else bias_shape[0]
+    bias_shape[1] = ((bias_shape[1] + block_out - 1) // block_out) if bias.get("ori_format") == "ND" else bias_shape[1]
     if km_shape != kn_shape:
         args_dict = {"errCode": "E60009", "a_1d": km_shape, "b_0d": kn_shape}
         raise RuntimeError(args_dict, error_manager.get_error_message(args_dict))
