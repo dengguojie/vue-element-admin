43e26bccd82c057002a14e884b3cc6a19fcfa92d
镇亮 <zhenliang1@huawei.com>
2021-02-08 21:05:01 +0800

    !3300 【onnx】bugfix(reduceMax) Support input.dtype=Int64 and axes=None Merge pull request !3300 from 镇亮/reduceMax

--- ops/built-in/framework/onnx_plugin/reduce_max_plugin.cc
+++ ops/built-in/framework/onnx_plugin/reduce_max_plugin.cc
@@ -17,43 +17,75 @@
 #include "op_log.h"
 #include "proto/onnx/ge_onnx.pb.h"
 #include "register/register.h"
+#include "graph.h"
+#include "all_ops.h"
 
+using namespace ge;
 namespace domi {
 using NodeProto = ge::onnx::NodeProto;
-Status ParseParamsReduceMax(const Message *op_src, ge::Operator &op_dest) {
-  const NodeProto *node = dynamic_cast<const NodeProto *>(op_src);
+Status ParseParamsReduceMax(const Message* op_src, ge::Operator& op_dest) {
+  const NodeProto* node = dynamic_cast<const NodeProto*>(op_src);
   if (node == nullptr) {
     OP_LOGE("ReduceMax", "Dynamic cast op_src to NodeProto failed.");
     return FAILED;
   }
 
-  std::vector<int> v_axis;
+  std::vector<int> axes = {};
   bool keep_dims = true;
-  for (const auto &attr : node->attribute()) {
-    if (attr.name() == "axes" &&
-        attr.type() == ge::onnx::AttributeProto::INTS) {
+  for (const auto& attr : node->attribute()) {
+    if (attr.name() == "axes" && attr.type() == ge::onnx::AttributeProto::INTS) {
       for (int i = 0; i < attr.ints_size(); i++) {
-        v_axis.push_back(attr.ints(i));
+        axes.push_back(attr.ints(i));
       }
-    } else if (attr.name() == "keepdims" &&
-               attr.type() == ge::onnx::AttributeProto::INT) {
-      if (attr.i() != 1) {
-        keep_dims = false;
-      }
+    } else if (attr.name() == "keepdims" && attr.type() == ge::onnx::AttributeProto::INT) {
+      keep_dims = (attr.i() == 1);
     }
   }
-  if (v_axis.size() == 0){
-      v_axis = {};
-  }
-  op_dest.SetAttr("axes", v_axis);
+  int num = axes.size();
+  ge::Shape shape({num});
+  ge::TensorDesc tensorDesc(shape, FORMAT_NCHW, DT_INT32);
+  ge::Tensor tensor(tensorDesc, reinterpret_cast<uint8_t*>(axes.data()), axes.size() * sizeof(DT_INT32));
+
+  op_dest.SetAttr("axes", tensor);
   op_dest.SetAttr("keep_dims", keep_dims);
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op_dest);
+  op_desc->AddDynamicInputDesc("x", 2);
+  op_desc->AddDynamicOutputDesc("y", 1);
+
+  return SUCCESS;
+}
+
+static Status ParseOpToGraphReduceMax(const ge::Operator& op, Graph& graph) {
+  auto data0 = op::Data("data0").set_attr_index(0);
+
+  ge::Tensor axes;
+  if (op.GetAttr("axes", axes) != SUCCESS) {
+    OP_LOGE("ReduceMax", "get axes from op failed");
+    return FAILED;
+  }
+  auto data1 = op::Const("data1").set_attr_value(axes);
+  auto reducemax = op::ReduceMax().set_input_x(data0).set_input_axes(data1);
+
+  bool keep_dims = false;
+  if (op.GetAttr("keep_dims", keep_dims) != SUCCESS) {
+    OP_LOGE("ReduceMax", "get keep_dims from op failed");
+    return FAILED;
+  }
+  reducemax.set_attr_keep_dims(keep_dims);
+
+  std::vector<ge::Operator> inputs{data0};
+  std::vector<std::pair<ge::Operator, std::vector<size_t> > > outputs;
+  outputs.emplace_back(reducemax, std::vector<std::size_t>{0});
+  graph.SetInputs(inputs).SetOutputs(outputs);
+
   return SUCCESS;
 }
 
 // register ReduceMax op info to GE
-REGISTER_CUSTOM_OP("ReduceMaxD")
-  .FrameworkType(ONNX)
-  .OriginOpType("ai.onnx::11::ReduceMax")
-  .ParseParamsFn(ParseParamsReduceMax)
-  .ImplyType(ImplyType::TVM);
+REGISTER_CUSTOM_OP("PartitionedCall")
+    .FrameworkType(ONNX)
+    .OriginOpType("ai.onnx::11::ReduceMax")
+    .ParseParamsFn(ParseParamsReduceMax)
+    .ParseOpToGraphFn(ParseOpToGraphReduceMax)
+    .ImplyType(ImplyType::TVM);
 }  // namespace domi
