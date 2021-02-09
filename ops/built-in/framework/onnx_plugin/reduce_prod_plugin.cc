57ae6448971391a47b7a25041def4bd2a941c60e
镇亮 <zhenliang1@huawei.com>
2021-02-09 10:20:39 +0800

    !3304 【onnx】bugfix(reduceProd) Support input.dtype=Int64 and axes=None Merge pull request !3304 from 镇亮/reduceprod

--- ops/built-in/framework/onnx_plugin/reduce_prod_plugin.cc
+++ ops/built-in/framework/onnx_plugin/reduce_prod_plugin.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+/* Copyright (c) Huawei Technologies Co., Ltd. 2012-2020. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the Apache License Version 2.0.
@@ -17,6 +17,10 @@
 #include "op_log.h"
 #include "proto/onnx/ge_onnx.pb.h"
 #include "register/register.h"
+#include "graph.h"
+#include "all_ops.h"
+
+using namespace ge;
 namespace domi {
 using NodeProto = ge::onnx::NodeProto;
 Status ParseParamsReduceProd(const Message* op_src, ge::Operator& op_dest) {
@@ -26,37 +30,62 @@ Status ParseParamsReduceProd(const Message* op_src, ge::Operator& op_dest) {
     return FAILED;
   }
 
-  std::vector<int> v_axis;
-  bool set_axes_flag = false;
+  std::vector<int> axes = {};
   bool keep_dims = true;
   for (const auto& attr : node->attribute()) {
-    if (attr.name() == "axes" &&
-        attr.type() == ge::onnx::AttributeProto::INTS) {
+    if (attr.name() == "axes" && attr.type() == ge::onnx::AttributeProto::INTS) {
       for (int i = 0; i < attr.ints_size(); i++) {
-        v_axis.push_back(attr.ints(i));
+        axes.push_back(attr.ints(i));
       }
-      set_axes_flag = true;
-    } else if (attr.name() == "keep_dims" &&
-               attr.type() == ge::onnx::AttributeProto::INT) {
-      if (attr.i() != 1) {
-        keep_dims = false;
-      }
+    } else if (attr.name() == "keepdims" && attr.type() == ge::onnx::AttributeProto::INT) {
+      keep_dims = (attr.i() == 1);
     }
   }
-  if (set_axes_flag) {
-    op_dest.SetAttr("axes", v_axis);
-  } else {
-    OP_LOGI("ReduceProd", "onnx ReduceProd op has no axes attr");
-  }
+  int num = axes.size();
+  ge::Shape shape({num});
+  ge::TensorDesc tensorDesc(shape, FORMAT_NCHW, DT_INT32);
+  ge::Tensor tensor(tensorDesc, reinterpret_cast<uint8_t*>(axes.data()), axes.size() * sizeof(DT_INT32));
+
+  op_dest.SetAttr("axes", tensor);
   op_dest.SetAttr("keep_dims", keep_dims);
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op_dest);
+  op_desc->AddDynamicInputDesc("x", 2);
+  op_desc->AddDynamicOutputDesc("y", 1);
 
   return SUCCESS;
 }
 
-// register Abs op info to GE
-REGISTER_CUSTOM_OP("ReduceProdD")
-  .FrameworkType(ONNX)
-  .OriginOpType("ai.onnx::11::ReduceProd")
-  .ParseParamsFn(ParseParamsReduceProd)
-  .ImplyType(ImplyType::TVM);
+static Status ParseOpToGraphReduceProd(const ge::Operator& op, Graph& graph) {
+  auto data0 = op::Data("data0").set_attr_index(0);
+
+  ge::Tensor axes;
+  if (op.GetAttr("axes", axes) != SUCCESS) {
+    OP_LOGE("ReduceProd", "get axes from op failed");
+    return FAILED;
+  }
+  auto data1 = op::Const("data1").set_attr_value(axes);
+  auto reduceprod = op::ReduceProd().set_input_x(data0).set_input_axes(data1);
+
+  bool keep_dims = false;
+  if (op.GetAttr("keep_dims", keep_dims) != SUCCESS) {
+    OP_LOGE("ReduceProd", "get keep_dims from op failed");
+    return FAILED;
+  }
+  reduceprod.set_attr_keep_dims(keep_dims);
+
+  std::vector<ge::Operator> inputs{data0};
+  std::vector<std::pair<ge::Operator, std::vector<size_t> > > outputs;
+  outputs.emplace_back(reduceprod, std::vector<std::size_t>{0});
+  graph.SetInputs(inputs).SetOutputs(outputs);
+
+  return SUCCESS;
+}
+
+// register ReduceProd op info to GE
+REGISTER_CUSTOM_OP("PartitionedCall")
+    .FrameworkType(ONNX)
+    .OriginOpType("ai.onnx::11::ReduceProd")
+    .ParseParamsFn(ParseParamsReduceProd)
+    .ParseOpToGraphFn(ParseOpToGraphReduceProd)
+    .ImplyType(ImplyType::TVM);
 }  // namespace domi
