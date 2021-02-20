652a464dad1dd32a2351f8e8d23ba7554968af53
丁邦林 <dingbanglin@huawei.com>
2021-02-20 15:57:42 +0800

    !3450 maskedscale operator support dynamic Format Merge pull request !3450 from 丁邦林/MaskedScale_0220

--- ops/built-in/tbe/impl/masked_scale.py
+++ ops/built-in/tbe/impl/masked_scale.py
@@ -20,8 +20,146 @@ from te import tvm
 import te.lang.cce as tbe
 from te.utils import para_check
 from te.utils.error_manager import error_manager_vector
+from impl.util.util_select_op_base import gen_param
+from impl.util.util_select_op_base import get_dynamic_param_in_json
 
 
+# op generate_param by dtype and format
+def generate_param(dtypes, formats):
+    """
+    Parameters
+    ----------
+    dtypes: list
+        supported dtype of input and output
+    formats: list
+        supported format of input and output
+    Returns
+    -------
+    None
+    """
+    dtype_x, dtype_mask, dtype_output = dtypes
+    format_x, format_mask, format_output = formats
+
+    input0 = gen_param(classify="input0", name="x",
+                       datatype=",".join(dtype_x),
+                       format=",".join(format_x))
+    input1 = gen_param(classify="input1", name="mask",
+                       datatype=",".join(dtype_mask),
+                       format=",".join(format_mask))
+    output0 = gen_param(classify="output0", name="y",
+                        datatype=",".join(dtype_output),
+                        format=",".join(format_output))
+    return input0, input1, output0
+
+# op get_format_same
+def get_format_same(dtype_list, format_list, dtype_total, mask_dtypes):
+    """
+    Parameters
+    ----------
+    dtype_list: list
+        supported dtype list of x
+    format_list: list
+        supported format list of mask and x
+    mask_dtypes: list
+        supported dtype list of mask
+    dtype_total: list
+        dtype of output and input
+    Returns
+    -------
+    None
+    """
+    for dtype in dtype_list:
+        dtype_total = dtype_total + [dtype] * len(format_list)
+
+    mask_dtype_total = mask_dtypes * len(dtype_total)
+    dtype_total = dtype_total * len(mask_dtypes)
+    format_list = format_list * len(dtype_list) * len(mask_dtypes)
+
+    dtypes = [dtype_total, mask_dtype_total, dtype_total]
+    formats = [format_list, format_list, format_list]
+    return dtypes, formats
+
+# pylint: disable=unused-argument
+def op_select_format(x, mask, y, value=1.0, kernel_name="masked_scale"):
+    """
+    Parameters
+    ----------
+    x: dict
+        shape and dtype of input, only support float16, float32
+    mask: dict
+        shape and dtype of input, only support int8
+    value: scaler
+        dtype is float, default value is 1.0
+    y: dict
+        shape and dtype of output, should be same shape and type as input
+    kernel_name: str
+        kernel name, default value is "masked_scale"
+
+    Returns
+    -------
+    None
+    """
+    shape_x = x.get("ori_shape")
+    shape_mask = mask.get("ori_shape")
+
+    dtype_list = ["float16", "float32"]
+    mask_dtypes = ["float16", "float32", "int8"]
+
+    dtype_total = []
+
+    # if shape is same, then all formats are supported.
+    if list(shape_x) == list(shape_mask):
+        format_list = ["ND", "FRACTAL_NZ", "NC1HWC0", "FRACTAL_Z", "C1HWNCoC0"]
+        dtypes, formats = get_format_same(dtype_list, format_list, dtype_total, mask_dtypes)
+        input0, input1, output0 = generate_param(dtypes, formats)
+
+        param_list = [input0, input1, output0]
+        param_dynamic_in_json = get_dynamic_param_in_json(param_list)
+        return param_dynamic_in_json
+
+    raise RuntimeError("The shape of x and mask must be the same.")
+
+# pylint: disable=unused-argument
+def masked_scale_compute(x, mask, y, value=1.0, kernel_name="masked_scale"):
+    """
+    function: compute of masked_scale
+    Parameters
+    ----------
+    x: tensor
+    mask: tensor
+        shape must be same with x
+    value: scaler
+        dtype is float, default value is 1.0
+    y: dict
+        shape and dtype of output, should be same shape and type as input
+    kernel_name: str
+        kernel name, default value is "masked_scale"
+
+    Returns
+    -------
+    None
+    """
+    # mask_dst_dtype="float16" case cast_to only support int8->float16
+    mask_dtype = mask.dtype
+    if mask_dtype == "int8":
+        mask = tbe.cast_to(mask, dtype="float16")
+        mask = tbe.cast_to(mask, dtype="float32")
+    elif mask_dtype == "float16":
+        mask = tbe.cast_to(mask, dtype="float32")
+
+    if x.dtype != "float32":
+        x = tbe.cast_to(x, dtype="float32")
+
+    res_vmul = tbe.vmul(x, mask)
+    data_value = tvm.const(value, dtype="float32")
+    res = tbe.vmuls(res_vmul, data_value)
+
+    if res.dtype != y.get("dtype"):
+        res = tbe.cast_to(res, dtype=y.get("dtype"))
+
+    return res
+
+# pylint: disable=unused-argument
 @para_check.check_op_params(para_check.REQUIRED_INPUT, para_check.REQUIRED_INPUT,
                             para_check.REQUIRED_OUTPUT, para_check.OPTION_ATTR_FLOAT,
                             para_check.KERNEL_NAME)
@@ -53,41 +191,17 @@ def masked_scale(x, mask, y, value=1.0, kernel_name="masked_scale"):
     mask_shape = mask.get("shape")
     mask_dtype = mask.get("dtype")
 
-    # check_dtype
-    x_dtype_list = ("float16", "float32")
-    para_check.check_dtype(x_dtype, x_dtype_list)
-
-    mask_dtype_list = ("int8", "float16", "float32")
-    para_check.check_dtype(mask_dtype, mask_dtype_list)
-
-    # check_shape
-    para_check.check_shape(x_shape)
-    para_check.check_shape(mask_shape)
-
+    # check shape
     if x_shape != mask_shape:
-       error_manager_vector.raise_err_two_input_shape_invalid("masked_scale", "x", 
-                                                              "mask", "shou1d be same shape")
-
-    # check_kernel_name
-    para_check.check_kernel_name(kernel_name)
+        error_manager_vector.raise_err_two_input_shape_invalid("masked_scale", "x", 
+                                                               "mask", "shou1d be same shape")
 
     # do compute
     data_x = tvm.placeholder(x_shape, name="data_x", dtype=x_dtype)
     data_mask = tvm.placeholder(mask_shape, name="data_mask", dtype=mask_dtype)
-    data_value = tvm.const(value, dtype="float32")
 
-    # mask_dst_dtype="float16" case cast_to only support int8->float16
-    data_mask_0 = data_mask
-    if mask_dtype == "int8":
-        mask_dtype = "float16"
-        data_mask_0 = tbe.cast_to(data_mask, dtype=mask_dtype)
-        
-    data_mask_1 = data_mask_0
-    if x_dtype != mask_dtype:
-       data_mask_1 = tbe.cast_to(data_mask_0, dtype=x_dtype)
+    res = masked_scale_compute(data_x, data_mask, y, value, kernel_name)
 
-    res_vmul = tbe.vmul(data_x, data_mask_1)
-    res = tbe.vmuls(res_vmul, data_value)
     with tvm.target.cce():
         sch = tbe.auto_schedule(res)
 
--- ops/built-in/tests/ut/ops_test/MaskedScale/test_masked_scale_impl.py
+++ ops/built-in/tests/ut/ops_test/MaskedScale/test_masked_scale_impl.py
@@ -4,79 +4,86 @@ from op_test_frame.ut import OpUT
 from op_test_frame.common import precision_info
 ut_case = OpUT("MaskedScale", None, None)
 
+
 #add cases
-def MaskedScaleCases(x_shape, x_dtype, format, mask_shape, mask_dtype,
-                     y_shape, y_dtype, attr_value=1.0, case_name="MaskedScale",
+def MaskedScaleCases(Xshape, Xdtype, input_format, Mshape, Mdtype,
+                     Yshape, Ydtype, attr_value=1.0, case_name="MaskedScale",
                      expect="success"):
-    case = {"params": [{"shape": x_shape, "dtype": x_dtype, "format": format, "ori_shape": x_shape,"ori_format": format},
-                       {"shape": mask_shape, "dtype": mask_dtype, "format": format, "ori_shape": mask_shape,"ori_format": format},
-                       {"shape": y_shape, "dtype": y_dtype, "format": format, "ori_shape": y_shape,"ori_format": format},
+    Case = {"params": [{"shape": Xshape, "dtype": Xdtype, "format": input_format, "ori_shape": Xshape,"ori_format": input_format},
+                       {"shape": Mshape, "dtype": Mdtype, "format": input_format, "ori_shape": Mshape,"ori_format": input_format},
+                       {"shape": Yshape, "dtype": Ydtype, "format": input_format, "ori_shape": Yshape,"ori_format": input_format},
                        attr_value],
             "case_name": case_name,
             "expect": expect,
-            "format_expect": ["ND"],
+            "format_expect": [input_format],
             "support_expect": True}
-    return case
+    return Case
 
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float16", "ND", (125, 125), "int8",
-                                   (125, 125), "float16",
-                                    0.5, "MaskedScale_0", "success"))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float32", "ND", (125, 125), "int8",
-                                   (125, 125), "float32",
-                                    0.5, "MaskedScale_1", "success"))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float16", "ND", (125, 125), "float16",
-                                   (125, 125), "float16",
-                                    0.5, "MaskedScale_2", "success"))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float32", "ND", (125, 125), "float16",
-                                   (125, 125), "float32",
-                                    0.5, "MaskedScale_3", "success"))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float16", "ND", (125, 125), "float32",
-                                   (125, 125), "float16",
-                                    0.5, "MaskedScale_4", "success"))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float32", "ND", (125, 125), "float32",
-                                   (125, 125), "float32",
-                                    0.5, "MaskedScale_5", "success"))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "float16", "ND", (125, 125), "int8",
-                                   (1, 125), "float16",
-                                    0.5, "MaskedScale_6", RuntimeError))
-ut_case.add_case(["Ascend310", "Ascend910A"], 
-                  MaskedScaleCases((125,125), "int8", "ND", (125, 125), "int8",
-                                   (125, 125), "float32",
-                                    0.5, "MaskedScale_7", RuntimeError))
-#precision cases
+# pylint: disable=unused-argument
 def calc_expect_func(x, mask, y, value=1.0):
     input_x_value = x["value"]
     input_x_dtype = x["dtype"]
     input_mask_value = mask["value"]
-
+    
     res = input_x_value * input_mask_value * value
     res = res.astype(input_x_dtype)
     return res
 
-def MaskedScalePrecisions(x_shape, x_dtype, format, mask_shape, mask_dtype,
-                          y_shape, y_dtype, attr_value=1.0):
-    case = {"params": [{"shape": x_shape, "dtype": x_dtype, "format": format, "ori_shape": x_shape,"ori_format": format, "param_type":"input"},
-                       {"shape": mask_shape, "dtype": mask_dtype, "format": format, "ori_shape": mask_shape,"ori_format": format, "param_type":"input"},
-                       {"shape": y_shape, "dtype": y_dtype, "format": format, "ori_shape": y_shape,"ori_format": format, "param_type":"output"},
+# do fill MaskedScalePrecisions param
+def MaskedScalePrecisions(Xshape, Xdtype, Xformat, Mshape, Mdtype,
+                          Yshape, Ydtype, attr_value=1.0, case_name="MaskedScalePrecisions"):
+    Case = {"params": [{"shape": Xshape, "dtype": Xdtype, "format": Xformat, "ori_shape": Xshape,"ori_format": Xformat, "param_type":"input"},
+                       {"shape": Mshape, "dtype": Mdtype, "format": Xformat, "ori_shape": Mshape,"ori_format": Xformat, "param_type":"input"},
+                       {"shape": Yshape, "dtype": Ydtype, "format": Xformat, "ori_shape": Yshape,"ori_format": Xformat, "param_type":"output"},
                        attr_value],
             "expect": "success",
+            "case_name": case_name,
             "calc_expect_func": calc_expect_func,
             "precision_standard": precision_info.PrecisionStandard(0.0001, 0.0001)}
-    return case
+    return Case
 
-ut_case.add_precision_case(["Ascend310", "Ascend910A"], 
-                           MaskedScalePrecisions((125,125), "float32", "ND", (125, 125), "float32",
-                                                 (125, 125), "float32", 0.5))
-ut_case.add_precision_case(["Ascend310", "Ascend910A"], 
-                           MaskedScalePrecisions((125,125), "float16", "ND", (125, 125), "float16",
-                                                 (125, 125), "float16", 0.5))
-ut_case.add_precision_case(["Ascend310", "Ascend910A"], 
-                           MaskedScalePrecisions((125,125), "float16", "ND", (125, 125), "int8",
-                                                 (125, 125), "float16", 0.5))
+# do test UT_AddCase(ut_case):
+format_list = ["ND", "FRACTAL_NZ", "NC1HWC0", "FRACTAL_Z", "C1HWNCoC0"]
+x_dtypes = ["float16","float32"]
+mask_dtypes = ["float16","float32","int8"]
+shape_data = [125,125,4]
+shape_data_ex = [125,125,3]
+for x_dtype in x_dtypes:
+    for mask_dtype in mask_dtypes:
+        for format_item in format_list:
+            case = MaskedScaleCases(shape_data, x_dtype, format_item, shape_data, mask_dtype,
+                                    shape_data, x_dtype,
+                                     0.5, "MaskedScale_0"+x_dtype+mask_dtype+format_item, "success")
+            ut_case.add_case(["Ascend310", "Ascend910A"],case)
+            case = MaskedScaleCases(shape_data, x_dtype, format_item, shape_data, mask_dtype,
+                                    shape_data_ex, x_dtype,
+                                     0.5, "MaskedScale_1"+x_dtype+mask_dtype+format_item, RuntimeError)
+            ut_case.add_case(["Ascend310", "Ascend910A"],case)
+            case = MaskedScaleCases(shape_data, "int8", format_item, shape_data, mask_dtype,
+                                    shape_data, x_dtype,
+                                     0.5, "MaskedScale_2"+x_dtype+mask_dtype+format_item, RuntimeError)
+            ut_case.add_case(["Ascend310", "Ascend910A"],case)
+
+# do test UT_AddPrecisionCase(ut_case)
+for x_dtype_1 in x_dtypes:
+    for mask_dtype_1 in mask_dtypes:
+        # todo: test not support the case
+        if x_dtype_1 == "float16" and mask_dtype_1 == "float32":
+            continue
+        for format_item_1 in format_list:
+            test_case_name = "MaskedScalePrecision_"+x_dtype_1+"_"+mask_dtype_1+"_"+format_item_1
+            case = MaskedScalePrecisions(shape_data, x_dtype_1, format_item_1, shape_data, mask_dtype_1,
+                                         shape_data, x_dtype_1, 0.5, test_case_name)
+            ut_case.add_precision_case(["Ascend310"],case)
+
+# pylint: disable=unused-argument
+def test_op_select_format(test_arg):
+    from impl.masked_scale import op_select_format
+
+    x1 = {"ori_shape":(20, 28, 16, 16)}
+    mask1 = {"ori_shape":(20, 28, 16, 16)}
+    y1 = {"ori_shape":(20, 28, 16, 16)}
+    value1 = 1.0
+    op_select_format(x1, mask1, y1, value1, "masked_scale_0")
+
+ut_case.add_cust_test_func(test_func=test_op_select_format)
