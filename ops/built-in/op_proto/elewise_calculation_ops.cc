0ddc06a9883ad2920b3a741195d1190704bbb6c9
linmeijuan <linmeijuan@huawei.com>
2021-03-04 09:39:58 +0800

    !3582 modify elewise_calculation_ops.cc for add mul pow sub inferdataslice Merge pull request !3582 from linmeijuan/master

--- ops/built-in/op_proto/elewise_calculation_ops.cc
+++ ops/built-in/op_proto/elewise_calculation_ops.cc
@@ -25,6 +25,8 @@
 #include "op_log.h"
 #include "./util/error_util.h"
 #include "graph/utils/node_utils.h"
+#include "register/infer_data_slice_registry.h"
+#include "graph/debug/ge_attr_define.h"
 
 namespace ge {
 bool BroadCastTwoShape(const Operator& op, const ge::Shape& shape_x, const ge::Shape& shape_y,
@@ -103,6 +105,119 @@ IMPLEMT_COMMON_INFERFUNC(OneInOneOutCommonInferShape) {
   return GRAPH_FAILED;
 }
 
+// --------------------------elewise data slice begin--------------------------
+static void InferElewiseTwoInput(vector<vector<int64_t>>& in_data_slice, const vector<vector<int64_t>> out_data_slice,
+                                 const vector<int64_t> in_dims, const vector<int64_t> out_dims) {
+  if (in_dims.size() == out_dims.size()) {
+    for (size_t i = 0; i < in_dims.size(); i++) {
+      if (in_dims[i] == 1) {
+        in_data_slice.push_back({0, 1});
+      } else {
+        in_data_slice.push_back(out_data_slice[i]);
+      }
+    }
+  } else {
+    for (size_t i = 0; i < in_dims.size(); i++) {
+      if (in_dims[i] == 1) {
+        in_data_slice.push_back({0, 1});
+      } else {
+        in_data_slice.push_back(out_data_slice[out_dims.size() - in_dims.size() + i]);
+      }
+    }
+  }
+}
+
+static void InferElewiseTwoInputdif(vector<vector<int64_t>>& in_data_slice, const vector<vector<int64_t>> out_data_slice,
+                                    const vector<int64_t> in_dims, const vector<int64_t> out_dims, const int64_t aixs) {
+  if (in_dims.size() == out_dims.size()) {
+    for (size_t i = 0; i < in_dims.size(); i++) {
+      if (in_dims[i] == 1) {
+        in_data_slice.push_back({0, 1});
+      } else {
+        in_data_slice.push_back(out_data_slice[i]);
+      }
+    }
+  } else if (in_dims.size() == 1) {
+    in_data_slice.push_back({out_data_slice[aixs][0] * 16, out_data_slice[aixs][1] * 16});
+  }
+}
+
+IMPLEMT_COMMON_INFER_DATA_SLICE(ElewiseTwoInputInferDataSlice) {
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  if (!op_desc) {
+    OP_LOGW(op.GetName().c_str(), "GetOpDescFromOperator failed.");
+    return GRAPH_FAILED;
+  }
+
+  auto tensor_desc_in_x1 = op_desc->MutableInputDesc("x1");
+  if (!tensor_desc_in_x1) {
+    OP_LOGW(op.GetName().c_str(), "Get input desc x1 failed.");
+    return GRAPH_FAILED;
+  }
+  auto x1_shape = tensor_desc_in_x1->MutableShape();
+  auto x1_format = tensor_desc_in_x1->GetFormat();
+  std::vector<int64_t> x1_dims = x1_shape.GetDims();
+
+  auto tensor_desc_in_x2 = op_desc->MutableInputDesc("x2");
+  if (!tensor_desc_in_x2) {
+    OP_LOGW(op.GetName().c_str(), "Get input desc x2 failed.");
+    return GRAPH_FAILED;
+  }
+  auto x2_shape = tensor_desc_in_x2->MutableShape();
+  auto x2_format = tensor_desc_in_x2->GetFormat();
+  std::vector<int64_t> x2_dims = x2_shape.GetDims();
+
+  auto tensor_desc_out_y = op_desc->MutableOutputDesc("y");
+  if (!tensor_desc_out_y) {
+    OP_LOGW(op.GetName().c_str(), "Get input desc y failed.");
+    return GRAPH_FAILED;
+  }
+  auto y_shape = tensor_desc_out_y->MutableShape();
+  auto y_format = tensor_desc_out_y->GetFormat();
+  std::vector<int64_t> y_dims = y_shape.GetDims();
+
+  vector<vector<int64_t>> y_data_slice = {};
+  vector<vector<int64_t>> x1_data_slice = {};
+  vector<vector<int64_t>> x2_data_slice = {};
+  if (!ge::AttrUtils::GetListListInt(tensor_desc_out_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice)) {
+    OP_LOGW(op.GetName().c_str(), "no data slice, use default as {}");
+    return GRAPH_FAILED;
+  }
+
+  if (x1_format == x2_format) {
+    InferElewiseTwoInput(x1_data_slice, y_data_slice, x1_dims, y_dims);
+    InferElewiseTwoInput(x2_data_slice, y_data_slice, x2_dims, y_dims);
+  } else {
+    if ((x1_format == FORMAT_NC1HWC0 && x2_format == FORMAT_ND) ||
+        (x1_format == FORMAT_ND && x2_format == FORMAT_NC1HWC0)) {
+      // 5HD+ND
+      InferElewiseTwoInputdif(x1_data_slice, y_data_slice, x1_dims, y_dims, 1);
+      InferElewiseTwoInputdif(x2_data_slice, y_data_slice, x2_dims, y_dims, 1);
+    } else if ((x1_format == FORMAT_FRACTAL_NZ && x2_format == FORMAT_ND) ||
+               (x1_format == FORMAT_ND && x2_format == FORMAT_FRACTAL_NZ)) {
+      // NZ+ND
+      InferElewiseTwoInputdif(x1_data_slice, y_data_slice, x1_dims, y_dims, y_dims.size() - 3);
+      InferElewiseTwoInputdif(x2_data_slice, y_data_slice, x2_dims, y_dims, y_dims.size() - 3);
+    } else if ((x1_format == FORMAT_FRACTAL_Z && x2_format == FORMAT_ND) ||
+               (x1_format == FORMAT_ND && x2_format == FORMAT_FRACTAL_Z)) {
+      // F_Z+ND
+      InferElewiseTwoInputdif(x1_data_slice, y_data_slice, x1_dims, y_dims, 0);
+      InferElewiseTwoInputdif(x2_data_slice, y_data_slice, x2_dims, y_dims, 0);
+    }
+  }
+
+  if (!ge::AttrUtils::SetListListInt(tensor_desc_in_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice)) {
+    OP_LOGW(op.GetName().c_str(), "data slice set failed");
+    return GRAPH_FAILED;
+  }
+  if (!ge::AttrUtils::SetListListInt(tensor_desc_in_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice)) {
+    OP_LOGW(op.GetName().c_str(), "data slice set failed");
+    return GRAPH_FAILED;
+  }
+  return GRAPH_SUCCESS;
+}
+// --------------------------elewise data slice end--------------------------
+
 // ----------------MaximumGrad-------------------
 IMPLEMT_COMMON_INFERFUNC(MaximumGradInferShape) {
   if (InferShapeForMaximumAndMinimum(op)) {
@@ -144,6 +259,7 @@ IMPLEMT_COMMON_INFERFUNC(AddInferShape) {
   return GRAPH_SUCCESS;
 }
 
+INFER_DATA_SLICE_FUNC_REG(Add, ElewiseTwoInputInferDataSlice);
 COMMON_INFER_FUNC_REG(Add, AddInferShape);
 VERIFY_FUNC_REG(Add, AddVerify);
 // ---------------------Add END------------------------
@@ -354,6 +470,7 @@ IMPLEMT_COMMON_INFERFUNC(SubInferShape) {
   return GRAPH_SUCCESS;
 }
 
+INFER_DATA_SLICE_FUNC_REG(Sub, ElewiseTwoInputInferDataSlice);
 COMMON_INFER_FUNC_REG(Sub, SubInferShape);
 // -----------------Sub END-----------------
 
@@ -702,6 +819,7 @@ IMPLEMT_VERIFIER(Mul, MulVerify) {
   return GRAPH_SUCCESS;
 }
 
+INFER_DATA_SLICE_FUNC_REG(Mul, ElewiseTwoInputInferDataSlice);
 COMMON_INFER_FUNC_REG(Mul, TwoInOneOutCommonInferShape);
 VERIFY_FUNC_REG(Mul, MulVerify);
 // ----------------Mul END--------------------
@@ -1902,6 +2020,7 @@ IMPLEMT_COMMON_INFERFUNC(PowInferShape) {
   return GRAPH_SUCCESS;
 }
 
+INFER_DATA_SLICE_FUNC_REG(Pow, ElewiseTwoInputInferDataSlice);
 COMMON_INFER_FUNC_REG(Pow, PowInferShape);
 VERIFY_FUNC_REG(Pow, PowVerify);
 // -------------------Pow END------------------------
--- ops/built-in/tests/ut/ops_test/Add/test_add_proto.cpp
+++ ops/built-in/tests/ut/ops_test/Add/test_add_proto.cpp
@@ -22,7 +22,9 @@
 #include <gtest/gtest.h>
 #include "op_proto_test_util.h"
 #include "elewise_calculation_ops.h"
-
+#include "graph/debug/ge_attr_define.h"
+#include "utils/op_desc_utils.h"
+#include "utils/attr_utils.h"
 class add : public testing::Test {
  protected:
   static void SetUpTestCase() {
@@ -59,3 +61,247 @@ TEST_F(add, add_infer_shape_fp16) {
   };
   EXPECT_EQ(output_shape_range, expected_shape_range);
 }
+
+TEST_F(add, add_data_slice_infer1) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100},{1, 100}, {1, 100}};
+  auto tensor_desc = create_desc_shape_range({16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_NC1HWC0,
+                                             {16, 256, 16, 16},
+                                             ge::FORMAT_NCHW,
+                                             shape_range);
+  op.UpdateInputDesc("x1", tensor_desc);
+  op.UpdateInputDesc("x2", tensor_desc);
+  op.UpdateOutputDesc("y", tensor_desc);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+TEST_F(add, add_data_slice_infer2) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}};
+  auto tensor_desc = create_desc_shape_range({16, 16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_NDC1HWC0,
+                                             {16, 16, 16, 16, 256},
+                                             ge::FORMAT_NDHWC,
+                                             shape_range);
+  op.UpdateInputDesc("x1", tensor_desc);
+  op.UpdateInputDesc("x2", tensor_desc);
+  op.UpdateOutputDesc("y", tensor_desc);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+
+TEST_F(add, add_data_slice_infer4) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}};
+  auto tensor_desc = create_desc_shape_range({16, 16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_FRACTAL_Z,
+                                             {256, 16, 16, 256},
+                                             ge::FORMAT_NHWC,
+                                             shape_range);
+  op.UpdateInputDesc("x1", tensor_desc);
+  op.UpdateInputDesc("x2", tensor_desc);
+  op.UpdateOutputDesc("y", tensor_desc);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+
+TEST_F(add, add_data_slice_infer5) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}};
+  auto tensor_desc = create_desc_shape_range({16, 16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_FRACTAL_Z,
+                                             {256, 256, 16, 16},
+                                             ge::FORMAT_NCHW,
+                                             shape_range);
+  op.UpdateInputDesc("x1", tensor_desc);
+  op.UpdateInputDesc("x2", tensor_desc);
+  op.UpdateOutputDesc("y", tensor_desc);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+
+TEST_F(add, add_data_slice_infer6) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100},
+                                                         {1, 100}};
+  auto tensor_desc = create_desc_shape_range({16, 16, 16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_FRACTAL_Z_3D,
+                                             {256, 16, 16, 16, 256},
+                                             ge::FORMAT_NDHWC,
+                                             shape_range);
+  op.UpdateInputDesc("x1", tensor_desc);
+  op.UpdateInputDesc("x2", tensor_desc);
+  op.UpdateOutputDesc("y", tensor_desc);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+TEST_F(add, add_data_slice_infer7) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}};
+  auto tensor_desc = create_desc_shape_range({16, 16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_FRACTAL_NZ,
+                                             {16, 16, 256, 256},
+                                             ge::FORMAT_ND,
+                                             shape_range);
+  op.UpdateInputDesc("x1", tensor_desc);
+  op.UpdateInputDesc("x2", tensor_desc);
+  op.UpdateOutputDesc("y", tensor_desc);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+TEST_F(add, add_data_slice_infer8) {
+  ge::op::Add op;
+  std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}, {1, 100}};
+  auto tensor_desc1 = create_desc_shape_range({16, 16, 16, 16, 16, 16},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_FRACTAL_NZ,
+                                             {16, 16, 256, 256},
+                                             ge::FORMAT_ND,
+                                             shape_range);
+
+  auto tensor_desc2 = create_desc_shape_range({256},
+                                             ge::DT_FLOAT16,
+                                             ge::FORMAT_ND,
+                                             {256},
+                                             ge::FORMAT_ND,
+                                             {{1, 100}});
+
+  op.UpdateInputDesc("x1", tensor_desc1);
+  op.UpdateInputDesc("x2", tensor_desc2);
+  op.UpdateOutputDesc("y", tensor_desc1);
+
+  std::vector<std::vector<int64_t>> y_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  auto op_desc = ge::OpDescUtils::GetOpDescFromOperator(op);
+  ge::GeTensorDescPtr tensor_desc_y = op_desc->MutableOutputDesc("y");
+  ge::AttrUtils::SetListListInt(tensor_desc_y, ge::ATTR_NAME_DATA_SLICE, y_data_slice);
+  auto status = op_desc->InferDataSlice();
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+
+  auto tensor_desc_x1 = op_desc->MutableInputDesc("x1");
+  auto tensor_desc_x2 = op_desc->MutableInputDesc("x2");
+  std::vector<std::vector<int64_t>> x1_data_slice;
+  std::vector<std::vector<int64_t>> x2_data_slice;
+  ge::AttrUtils::GetListListInt(tensor_desc_x1, ge::ATTR_NAME_DATA_SLICE, x1_data_slice);
+  ge::AttrUtils::GetListListInt(tensor_desc_x2, ge::ATTR_NAME_DATA_SLICE, x2_data_slice);
+  std::vector<std::vector<int64_t>> excepted_x1_data_slice = {{0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}, {0, 4}};
+  std::vector<std::vector<int64_t>> excepted_x2_data_slice = {{0, 64}};
+  EXPECT_EQ(excepted_x1_data_slice, x1_data_slice);
+  EXPECT_EQ(excepted_x2_data_slice, x2_data_slice);
+}
+
+
+
+
+
+
+
+
