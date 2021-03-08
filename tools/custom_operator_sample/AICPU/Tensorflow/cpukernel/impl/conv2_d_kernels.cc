70c4854d02546b7d7f0371a8f30c40d0ecfbe44d
fangfang <fangfang7@huawei.com>
2021-03-08 18:09:54 +0800

    !4016 add operator sample Merge pull request !4016 from fangfang/master

---
+++ tools/custom_operator_sample/AICPU/Tensorflow/cpukernel/impl/conv2_d_kernels.cc
@@ -0,0 +1,19 @@
+
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020. All rights reserved.
+ * Description: implement of Conv2D
+ */
+#include "conv2_d_kernels.h"
+
+namespace  {
+const char *CONV2_D = "Conv2D";
+}
+
+namespace aicpu  {
+uint32_t Conv2DCpuKernel::Compute(CpuKernelContext &ctx)
+{
+    return 0;
+}
+
+REGISTER_CPU_KERNEL(CONV2_D, Conv2DCpuKernel);
+} // namespace aicpu
