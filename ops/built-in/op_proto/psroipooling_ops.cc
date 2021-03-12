a9306c1f540441889fe7d4d90921cbe3dd58b97e
tc123456 <tangchang1@huawei.com>
2021-03-12 16:22:45 +0800

    !4209 增加PSROIPoolingV2正向算子 Merge pull request !4209 from tc123456/master

--- ops/built-in/op_proto/psroipooling_ops.cc
+++ ops/built-in/op_proto/psroipooling_ops.cc
@@ -29,76 +29,139 @@
 namespace ge {
 
 IMPLEMT_INFERFUNC(PSROIPooling, PSROIPoolingInferShape) {
-  auto output_dim = op.get_attr_output_dim();
-  auto group_size = op.get_attr_group_size();
-  // The value of group_size must be less than 128
-  if (group_size <= 0 || group_size >= 128) {
-    OP_LOGE(op.GetName().c_str(), "The value of group_size not support, is %ld", group_size);
-    OpsAttrValueErrReport(op.GetName(), "group_size", "less than 128 and greater than 0", ConcatString(group_size));
-    return GRAPH_FAILED;
-  }
+    auto output_dim = op.get_attr_output_dim();
+    auto group_size = op.get_attr_group_size();
+    // The value of group_size must be less than 128
+    if (group_size <= 0 || group_size >= 128) {
+        OP_LOGE(op.GetName().c_str(), "The value of group_size not support, is %ld", group_size);
+        OpsAttrValueErrReport(op.GetName(), "group_size", "less than 128 and greater than 0", ConcatString(group_size));
+        return GRAPH_FAILED;
+    }
 
-  auto x_shape = op.get_input_desc_x().GetShape();
-  auto x_dtype = op.get_input_desc_x().GetDataType();
-  int64_t pool_h = group_size;
-  int64_t pool_w = group_size;
+    auto x_shape = op.get_input_desc_x().GetShape();
+    auto x_dtype = op.get_input_desc_x().GetDataType();
+    int64_t pool_h = group_size;
+    int64_t pool_w = group_size;
 
-  int64_t c_output_dim = x_shape.GetDim(1) / (group_size * group_size);
-  if (c_output_dim != output_dim) {
-    OP_LOGE(op.GetName().c_str(), "The c of input fm is invalid, is %ld, %ld", x_shape.GetDim(1), output_dim);
-    OpsInputShapeErrReport(op.GetName(), "c_output_dim shoule be equal to output_dim",
-                          "c_output_dim", ConcatString(c_output_dim));
-    return GRAPH_FAILED;
-  }
+    int64_t c_output_dim = x_shape.GetDim(1) / (group_size * group_size);
+    if (c_output_dim != output_dim) {
+        OP_LOGE(op.GetName().c_str(), "The c of input fm is invalid, is %ld, %ld", x_shape.GetDim(1), output_dim);
+        OpsInputShapeErrReport(op.GetName(), "c_output_dim shoule be equal to output_dim", "c_output_dim",
+                                ConcatString(c_output_dim));
+        return GRAPH_FAILED;
+    }
 
-  auto rois_shape = op.get_input_desc_rois().GetShape();
-  int64_t rois_num = rois_shape.GetDim(0) * rois_shape.GetDim(2);
+    auto rois_shape = op.get_input_desc_rois().GetShape();
+    int64_t rois_num = rois_shape.GetDim(0) * rois_shape.GetDim(2);
 
-  vector<int64_t> y_shape({rois_num, output_dim, pool_h, pool_w});
+    vector<int64_t> y_shape({rois_num, output_dim, pool_h, pool_w});
 
-  auto out_desc = op.GetOutputDesc("y");
-  out_desc.SetShape(Shape(y_shape));
-  out_desc.SetDataType(ge::DataType(x_dtype));
-  (void)op.update_output_desc_y(out_desc);
+    auto out_desc = op.GetOutputDesc("y");
+    out_desc.SetShape(Shape(y_shape));
+    out_desc.SetDataType(ge::DataType(x_dtype));
+    (void)op.update_output_desc_y(out_desc);
 
-  return GRAPH_SUCCESS;
+    return GRAPH_SUCCESS;
 }
 
 IMPLEMT_VERIFIER(PSROIPooling, PSROIPoolingVerify) {
-  // input x only support NCHW format
-  auto x_shape = op.get_input_desc_x().GetShape();
-  if (x_shape.GetDims().size() != 4) {
-    OP_LOGE(op.GetName().c_str(),
-            "input x shape must be 4d,"
-            "input x shape size is %d",
-            x_shape.GetDims().size());
-    OpsAttrValueErrReport(op.GetName(), "x_shape's size", "4", ConcatString(x_shape.GetDims().size()));
-    return GRAPH_FAILED;
-  }
+    // input x only support NCHW format
+    auto x_shape = op.get_input_desc_x().GetShape();
+    if (x_shape.GetDims().size() != 4) {
+        OP_LOGE(op.GetName().c_str(),
+                "input x shape must be 4d,"
+                "input x shape size is %d",
+                x_shape.GetDims().size());
+        OpsAttrValueErrReport(op.GetName(), "x_shape's size", "4", ConcatString(x_shape.GetDims().size()));
+        return GRAPH_FAILED;
+    }
 
-  Format x_format = op.get_input_desc_x().GetFormat();
-  if (x_format != FORMAT_NCHW) {
-    OP_LOGE(op.GetName().c_str(), "input x format must be NCHW");
-    OpsInputFormatErrReport(op.GetName(), "x", "NCHW", ConcatString(x_format));
-    return GRAPH_FAILED;
-  }
+    Format x_format = op.get_input_desc_x().GetFormat();
+    if (x_format != FORMAT_NCHW) {
+        OP_LOGE(op.GetName().c_str(), "input x format must be NCHW");
+        OpsInputFormatErrReport(op.GetName(), "x", "NCHW", ConcatString(x_format));
+        return GRAPH_FAILED;
+    }
 
-  // rois shape is (batch, 5, rois_num), shape size is 3
-  auto rois_shape = op.get_input_desc_rois().GetShape();
-  if (rois_shape.GetDims().size() < 3) {
-    OP_LOGE(op.GetName().c_str(),
-            "input rois shape must be equal 3,"
-            "input rois shape size is %d",
-            rois_shape.GetDims().size());
-    OpsAttrValueErrReport(op.GetName(), "rois_shape's size", "3",
-                          ConcatString(rois_shape.GetDims().size()));
-    return GRAPH_FAILED;
-  }
+    // rois shape is (batch, 5, rois_num), shape size is 3
+    auto rois_shape = op.get_input_desc_rois().GetShape();
+    if (rois_shape.GetDims().size() < 3) {
+        OP_LOGE(op.GetName().c_str(),
+                "input rois shape must be equal 3,"
+                "input rois shape size is %d",
+                rois_shape.GetDims().size());
+        OpsAttrValueErrReport(op.GetName(), "rois_shape's size", "3", ConcatString(rois_shape.GetDims().size()));
+        return GRAPH_FAILED;
+    }
 
-  return GRAPH_SUCCESS;
+    return GRAPH_SUCCESS;
 }
 
 INFER_FUNC_REG(PSROIPooling, PSROIPoolingInferShape);
 VERIFY_FUNC_REG(PSROIPooling, PSROIPoolingVerify);
+
+IMPLEMT_INFERFUNC(PSROIPoolingV2, PSROIPoolingV2InferShape) {
+    auto output_dim = op.get_attr_output_dim();
+    auto group_size = op.get_attr_group_size();
+    // The value of group_size must be less than 128
+    if (group_size <= 0 || group_size >= 128) {
+        OP_LOGE(op.GetName().c_str(), "The value of group_size not support, is %ld", group_size);
+        OpsAttrValueErrReport(op.GetName(), "group_size", "less than 128 and greater than 0", ConcatString(group_size));
+        return GRAPH_FAILED;
+    }
+
+    auto x_shape = op.GetInputDesc("x").GetShape();
+    auto x_dtype = op.GetInputDesc("x").GetDataType();
+    int64_t pool_h = group_size;
+    int64_t pool_w = group_size;
+    int64_t c_output_dim = x_shape.GetDim(1) / (group_size * group_size);
+    if (c_output_dim != output_dim) {
+        OP_LOGE(op.GetName().c_str(), "The c of input fm is invalid, is %ld, %ld", x_shape.GetDim(1), output_dim);
+        OpsInputShapeErrReport(op.GetName(), "c_output_dim shoule be equal to output_dim", "c_output_dim",
+                            ConcatString(c_output_dim));
+        return GRAPH_FAILED;
+    }
+
+    auto rois_shape = op.get_input_desc_rois().GetShape();
+    int64_t rois_num = rois_shape.GetDim(0) * rois_shape.GetDim(2);
+    vector<int64_t> y_shape({rois_num, output_dim, pool_h, pool_w});
+
+    auto out_desc = op.GetOutputDesc("y");
+    out_desc.SetShape(Shape(y_shape));
+    out_desc.SetDataType(ge::DataType(x_dtype));
+    (void)op.update_output_desc_y(out_desc);
+
+    return GRAPH_SUCCESS;
+}
+
+IMPLEMT_VERIFIER(PSROIPoolingV2, PSROIPoolingV2Verify) {
+    // input x only support NCHW format
+    auto x_shape = op.get_input_desc_x().GetShape();
+    if (x_shape.GetDims().size() != 4) {
+        OP_LOGE(op.GetName().c_str(), "input x shape must be 4d, input x shape size is %d", x_shape.GetDims().size());
+        OpsAttrValueErrReport(op.GetName(), "x_shape's size", "4", ConcatString(x_shape.GetDims().size()));
+        return GRAPH_FAILED;
+    }
+
+    Format x_format = op.get_input_desc_x().GetFormat();
+    if (x_format != FORMAT_NCHW) {
+        OP_LOGE(op.GetName().c_str(), "input x format must be NCHW");
+        OpsInputFormatErrReport(op.GetName(), "x", "NCHW", ConcatString(x_format));
+        return GRAPH_FAILED;
+    }
+
+    // rois shape is (batch, 5, rois_num), shape size is 3
+    auto rois_shape = op.get_input_desc_rois().GetShape();
+    if (rois_shape.GetDims().size() < 3) {
+        OP_LOGE(op.GetName().c_str(), "input rois shape must be equal 3, input rois shape size is %d",
+                rois_shape.GetDims().size());
+        OpsAttrValueErrReport(op.GetName(), "rois_shape's size", "3", ConcatString(rois_shape.GetDims().size()));
+        return GRAPH_FAILED;
+    }
+    return GRAPH_SUCCESS;
+}
+
+INFER_FUNC_REG(PSROIPoolingV2, PSROIPoolingV2InferShape);
+VERIFY_FUNC_REG(PSROIPoolingV2, PSROIPoolingV2Verify);
 
 }  // namespace ge
---
+++ ops/built-in/tests/ut/ops_test/PSROIPoolingV2/test_p_s_r_o_i_pooling_v2_proto.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include "op_proto_test_util.h"
+#include "nn_detect_ops.h"
+
+class PSROIPoolingV2ProtoTest : public testing::Test {
+protected:
+    static void SetUpTestCase() {
+        std::cout << "PSROIPoolingV2 Proto Test SetUp" << std::endl;
+    }
+
+    static void TearDownTestCase() {
+        std::cout << "PSROIPoolingV2 Proto Test TearDown" << std::endl;
+    }
+};
+
+TEST_F(PSROIPoolingV2ProtoTest, p_s_r_o_i_pooling_v2_test_case_1) {
+    ge::op::PSROIPoolingV2 op;
+
+    ge::TensorDesc tensor_x_desc;
+    ge::Shape x_shape({2, 16*7*7, 7, 7});
+    tensor_x_desc.SetDataType(ge::DT_FLOAT16);
+    tensor_x_desc.SetShape(x_shape);
+    op.UpdateInputDesc("x", tensor_x_desc);
+
+    ge::TensorDesc tensor_rois;
+    ge::Shape roi_shape({2, 5, 16});
+    tensor_rois.SetDataType(ge::DT_FLOAT16);
+    tensor_rois.SetShape(roi_shape);
+    op.UpdateInputDesc("rois", tensor_rois);
+
+    op.SetAttr("output_dim", 16);
+    op.SetAttr("group_size", 7);
+    op.SetAttr("spatial_scale", 0.0625f);
+
+    auto ret = op.InferShapeAndType();
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+    auto output_desc = op.GetOutputDesc("y");
+    EXPECT_EQ(output_desc.GetDataType(), ge::DT_FLOAT16);
+    std::vector<int64_t> expected_output_shape = {32, 16, 7, 7};
+    EXPECT_EQ(output_desc.GetShape().GetDims(), expected_output_shape);
+}
+
+TEST_F(PSROIPoolingV2ProtoTest, p_s_r_o_i_pooling_v2_test_case_2) {
+    ge::op::PSROIPoolingV2 op;
+
+    ge::TensorDesc tensor_x_desc;
+    ge::Shape shape({2, 2*7*7, 7, 7});
+    tensor_x_desc.SetDataType(ge::DT_FLOAT);
+    tensor_x_desc.SetShape(shape);
+    op.UpdateInputDesc("x", tensor_x_desc);
+
+    ge::TensorDesc tensor_rois;
+    ge::Shape roi_shape({2, 5, 16});
+    tensor_rois.SetDataType(ge::DT_FLOAT);
+    tensor_rois.SetShape(roi_shape);
+    op.UpdateInputDesc("rois", tensor_rois);
+
+    op.SetAttr("output_dim", 2);
+    op.SetAttr("group_size", 7);
+    op.SetAttr("spatial_scale", 0.0625f);
+
+    auto ret = op.InferShapeAndType();
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+    auto output_desc = op.GetOutputDesc("y");
+    EXPECT_EQ(output_desc.GetDataType(), ge::DT_FLOAT);
+    std::vector<int64_t> expected_output_shape = {32, 2, 7, 7};
+    EXPECT_EQ(output_desc.GetShape().GetDims(), expected_output_shape);
+}
