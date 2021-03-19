b1023e9838df760a59fe823799938b46ad0d1054
周奇龙 <zhouqilong2@huawei.com>
2021-03-19 09:40:37 +0800

    !4434 bugfix for dynamic shape of concat when format is 5hd Merge pull request !4434 from 周奇龙/master

--- ops/built-in/tbe/impl/dynamic/concat_v2_d.py
+++ ops/built-in/tbe/impl/dynamic/concat_v2_d.py
@@ -1325,6 +1325,9 @@ def concat_v2_d(input_values, output_data, axis, kernel_name="concat_v2_d"):
     -------
     tik instance
     """
+    if not util_common.is_unknown_rank_input(input_values):
+        _check_params(input_values, axis)
+
     # update axis base on input format
     for _, _input_dict in enumerate(input_values):
         ori_shape = _input_dict.get("ori_shape")
@@ -1334,9 +1337,6 @@ def concat_v2_d(input_values, output_data, axis, kernel_name="concat_v2_d"):
             ori_format = _input_dict.get("ori_format")
             axis = util_common.update_axis_for_other_format(ori_shape, axis, input_format, ori_format)
             break
-
-    if not util_common.is_unknown_rank_input(input_values):
-        _check_params(input_values, axis)
 
     concat_instance = ConcatV2(input_values, axis, kernel_name)
     return concat_instance.concat_compute()
