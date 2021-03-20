ef5da82783873263a5e819e7222b24397db414ce
wangyu_ai <wangyu_ai2012@163.com>
2021-03-20 13:54:55 +0800

    !4439 commit vmul ut Merge pull request !4439 from wangyu_ai/master

---
+++ auto_schedule/python/tests/ut/vmul/test_dynamic_vmul_impl.py
@@ -0,0 +1,99 @@
+# # -*- coding:utf-8 -*-
+from sch_test_frame.ut import OpUT
+from sch_test_frame.utils.op_param_util import cartesian_set_format_dtype
+from sch_test_frame.common import precision_info
+import numpy as np
+
+import tbe
+from tbe import tvm
+from tbe.common.utils import shape_util
+from tbe.common.register import register_operator
+
+
+@register_operator("vmul")
+def dsl_dynamic_vmul(x, y, z, kernel_name="dsl_dynamic_vmul"):
+    input_dtype = x.get("dtype")
+
+    ins = tbe.dsl.classify([x, y], "elewise")
+    schedules, tensors = [], []
+
+    for (x, y) in ins:
+        with tbe.dsl.compute():
+            shape_x, shape_y = shape_util.variable_shape([x, y])
+            data1 = tvm.placeholder(shape_x, name='data1', dtype=input_dtype)
+            data2 = tvm.placeholder(shape_y, name='data2', dtype=input_dtype)
+            res = tbe.dsl.vmul(data1, data2)
+
+            tensors.append((data1, data2, res))
+
+        with tvm.target.cce():
+            sch = tbe.dsl.auto_schedule(res)
+        schedules.append(sch)
+
+    config = {"name": kernel_name, "tensor_list": tensors}
+    tbe.dsl.build(schedules, config)
+
+
+ut_case = OpUT("vmul", "vmul.test_dynamic_vmul_impl", "dsl_dynamic_vmul")
+
+case1 = {
+    "params": [{
+        "shape": (-1, -1),
+        "dtype": "float32",
+        "range": [(1, None), (1, None)]
+    }, {
+        "shape": (-1, -1),
+        "dtype": "float32",
+        "range": [(1, None), (1, None)]
+    }, {
+        "shape": (-1, -1),
+        "dtype": "float32",
+        "range": [(1, None), (1, None)]
+    }],
+    "case_name":
+        "test_dync_vmul_1",
+    "expect":
+        "success",
+    "support_expect":
+        True
+}
+
+ut_case.add_case(["Ascend910", "Ascend310", "Ascend710"], case1)
+
+
+def calc_expect_func(x, y, z):
+    x_value = x.get("value")
+    y_value = y.get("value")
+    res = np.multiply(x_value, y_value)
+    return (res, )
+
+
+ut_case.add_precision_case(
+    "all", {
+        "params": [
+            {
+                "shape": (-1, -1),
+                "dtype": "float32",
+                "range": [(1, 200), (1, 100)],
+                "run_shape": (2, 10),
+                "param_type": "input"
+            },
+            {
+                "shape": (-1, -1),
+                "dtype": "float32",
+                "range": [(1, 200), (1, 100)],
+                "run_shape": (2, 10),
+                "param_type": "input"
+            },
+            {
+                "shape": (-1, -1),
+                "dtype": "float32",
+                "range": [(1, 200), (1, 100)],
+                "run_shape": (2, 10),
+                "param_type": "output"
+            },
+        ],
+        "calc_expect_func": calc_expect_func,
+        "precision_standard": precision_info.PrecisionStandard(0.001, 0.001),
+        "case_name": "test_dync_vmul_prec_01"
+    })
