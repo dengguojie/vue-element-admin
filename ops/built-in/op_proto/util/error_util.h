c478ee839e0ea76bd42f335d277d7654bceef739
傅煌培 <fuhuangpei@hisilicon.com>
2021-04-12 17:35:34 +0800

    !5451 fix the bug of error report func Merge pull request !5451 from 傅煌培/fix_error_func_bug

--- ops/built-in/op_proto/util/error_util.h
+++ ops/built-in/op_proto/util/error_util.h
@@ -53,29 +53,25 @@
 #define CUBE_INNER_ERR_REPORT(op_name, err_msg, ...) \
   do { \
       OP_LOGE(op_name, err_msg, ##__VA_ARGS__); \
-      REPORT_INNER_ERROR(ge::GetViewErrorCodeStr(ge::ViewErrorCode::CUBE_INNER_ERROR), \
-       "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
+      REPORT_INNER_ERROR("E69999", "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
   } while(0)
 
 #define CUBE_CALL_ERR_REPORT(op_name, err_msg, ...) \
   do { \
       OP_LOGE(op_name, err_msg, ##__VA_ARGS__); \
-      REPORT_CALL_ERROR(ge::GetViewErrorCodeStr(ge::ViewErrorCode::CUBE_INNER_ERROR), \
-       "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
+      REPORT_CALL_ERROR("E69999", "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
   } while(0)
 
 #define CUBE_INNER_ERR_REPORT_PLUGIN(op_name, err_msg, ...) \
   do { \
       OP_LOGE(op_name, err_msg, ##__VA_ARGS__); \
-      REPORT_INNER_ERROR(ge::GetViewErrorCodeStr(ge::ViewErrorCode::CUBE_INNER_ERROR_PLUGIN), \
-       "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
+      REPORT_INNER_ERROR("E59999", "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
   } while(0)
 
 #define CUBE_CALL_ERR_REPORT_PLUGIN(op_name, err_msg, ...) \
   do { \
       OP_LOGE(op_name, err_msg, ##__VA_ARGS__); \
-      REPORT_CALL_ERROR(ge::GetViewErrorCodeStr(ge::ViewErrorCode::CUBE_INNER_ERROR_PLUGIN), \
-       "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
+      REPORT_CALL_ERROR("E59999", "op[%s], " err_msg, op_name, ##__VA_ARGS__); \
   } while(0)
 
 namespace ge {
