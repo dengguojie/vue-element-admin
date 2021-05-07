9bf007e5e432b0276fcbd00253ac4a184449ef8d
刘哲续 <liuzhexu@huawei.com>
2021-05-07 20:25:31 +0800

    !6214 Sort增强 Merge pull request !6214 from 刘哲续/master

--- ops/built-in/tests/ut/ops_test/Sort/test_Sort_impl.py
+++ ops/built-in/tests/ut/ops_test/Sort/test_Sort_impl.py
@@ -31,6 +31,31 @@ ut_case.add_case("all", {
     "format_expect": ["ND"],
     "support_expect": True})
 
+ut_case.add_case("all", {
+    "params": [{"dtype": "float16", "format": "ND", "ori_format": "ND", "ori_shape": (2, 50000), "shape": (2, 50000),
+                "param_type": "input"},
+               {"dtype": "float16", "format": "ND", "ori_format": "ND", "ori_shape": (2, 50000), "shape": (2, 50000),
+                "param_type": "output"},
+               {"dtype": "int32", "format": "ND", "ori_format": "ND", "ori_shape": (2, 50000), "shape": (2, 50000),
+                "param_type": "output"},
+               -1, True],
+    "case_name": "test1",
+    "expect": "success",
+    "format_expect": ["ND"],
+    "support_expect": True})
+
+ut_case.add_case("all", {
+    "params": [{"dtype": "float16", "format": "ND", "ori_format": "ND", "ori_shape": (2, 3, 4, 10, 32, 8, 3),
+               "shape": (2, 3, 4, 10, 32, 8, 3), "param_type": "input"},
+               {"dtype": "float16", "format": "ND", "ori_format": "ND", "ori_shape": (2, 3, 4, 10, 32, 8, 3),
+               "shape": (2, 3, 4, 10, 32, 8, 3), "param_type": "input"},
+               {"dtype": "int32", "format": "ND", "ori_format": "ND", "ori_shape": (2, 3, 4, 10, 32, 8, 3),
+               "shape": (2, 3, 4, 10, 32, 8, 3), "param_type": "output"},
+               -1, True],
+    "case_name": "test1",
+    "expect": "success",
+    "format_expect": ["ND"],
+    "support_expect": True})
 
 if __name__ == '__main__':
     ut_case.run("Ascend910A")
--- ops/built-in/tests/ut/ops_test/Sort/test_Sort_proto.cpp
+++ ops/built-in/tests/ut/ops_test/Sort/test_Sort_proto.cpp
@@ -79,3 +79,25 @@ TEST_F(SortTest, Sort_test_2){
   EXPECT_EQ(output_desc_1.GetShape().GetDims(), expected_output_shape);
   EXPECT_EQ(output_desc_2.GetShape().GetDims(), expected_output_shape);
 }
+
+TEST_F(SortTest, Sort_test_3){
+  ge::op::Sort op;
+  ge::TensorDesc tensorDesc;
+  ge::Shape shape({20000});
+  tensorDesc.SetDataType(ge::DT_FLOAT16);
+  tensorDesc.SetShape(shape);
+  
+  op.UpdateInputDesc("x",tensorDesc);
+  
+  auto ret = op.InferShapeAndType();
+  EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+  auto output_desc_1 = op.GetOutputDesc("y1");
+  auto output_desc_2 = op.GetOutputDesc("y2");
+  EXPECT_EQ(output_desc_1.GetDataType(), ge::DT_FLOAT16);
+  EXPECT_EQ(output_desc_2.GetDataType(), ge::DT_INT32);
+  
+  std::vector<int64_t> expected_output_shape = {20000};
+  EXPECT_EQ(output_desc_1.GetShape().GetDims(), expected_output_shape);
+  EXPECT_EQ(output_desc_2.GetShape().GetDims(), expected_output_shape);
+}
