3ee6775a14df26e42a3f88ae7e3bb47282a1eda4
xieyang_auto <xieyang_auto@163.com>
2021-05-11 20:59:17 +0800

    !6345 回退 'Pull Request !5743 : gatherv2算子动静合一，优化数据搬运策略' Merge pull request !6345 from xieyang_auto/revert-merge-5743-master

--- ops/built-in/tests/ut/ops_test/GatherV2/test_Dynamic_GatherV2_impl.py
+++ ops/built-in/tests/ut/ops_test/GatherV2/test_Dynamic_GatherV2_impl.py
@@ -21,30 +21,6 @@ from op_test_frame.ut import OpUT
 ut_case = OpUT("GatherV2", "impl.dynamic.gather_v2", "gather_v2")
 
 
-def test_op_check_supported(test_arg):
-    from impl.dynamic.gather_v2 import check_supported_with_reason
-    check_supported_with_reason({"shape": [20, 28], "dtype": "int8", "format": "ND", "ori_shape": [20, 28], "ori_format": "ND"},
-                     {"shape": [200], "dtype": "int32", "format": "ND", "ori_shape": [200], "ori_format": "ND"},
-                     {"shape": [1], "dtype": "int32", "format": "NCHW", "ori_shape": [1], "ori_format": "NCHW"},
-                     {"shape": [200, 28], "dtype": "int8", "format": "NCHW", "ori_shape": [200, 28],"ori_format": "ND"})
-    check_supported_with_reason({"shape": [20], "dtype": "float16", "format": "ND", "ori_shape": [-2], "ori_format": "ND"},
-                     {"shape": [10], "dtype": "int32", "format": "ND", "ori_shape": [10], "ori_format": "ND"},
-                     {"shape": [1], "dtype": "int32", "format": "NCHW", "ori_shape": [1], "ori_format": "NCHW"},
-                     {"shape": [10], "dtype": "float16", "format": "NCHW", "ori_shape": [10], "ori_format": "ND"})
-    check_supported_with_reason({"shape": [30, 5, 61], "dtype": "int32", "format": "ND", "ori_shape": [30, 5, 61],
-                     "ori_format": "ND"},
-                     {"shape": [10], "dtype": "int32", "format": "ND", "ori_shape": [-2], "ori_format": "ND"},
-                     {"shape": [1], "dtype": "int32", "format": "NCHW", "ori_shape": [1], "ori_format": "NCHW"},
-                     {"shape": [10, 5, 61], "dtype": "int32", "format": "NCHW", "ori_shape": [10, 5, 61],
-                      "ori_format": "ND"})
-    check_supported_with_reason({"shape": [10, 1, 28, 16], "dtype": "int64", "format": "ND", "ori_shape": [10, 1, 28, 16],
-                     "ori_format": "ND"},
-                     {"shape": [10], "dtype": "int32", "format": "ND", "ori_shape": [10], "ori_format": "ND"},
-                     {"shape": [1], "dtype": "int32", "format": "NCHW", "ori_shape": [-2], "ori_format": "NCHW"},
-                     {"shape": [10, 1, 28, 16], "dtype": "int64", "format": "NCHW", "ori_shape": [10, 1, 28, 16],
-                      "ori_format": "ND"})
-
-
 def gen_dynamic_gather_v2_case(dict_params, dict_indices, dict_axis, dict_y, kernel_name_val, expect):
     return {"params": [dict_params, dict_indices, dict_axis, dict_y],
             "case_name": kernel_name_val,
@@ -52,7 +28,7 @@ def gen_dynamic_gather_v2_case(dict_params, dict_indices, dict_axis, dict_y, ker
             "support_expect": True}
 
 
-ut_case.add_case(["Ascend910A"],
+ut_case.add_case(["Ascend910A", "Ascend310", "Ascend710"],
                  gen_dynamic_gather_v2_case(
                      {"shape": (163623, 80), "dtype": "float32", "ori_shape": (163623, 80),
                       "format": "ND", "ori_format": "ND", "range": ((163623, 163623), (80, 80))},
@@ -64,7 +40,7 @@ ut_case.add_case(["Ascend910A"],
                       "format": "ND", "ori_format": "ND", "range": ((22551, 22551), (80, 80))},
                      "dynamic_gather_v2_01", "success"))
 
-ut_case.add_case(["Ascend910A"],
+ut_case.add_case(["Ascend910A", "Ascend310", "Ascend710"],
                  gen_dynamic_gather_v2_case(
                      {"shape": (163623, 1), "dtype": "float32", "ori_shape": (163623, 1),
                       "format": "ND", "ori_format": "ND", "range": ((163623, 163623), (1, 1))},
@@ -76,7 +52,7 @@ ut_case.add_case(["Ascend910A"],
                       "format": "ND", "ori_format": "ND", "range": ((22551, 22551), (1, 1))},
                      "dynamic_gather_v2_02", "success"))
 
-ut_case.add_case(["Ascend910A"],
+ut_case.add_case(["Ascend910A", "Ascend310", "Ascend710"],
                  gen_dynamic_gather_v2_case(
                      {"shape": (163623, 1), "dtype": "float16", "ori_shape": (163623, 1),
                       "format": "ND", "ori_format": "ND", "range": ((163623, 163623), (1, 1))},
@@ -88,7 +64,7 @@ ut_case.add_case(["Ascend910A"],
                       "format": "ND", "ori_format": "ND", "range": ((22551, 22551), (1, 1))},
                      "dynamic_gather_v2_03", "success"))
 
-ut_case.add_case(["Ascend910A"],
+ut_case.add_case(["Ascend910A", "Ascend310", "Ascend710"],
                  gen_dynamic_gather_v2_case(
                      {"shape": (163623, 1), "dtype": "int32", "ori_shape": (163623, 1),
                       "format": "ND", "ori_format": "ND", "range": ((163623, 163623), (1, 1))},
@@ -100,7 +76,7 @@ ut_case.add_case(["Ascend910A"],
                       "format": "ND", "ori_format": "ND", "range": ((22551, 22551), (1, 1))},
                      "dynamic_gather_v2_04", "success"))
 
-ut_case.add_case(["Ascend910A"],
+ut_case.add_case("all",
                  gen_dynamic_gather_v2_case(
                      {"shape": (-1, -1), "dtype": "float32", "ori_shape": (-1, -1),
                       "format": "ND", "ori_format": "ND", "range": ((163623, 163623), (1, 1))},
@@ -126,6 +102,5 @@ ut_case.add_case("all",
                      "dynamic_gather_v2_06", RuntimeError))
 
 
-ut_case.add_cust_test_func(test_func=test_op_check_supported)
 if __name__ == '__main__':
     ut_case.run("Ascend910A")
