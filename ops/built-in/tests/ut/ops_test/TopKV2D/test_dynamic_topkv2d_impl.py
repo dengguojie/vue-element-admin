b600dfa0a08a1a9628da21b3deaf55b34d9bc3de
Ziheng Liu <liuziheng@huawei.com>
2021-06-15 23:33:40 +0800

    !7349 topk tiling add log Merge pull request !7349 from Ziheng Liu/tiling_upload

--- ops/built-in/tests/ut/ops_test/TopKV2D/test_dynamic_topkv2d_impl.py
+++ ops/built-in/tests/ut/ops_test/TopKV2D/test_dynamic_topkv2d_impl.py
@@ -30,8 +30,20 @@ case1 = {"params": [{"shape": (1, 16), "dtype": "float16", "ori_shape": (1, 16),
          "expect": "success",
          "support_expect": True}
 
+case2 = {"params": [{"shape": (1, 160000), "dtype": "float16", "ori_shape": (1, 160000), "format": "ND", "ori_format": "ND", "range": ((1, 1), (160000, 160000))},
+                    {"shape": (1,), "dtype": "int32", "format": "ND", "ori_shape": (1,),"ori_format": "ND","range":((1,1),)},
+                    {"shape": (8192,), "dtype": "float16", "format": "ND", "ori_shape": (8192,),"ori_format": "ND", "range": ((8192,8192),)},
+                    {"shape": (1,-1), "dtype": "float16", "format": "ND", "ori_shape": (1,-1),"ori_format": "ND", "range": ((1, 1), (1, 160000))},
+                    {"shape": (1,-1), "dtype": "int32", "format": "ND", "ori_shape": (1,-1),"ori_format": "ND", "range": ((1, 1), (160000, 160000))},
+                    True,-1,True],
+         "case_name": "TopKV2D_1",
+         "expect": "success",
+         "support_expect": True}
 # TODO fix me, this comment, run failed
 ut_case.add_case(["Ascend910A"], case1)
+ut_case.add_case(["Ascend910A"], case2)
+
+
 
 if __name__ == '__main__':
     ut_case.run(["Ascend910A"])
