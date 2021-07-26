a48bcfd7fd47019f9266e7e88a24e787b14f876c
李博涛 <libotao5@hisilicon.com>
2021-07-26 09:08:59 +0000

    !8536 增加UT测试复数结果对比函数 Merge pull request !8536 from 李博涛/master

--- ops/built-in/tests/utils/aicpu_test_utils.cpp
+++ ops/built-in/tests/utils/aicpu_test_utils.cpp
@@ -15,6 +15,8 @@
  */
 #include "aicpu_test_utils.h"
 
+using namespace std;
+
 uint64_t CalTotalElements(std::vector<std::vector<int64_t>> &shapes,
                           uint32_t index) {
   if(index < 0) {
@@ -61,6 +63,70 @@ bool CompareResult(double output[], double expect_output[], uint64_t num) {
       relative_error = absolute_error / std::fabs(expect_output[i]);
     }
     if ((absolute_error > 1e-12) && (relative_error > 1e-10)) {
+      std::cout << "output[" << i << "] = ";
+      std::cout << output[i];
+      std::cout << ", expect_output[" << i << "] = ";
+      std::cout << expect_output[i] << std::endl;
+      result = false;
+    }
+  }
+  return result;
+}
+
+template <>
+bool CompareResult(complex<float> output[], complex<float> expect_output[], uint64_t num) {
+  bool result = true;
+  for (uint64_t i = 0; i < num; ++i) {
+    double absolute_real_error = std::fabs(output[i].real() - expect_output[i].real());
+    double relative_real_error = 0;
+    if (expect_output[i].real() == 0) {
+      relative_real_error = 2e-6;
+    } else {
+      relative_real_error = absolute_real_error / std::fabs(expect_output[i].real());
+    }
+
+    double absolute_imag_error = std::fabs(output[i].imag() - expect_output[i].imag());
+    double relative_imag_error = 0;
+    if (expect_output[i].imag() == 0) {
+      relative_imag_error = 2e-6;
+    } else {
+      relative_imag_error = absolute_imag_error / std::fabs(expect_output[i].imag());
+    }
+
+    if ((absolute_real_error > 1e-6) && (relative_real_error > 1e-6) ||
+        (absolute_imag_error > 1e-6) && (relative_imag_error > 1e-6)) {
+      std::cout << "output[" << i << "] = ";
+      std::cout << output[i];
+      std::cout << ", expect_output[" << i << "] = ";
+      std::cout << expect_output[i] << std::endl;
+      result = false;
+    }
+  }
+  return result;
+}
+
+template <>
+bool CompareResult(complex<double> output[], complex<double> expect_output[], uint64_t num) {
+  bool result = true;
+  for (uint64_t i = 0; i < num; ++i) {
+    double absolute_real_error = std::fabs(output[i].real() - expect_output[i].real());
+    double relative_real_error = 0;
+    if (expect_output[i].real() == 0) {
+      relative_real_error = 2e-10;
+    } else {
+      relative_real_error = absolute_real_error / std::fabs(expect_output[i].real());
+    }
+
+    double absolute_imag_error = std::fabs(output[i].imag() - expect_output[i].imag());
+    double relative_imag_error = 0;
+    if (expect_output[i].imag() == 0) {
+      relative_imag_error = 2e-10;
+    } else {
+      relative_imag_error = absolute_imag_error / std::fabs(expect_output[i].imag());
+    }
+
+    if ((absolute_real_error > 1e-12) && (relative_real_error > 1e-12) ||
+        (absolute_imag_error > 1e-12) && (relative_imag_error > 1e-12)) {
       std::cout << "output[" << i << "] = ";
       std::cout << output[i];
       std::cout << ", expect_output[" << i << "] = ";
