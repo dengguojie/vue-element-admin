755bb9a6c5edbdb9613a98a8203ca534227c809f
Ziheng Liu <liuziheng@huawei.com>
2021-08-16 15:45:11 +0000

    !9158 fix mul format origin Merge pull request !9158 from Ziheng Liu/mul_format_fix2

--- ops/built-in/tbe/impl/mul.py
+++ ops/built-in/tbe/impl/mul.py
@@ -606,7 +606,7 @@ def op_select_format(x, y, output, kernel_name="mul"):
                                                 format=",".join(y_format_list))
 
     # ND+ND NZ+NZ 5HD+5HD FZ+FZ
-    if len(shape_x) >= 2 and len(shape_y) >= 2:
+    elif len(shape_x) >= 2 and len(shape_y) >= 2:
         if shape_x[-2:] == shape_y[-2:]:
             format_list.append("FRACTAL_NZ")
         if len(shape_x) == 4 and len(shape_y) == 4 and format_x in format_4d_list and format_y in format_4d_list:
