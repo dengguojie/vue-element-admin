dca2618aeda1a7c8b80dc1b6c79cf1e4e60c26e8
黄强 <huangqiang3@huawei.com>
2021-09-14 07:16:33 +0000

    !9999 UT公共文件不include tiling头文件 Merge pull request !9999 from 黄强/op_ut_test

--- ops/built-in/tests/common/utils/ut_op_util.cpp
+++ ops/built-in/tests/common/utils/ut_op_util.cpp
@@ -22,6 +22,25 @@
 
 namespace ut_util {
 
+const std::map<std::string, DataType> STR_TO_DATATYPE = {{"float", DT_FLOAT},
+                                                         {"float32", DT_FLOAT},
+                                                         {"float16", DT_FLOAT16},
+                                                         {"int8", DT_INT8},
+                                                         {"int16", DT_INT16},
+                                                         {"int32", DT_INT32},
+                                                         {"int64", DT_INT64},
+                                                         {"uint8", DT_UINT8},
+                                                         {"uint16", DT_UINT16},
+                                                         {"uint32", DT_UINT32},
+                                                         {"uint64", DT_UINT64},
+                                                         {"bool", DT_BOOL},
+                                                         {"double", DT_DOUBLE},
+                                                         {"dual", DT_DUAL},
+                                                         {"dual_sub_int8", DT_DUAL_SUB_INT8},
+                                                         {"dual_sub_uint8", DT_DUAL_SUB_UINT8},
+                                                         {"int4", DT_INT4},
+                                                         {"bfloat16", DT_BF16}};
+
 void TransformerOpBaseFormat(const Operator& op, const std::string& input_name, const Format storage_format) {
   auto op_desc = OpDescUtils::GetOpDescFromOperator(op);
   auto tensordesc_input = op_desc->MutableInputDesc(input_name);
@@ -37,10 +56,6 @@ void TransformerOpBaseFormat(const Operator& op, const std::string& input_name, 
                                                     storage_format, data_type,      transformer::EN_IMPL_HW_TBE};
   (void)shape_transfer.GetShapeAccordingToFormat(shape_and_format_info);
   auto new_shape_shape = GeShape(new_shape_dims);
-  OP_LOGI("TransformerOpBaseFormat",
-          "Transform shape successfully, node:%s, name:%s, shape:%s, format:%s, new shape:%s, new format:%s.",
-          op_desc->GetName().c_str(), input_name.c_str(), shape_shape.ToString().c_str(), to_string(format).c_str(),
-          new_shape_shape.ToString().c_str(), to_string(storage_format).c_str());
   std::vector<std::pair<int64_t, int64_t>> origin_range;
   std::vector<std::pair<int64_t, int64_t>> output_range;
   tensordesc_input->GetOriginShapeRange(origin_range);
@@ -52,11 +67,16 @@ void TransformerOpBaseFormat(const Operator& op, const std::string& input_name, 
   tensordesc_input->SetFormat(storage_format);
   tensordesc_input->SetShape(GeShape(new_shape_dims));
   tensordesc_input->SetShapeRange(output_range);
+  OP_LOGI("TransformerOpBaseFormat",
+          "Transform shape successfully, node:%s, name:%s, shape:%s, format:%s, new shape:%s, new format:%s.",
+          op_desc->GetName().c_str(), input_name.c_str(), shape_shape.ToString().c_str(),
+          ge::TypeUtils::FormatToSerialString(format).c_str(), new_shape_shape.ToString().c_str(),
+          ge::TypeUtils::FormatToSerialString(storage_format).c_str());
 }
 
 DataType StringToDtype(std::string dtype_string) {
-  auto find_it = optiling::STR_TO_DATATYPE.find(dtype_string);
-  if (find_it != optiling::STR_TO_DATATYPE.end()) {
+  auto find_it = STR_TO_DATATYPE.find(dtype_string);
+  if (find_it != STR_TO_DATATYPE.end()) {
     return find_it->second;
   }
   return ge::DT_FLOAT16;
