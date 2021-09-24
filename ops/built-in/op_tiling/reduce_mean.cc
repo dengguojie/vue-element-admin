2fb281d60d1a8080e7b0914ccf30072991968d1c
caorp <caorenping@huawei.com>
2021-09-24 01:34:26 +0000

    !10207 reducemean optiling Merge pull request !10207 from caorp/reducemeantiling

--- ops/built-in/op_tiling/reduce_mean.cc
+++ ops/built-in/op_tiling/reduce_mean.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the Apache License Version 2.0.You may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 #include "reduce_tiling.h"
 #include "../fusion_pass/common/fp16_t.hpp"
 #include "error_log.h"
+#include "op_tiling_util.h"
+#include "vector_tiling_profiling.h"
+#include "graph/utils/op_desc_utils.h"
 
 namespace optiling {
 
@@ -36,66 +39,43 @@ bool IsPureMove(const std::vector<int64_t>& input_shape, const std::vector<int32
   return true;
 }
 
-bool GetInputShape(const std::string& op_type, const TeOpParas& op_paras,
+bool GetInputShape(const std::string& op_type, const ge::Operator& op_paras,
                    const nlohmann::json& op_info, std::vector<int64_t>& input_shape_ori) {
   int idx = op_info.count("_idx_before_reduce") > 0 ?
             op_info.at("_idx_before_reduce").get<int>() : 0;
   // CHECK INPUT
-  V_OP_TILING_CHECK(!op_paras.inputs.empty(),
-                    VECTOR_INNER_ERR_REPORT_TILIING(op_type, "inputs cannot be empty"),
-                    return false);
-  V_OP_TILING_CHECK(!(op_paras.inputs.size() <= uint(idx) || idx < 0),
-                    VECTOR_INNER_ERR_REPORT_TILIING(op_type, "idx is invalid index for inputs"),
-                    return false);
-  V_OP_TILING_CHECK(!op_paras.inputs[uint(idx)].tensor.empty(),
-                    VECTOR_INNER_ERR_REPORT_TILIING(op_type, "tensor cannot be empty"),
-                    return false);
-  input_shape_ori = op_paras.inputs[idx].tensor[0].shape;
+  OP_TILING_CHECK((idx < 0),
+                  VECTOR_INNER_ERR_REPORT_TILIING(op_type, "idx is invalid index for inputs"), return false);
+  auto operator_info = ge::OpDescUtils::GetOpDescFromOperator(op_paras);
+  OP_TILING_CHECK(operator_info == nullptr,
+                  VECTOR_INNER_ERR_REPORT_TILIING(op_type, "GetOpDescFromOperator return nullptr!"), return false);
+
+  auto input_desc = operator_info->MutableInputDesc(idx);
+  OP_TILING_CHECK(input_desc == nullptr,
+                  VECTOR_INNER_ERR_REPORT_TILIING(op_type, "get input %d opdesc failed", idx), return false);
+  input_shape_ori = input_desc->MutableShape().GetDims();
   return true;
 }
 
-bool GetReduceAxis(const std::string& op_type, const TeOpParas& op_paras, const nlohmann::json& op_info,
+bool GetReduceAxis(const std::string& op_type, const ge::Operator& op_paras, const nlohmann::json& op_info,
                    const std::vector<int64_t>& input_shape_ori, std::vector<int32_t>& reduce_axis_ori) {
   // Get ori reduce aixs
-  if (op_paras.const_inputs.find("axes") != op_paras.const_inputs.end() || op_info.count("axes_idx") > 0) {
-    // axis_unknown
-    TeConstTensorData reduce_axis_info;
+  std::vector<int64_t> values;
+  if(op_type == "ReduceMean") {
+    std::string axes_name = "axes";
     if (op_info.count("axes_idx") > 0) {
+      auto operator_info = ge::OpDescUtils::GetOpDescFromOperator(op_paras);
+      OP_TILING_CHECK(operator_info == nullptr,
+                      VECTOR_INNER_ERR_REPORT_TILIING(op_type, "GetOpDescFromOperator return nullptr!"), return false);
       int axes_idx = op_info.at("axes_idx").get<int>();
-      std::string axes_name = op_paras.inputs[axes_idx].tensor[0].name;
-      for (auto &item: op_paras.const_inputs) {
-          const std::string key = item.first;
-      }
-      reduce_axis_info = op_paras.const_inputs.at(axes_name);
-    } else {
-      reduce_axis_info = op_paras.const_inputs.at("axes");
+      axes_name = operator_info->GetInputNameByIndex(axes_idx);
     }
-    auto size = std::get<1>(reduce_axis_info);
-    ge::DataType axis_type = std::get<2>(reduce_axis_info).GetTensorDesc().GetDataType();
-    V_OP_TILING_CHECK(!(axis_type != ge::DT_INT32 && axis_type != ge::DT_INT64),
-                      VECTOR_INNER_ERR_REPORT_TILIING(op_type, "axis_type is not belong to [int32, int64]"),
-                      return false);
-
-    if (axis_type == ge::DT_INT32) {
-      int count = size / sizeof(int32_t);
-      const int32_t* data_addr = reinterpret_cast<const int32_t*>(std::get<0>(reduce_axis_info));
-      reduce_axis_ori.resize(count);
-      for (int i = 0; i < count; i++) {
-        reduce_axis_ori[i] = *data_addr;
-        data_addr++;
-      }
-    } else {
-      int count = size / sizeof(int64_t);
-      const int64_t* data_addr = reinterpret_cast<const int64_t*>(std::get<0>(reduce_axis_info));
-      reduce_axis_ori.resize(count);
-      for (int i = 0; i < count; i++) {
-        reduce_axis_ori[i] = (int32_t)*data_addr;
-        data_addr++;
-      }
-    }
+    OP_TILING_CHECK(!GetConstValue(op_paras, axes_name, values),
+                    VECTOR_INNER_ERR_REPORT_TILIING(op_type, "GetConstValue %s error!",axes_name.c_str()), return false);
     // clear reduce_axis_ori when shape of input axis is (0, )
-    if (size == 0) {
-      reduce_axis_ori.clear();
+    reduce_axis_ori.resize(values.size());
+    for (int i = 0; i < values.size(); i++) {
+      reduce_axis_ori[i] = values[i];
     }
   } else {
     // axis_known
@@ -119,55 +99,67 @@ bool GetReduceAxis(const std::string& op_type, const TeOpParas& op_paras, const 
   return true;
 }
 
-bool ReduceMeanTiling(const std::string& op_type, const TeOpParas& op_paras, const nlohmann::json& op_info,
-                      OpRunInfo& run_info) {
-  bool ret = ReduceTiling(op_type, op_paras, op_info, run_info);
+bool ReduceMeanTiling(const std::string& op_type, const ge::Operator& op_paras, const nlohmann::json& op_info,
+                      utils::OpRunInfo& run_info) {
+  PROFILING_TILING_INIT(op_type.c_str());
+  bool ret = true;
+  auto operator_info = ge::OpDescUtils::GetOpDescFromOperator(op_paras);
+  OP_TILING_CHECK(operator_info == nullptr,
+                  VECTOR_INNER_ERR_REPORT_TILIING(op_type, "GetOpDescFromOperator return nullptr!"), return false);
   std::vector<int64_t> input_shape{std::vector<int64_t>(10, 0)};
   std::vector<int32_t> reduce_axis{std::vector<int32_t>(10, 0)};
   ret = ret && GetInputShape(op_type, op_paras, op_info, input_shape);
+  ScalarToShape(input_shape);
+  PROFILING_TILING_AFTER_GET_SHAPE_REG();
   ret = ret && GetReduceAxis(op_type, op_paras, op_info, input_shape, reduce_axis);
-
+  PROFILING_TILING_AFTER_GET_COMPILE_INFO_REG();
+  std::vector<std::vector<int64_t>> shapes = {input_shape};
+  std::vector<std::vector<int32_t>> axes{reduce_axis};
+  ge::DataType type = operator_info->MutableInputDesc(0)->GetDataType();
+  OpInfo eletwise_info(shapes, type, axes);
+  ret = ReduceTiling(op_type, op_paras, op_info, run_info, eletwise_info);
   // reduce_mean_cof is not required when handling pure dma_copy case
   if (IsPureMove(input_shape, reduce_axis)) {
     return ret;
   }
-
+  PROFILING_TILING_AFTER_CALCU_TILING_REG();
   float reduce_mean_cof = 1.0;
   if (op_info.count("reduce_mean_cof_dtype") > 0) {
     const std::string& reduce_mean_cof_dtype = op_info.at("reduce_mean_cof_dtype").get<std::string>();
     if (reduce_mean_cof_dtype == "float32") {
       for (uint32_t i = 0; i < input_shape.size(); i++) {
         if (input_shape[i] == 0) {
-	  OP_LOGD(op_type.c_str(), "reduce mean shape is 0");
-	  return ret;
-	}
-        if (IsInVector(reduce_axis, i)) {
-          reduce_mean_cof = reduce_mean_cof / input_shape[i];
-        }
-      }
-      ByteBufferPut(run_info.tiling_data, (float)reduce_mean_cof);
-      OP_LOGD(op_type.c_str(), "reduce mean cof:%f", reduce_mean_cof);
-    } else if (reduce_mean_cof_dtype == "float16") {
-      for (uint32_t i = 0; i < input_shape.size(); i++) {
-	if (input_shape[i] == 0) {
-          OP_LOGD(op_type.c_str(), "reduce mean shape is 0, dtype is fp16");
+          OP_LOGD(op_type.c_str(), "reduce mean shape is 0");
           return ret;
         }
         if (IsInVector(reduce_axis, i)) {
           reduce_mean_cof = reduce_mean_cof / input_shape[i];
         }
       }
+      run_info.AddTilingData((float)reduce_mean_cof);
+      OP_LOGD(op_type.c_str(), "reduce mean cof:%f", reduce_mean_cof);
+    } else if (reduce_mean_cof_dtype == "float16") {
+      for (uint32_t i = 0; i < input_shape.size(); i++) {
+        if (input_shape[i] == 0) {
+          OP_LOGD(op_type.c_str(), "reduce mean shape is 0, dtype is fp16");
+          return ret;
+        } 
+        if (IsInVector(reduce_axis, i)) {
+          reduce_mean_cof = reduce_mean_cof / input_shape[i];
+        }
+      }
       fe::fp16_t reduce_mean_cof_fp16;
       reduce_mean_cof_fp16 = reduce_mean_cof;
-      ByteBufferPut(run_info.tiling_data, (fe::fp16_t)reduce_mean_cof_fp16);
-      ByteBufferPut(run_info.tiling_data, (uint16_t)0);
+      run_info.AddTilingData((fe::fp16_t)reduce_mean_cof_fp16);
+      run_info.AddTilingData((uint16_t)0);
       OP_LOGD(op_type.c_str(), "reduce mean cof:%f", reduce_mean_cof);
     }
   }
+  PROFILING_TILING_END();
 
   return ret;
 }
 
-REGISTER_OP_TILING_FUNC_BUFFERED(ReduceMean, ReduceMeanTiling);
-REGISTER_OP_TILING_FUNC_BUFFERED(ReduceMeanD, ReduceMeanTiling);
+REGISTER_OP_TILING_FUNC_BUFFERED_V2(ReduceMean, ReduceMeanTiling);
+REGISTER_OP_TILING_FUNC_BUFFERED_V2(ReduceMeanD, ReduceMeanTiling);
 }  // namespace optiling
--- ops/built-in/tests/ut/op_tiling_test/test_reduce_mean_tiling.cpp
+++ ops/built-in/tests/ut/op_tiling_test/test_reduce_mean_tiling.cpp
@@ -1,9 +1,23 @@
+/*
+ * Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Apache License Version 2.0.You may not use this file except in compliance with the License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License for more details at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 #include <iostream>
 #include <fstream>
 #include <vector>
 
 #include <gtest/gtest.h>
 #include "register/op_tiling_registry.h"
+#include "reduce_ops.h"
+#include "array_ops.h"
 
 using namespace std;
 
@@ -31,176 +45,105 @@ static string to_string(const std::stringstream &tiling_data) {
   return result;
 }
 
-TEST_F(ReduceMeanTiling, ReduceMeanTiling1) {
-  using namespace optiling;
-  std::string op_name = "ReduceMeanD";
-  auto iter = optiling::OpTilingRegistryInterf::RegisteredOpInterf().find(op_name);
-  ASSERT_TRUE(iter != optiling::OpTilingRegistryInterf::RegisteredOpInterf().end());
+using namespace ge;
+#include "common/utils/ut_op_util.h"
+using namespace ut_util;
+/*
+.INPUT(x, TensorType({DT_FLOAT16, DT_FLOAT}))
+    .OUTPUT(y, TensorType({DT_FLOAT16, DT_FLOAT}))
+    .REQUIRED_ATTR(axes, ListInt)
+    .ATTR(keep_dims, Bool, false)
+*/
 
+TEST_F(ReduceMeanTiling, ReduceMeanTiling1) {
+  std::string op_name = "ReduceMeanD";
+  auto iter = optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().find(op_name);
+  ASSERT_TRUE(iter != optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().end());
 
   std::string compileInfo = R"({ "_ori_axis": [0], "_pattern": "CommReduce","push_status": 0,"_common_info": [32, 1, 8, 1, 1], "_pattern_info": [5], "_ub_info": [16256], "_ub_info_rf": [16256], "_vars": {"-1000500": ["_dim_1_0", "_block_factor", "_ub_factor"]}})";
 
   std::vector<int64_t> input{1};
   std::vector<int64_t> output{1};
-  std::string in_dtype = "float32";
+  ge::DataType in_dtype = ge::DT_FLOAT;
 
-  TeOpTensor tensor_input;
-  tensor_input.shape = input;
-  tensor_input.dtype = in_dtype;
-  TeOpTensor tensor_output;
-  tensor_output.shape = output;
-  tensor_output.dtype = in_dtype;
-  TeOpTensorArg tensor_arg;
-  tensor_arg.tensor.push_back(tensor_input);
-  tensor_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_arg_out;
-  tensor_arg_out.tensor.push_back(tensor_output);
-  tensor_arg_out.arg_type = TA_SINGLE;
-  TeOpParas opParas;
-  opParas.inputs.push_back(tensor_arg);
-  opParas.outputs.push_back(tensor_arg_out);
-  opParas.op_type = op_name;
-  OpCompileInfo op_compile_info;
-  op_compile_info.str = compileInfo;
-  op_compile_info.key = "REDUCEMEAN__COUNTER__1";
-  OpRunInfo runInfo;
+  auto opParas = op::ReduceMeanD("ReduceMeanD");
+  TENSOR_INPUT_WITH_SHAPE(opParas, x, input, in_dtype, ge::FORMAT_ND, {});
+  TENSOR_OUTPUT_WITH_SHAPE(opParas, y, output, in_dtype, ge::FORMAT_ND, {});
+  optiling::utils::OpCompileInfo op_compile_info(this->test_info_->name(), compileInfo.c_str());
+  optiling::utils::OpRunInfo runInfo;
   ASSERT_TRUE(iter->second(opParas, op_compile_info, runInfo));
-  EXPECT_EQ(runInfo.block_dim, 1);
-  EXPECT_EQ(to_string(runInfo.tiling_data), "1 1 1 ");
+  EXPECT_EQ(runInfo.GetBlockDim(), 1);
+  EXPECT_EQ(to_string(runInfo.GetAllTilingData()), "1 1 1 ");
 }
 
 TEST_F(ReduceMeanTiling, ReduceMeanTiling2) {
-  using namespace optiling;
   std::string op_name = "ReduceMeanD";
-  auto iter = optiling::OpTilingRegistryInterf::RegisteredOpInterf().find(op_name);
-  ASSERT_TRUE(iter != optiling::OpTilingRegistryInterf::RegisteredOpInterf().end());
-
+  auto iter = optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().find(op_name);
+  ASSERT_TRUE(iter != optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().end());
 
   std::string compileInfo = R"({ "_ori_axis": [1], "_pattern": "CommReduce", "push_status": 0, "_common_info": [32, 1, 8, 1, 1], "_pattern_info": [5, 4, 9], "_ub_info": [16256, 16000, 16256], "_ub_info_rf": [16256, 16000, 16256], "reduce_mean_cof_dtype": "float32"})";
 
   std::vector<int64_t> input{7, 2};
   std::vector<int64_t> output{7};
-  std::string in_dtype = "float32";
+  ge::DataType in_dtype = ge::DT_FLOAT;
 
-  TeOpTensor tensor_input;
-  tensor_input.shape = input;
-  tensor_input.dtype = in_dtype;
-  TeOpTensor tensor_output;
-  tensor_output.shape = output;
-  tensor_output.dtype = in_dtype;
-  TeOpTensorArg tensor_arg;
-  tensor_arg.tensor.push_back(tensor_input);
-  tensor_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_arg_out;
-  tensor_arg_out.tensor.push_back(tensor_output);
-  tensor_arg_out.arg_type = TA_SINGLE;
-  TeOpParas opParas;
-  opParas.inputs.push_back(tensor_arg);
-  opParas.outputs.push_back(tensor_arg_out);
-  opParas.op_type = op_name;
-  OpCompileInfo op_compile_info;
-  op_compile_info.str = compileInfo;
-  op_compile_info.key = "REDUCEMEAN__COUNTER__2";
-  OpRunInfo runInfo;
+  auto opParas = op::ReduceMeanD("ReduceMeanD");
+  TENSOR_INPUT_WITH_SHAPE(opParas, x, input, in_dtype, ge::FORMAT_ND, {});
+  TENSOR_OUTPUT_WITH_SHAPE(opParas, y, output, in_dtype, ge::FORMAT_ND, {});
+  optiling::utils::OpCompileInfo op_compile_info(this->test_info_->name(), compileInfo.c_str());
+  optiling::utils::OpRunInfo runInfo;
   ASSERT_TRUE(iter->second(opParas, op_compile_info, runInfo));
-  EXPECT_EQ(runInfo.block_dim, 1);
-  EXPECT_EQ(to_string(runInfo.tiling_data), "7 2 8 8 1056964608 ");
+  EXPECT_EQ(runInfo.GetBlockDim(), 1);
+  EXPECT_EQ(to_string(runInfo.GetAllTilingData()), "7 2 8 8 1056964608 ");
 }
 
 TEST_F(ReduceMeanTiling, ReduceMeanTiling3) {
-  using namespace optiling;
   std::string op_name = "ReduceMeanD";
-  auto iter = optiling::OpTilingRegistryInterf::RegisteredOpInterf().find(op_name);
-  ASSERT_TRUE(iter != optiling::OpTilingRegistryInterf::RegisteredOpInterf().end());
-
+  auto iter = optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().find(op_name);
+  ASSERT_TRUE(iter != optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().end());
 
   std::string compileInfo = R"({ "_ori_axis": [1], "_pattern": "CommReduce", "push_status": 0, "_common_info": [2, 1, 16, 0, 1], "_pattern_info": [5, 4, 9], "_ub_info": [31488, 31104, 31488], "_ub_info_rf": [31488, 31104, 31488], "reduce_mean_cof_dtype": "float16"})";
 
   std::vector<int64_t> input{7, 2};
   std::vector<int64_t> output{7};
-  std::string in_dtype = "float16";
+  ge::DataType in_dtype = ge::DT_FLOAT16;
 
-  TeOpTensor tensor_input;
-  tensor_input.shape = input;
-  tensor_input.dtype = in_dtype;
-  TeOpTensor tensor_output;
-  tensor_output.shape = output;
-  tensor_output.dtype = in_dtype;
-  TeOpTensorArg tensor_arg;
-  tensor_arg.tensor.push_back(tensor_input);
-  tensor_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_arg_out;
-  tensor_arg_out.tensor.push_back(tensor_output);
-  tensor_arg_out.arg_type = TA_SINGLE;
-  TeOpParas opParas;
-  opParas.inputs.push_back(tensor_arg);
-  opParas.outputs.push_back(tensor_arg_out);
-  opParas.op_type = op_name;
-  OpCompileInfo op_compile_info;
-  op_compile_info.str = compileInfo;
-  op_compile_info.key = "REDUCEMEAN__COUNTER__3";
-  OpRunInfo runInfo;
+  auto opParas = op::ReduceMeanD("ReduceMeanD");
+  TENSOR_INPUT_WITH_SHAPE(opParas, x, input, in_dtype, ge::FORMAT_ND, {});
+  TENSOR_OUTPUT_WITH_SHAPE(opParas, y, output, in_dtype, ge::FORMAT_ND, {});
+  optiling::utils::OpCompileInfo op_compile_info(this->test_info_->name(), compileInfo.c_str());
+  optiling::utils::OpRunInfo runInfo;
   ASSERT_TRUE(iter->second(opParas, op_compile_info, runInfo));
-  EXPECT_EQ(runInfo.block_dim, 1);
-  EXPECT_EQ(to_string(runInfo.tiling_data), "7 2 16 16 14336 ");
+  EXPECT_EQ(runInfo.GetBlockDim(), 1);
+  EXPECT_EQ(to_string(runInfo.GetAllTilingData()), "7 2 16 16 14336 ");
 }
 
 TEST_F(ReduceMeanTiling, ReduceTiling4) {
-  using namespace optiling;
   std::string op_name = "ReduceMean";
-  auto iter = optiling::OpTilingRegistryInterf::RegisteredOpInterf().find(op_name);
-  ASSERT_TRUE(iter != optiling::OpTilingRegistryInterf::RegisteredOpInterf().end());
-
+  auto iter = optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().find(op_name);
+  ASSERT_TRUE(iter != optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().end());
 
   std::string compileInfo = R"({"_idx_before_reduce": 0, "_pattern": "CommReduce", "_common_info": [32, 1, 8, 1, 1], "_pattern_info": [-1], "_ub_info": [32512], "_ub_info_rf": [32512], "_reduce_shape_known": true, "_const_shape_post": true, "_compile_pattern": -1, "_block_dims": {"-1": 32}, "_atomic_flags": {"-1": false}})";
 
   std::vector<int64_t> input{1, 10};
   std::vector<int64_t> output{1, 10};
   std::vector<int64_t> input_axis{0};
-  std::vector<int32_t> axis{0};
-  std::string in_dtype = "float32";
+  std::vector<int32_t> axis = {0};
+  ge::DataType in_dtype = ge::DT_FLOAT;
 
-  TeOpTensor tensor_input;
-  tensor_input.shape = input;
-  tensor_input.dtype = in_dtype;
-  TeOpTensor tensor_input_axis;
-  tensor_input_axis.shape = input_axis;
-  tensor_input_axis.dtype = "int32";
-  TeOpTensor tensor_output;
-  tensor_output.shape = output;
-  tensor_output.dtype = in_dtype;
-  TeOpTensorArg tensor_arg;
-  tensor_arg.tensor.push_back(tensor_input);
-  tensor_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_input_axis_arg;
-  tensor_input_axis_arg.tensor.push_back(tensor_input_axis);
-  tensor_input_axis_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_arg_out;
-  tensor_arg_out.tensor.push_back(tensor_output);
-  tensor_arg_out.arg_type = TA_SINGLE;
-  TeOpParas opParas;
-  opParas.inputs.push_back(tensor_arg);
-  ge::TensorDesc tensorDesc1;
-  tensorDesc1.SetDataType(ge::DT_INT32);
-  auto ge_tensor = ge::Tensor(tensorDesc1);
-  opParas.const_inputs["axes"] =
-    std::tuple<const uint8_t*, size_t, ge::Tensor>((const uint8_t*)axis.data(), axis.size() * 4, ge_tensor);
-  opParas.inputs.push_back(tensor_input_axis_arg);
-  opParas.outputs.push_back(tensor_arg_out);
-  opParas.op_type = op_name;
-  OpCompileInfo op_compile_info;
-  op_compile_info.str = compileInfo;
-  op_compile_info.key = "REDUCEMEAN__COUNTER__4";
-  OpRunInfo runInfo;
+  auto opParas = op::ReduceMean("ReduceMean");
+  TENSOR_INPUT_WITH_SHAPE(opParas, x, input, in_dtype, ge::FORMAT_ND, {});
+  TENSOR_INPUT_WITH_SHAPE_AND_CONST_VALUE(opParas, axes, input_axis, ge::DT_INT64, FORMAT_ND, axis);
+  TENSOR_OUTPUT_WITH_SHAPE(opParas, y, output, in_dtype, ge::FORMAT_ND, {});
+  optiling::utils::OpCompileInfo op_compile_info(this->test_info_->name(), compileInfo.c_str());
+  optiling::utils::OpRunInfo runInfo;
   ASSERT_TRUE(iter->second(opParas, op_compile_info, runInfo));
 }
-TEST_F(ReduceMeanTiling, ReduceTiling6) {
-  using namespace optiling;
-  std::cout << "ReduceMeanTiling ReduceTiling6" << std::endl;
-  std::string op_name = "ReduceMean";
-  auto iter = optiling::OpTilingRegistryInterf::RegisteredOpInterf().find(op_name);
-  ASSERT_TRUE(iter != optiling::OpTilingRegistryInterf::RegisteredOpInterf().end());
 
+TEST_F(ReduceMeanTiling, ReduceTiling5) {
+  std::string op_name = "ReduceMean";
+  auto iter = optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().find(op_name);
+  ASSERT_TRUE(iter != optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().end());
 
   std::string compileInfo = R"({"_idx_before_reduce": 0, "_pattern": "CommReduce", "_common_info": [32, 1, 8, 1, 1], "_pattern_info": [-1], "_ub_info": [32512], "_ub_info_rf": [32512], "_reduce_shape_known": true, "_const_shape_post": true, "_compile_pattern": -1, "_block_dims": {"-1": 32}, "_atomic_flags": {"-1": false},"reduce_mean_cof_dtype":"float32"})";
 
@@ -208,49 +151,21 @@ TEST_F(ReduceMeanTiling, ReduceTiling6) {
   std::vector<int64_t> output{0, 10,3};
   std::vector<int64_t> input_axis{1};
   std::vector<int64_t> axis{1};
-  std::string in_dtype = "float32";
+  ge::DataType in_dtype = ge::DT_FLOAT;
 
-  TeOpTensor tensor_input;
-  tensor_input.shape = input;
-  tensor_input.dtype = in_dtype;
-  TeOpTensor tensor_input_axis;
-  tensor_input_axis.shape = input_axis;
-  tensor_input_axis.dtype = "int64";
-  TeOpTensor tensor_output;
-  tensor_output.shape = output;
-  tensor_output.dtype = in_dtype;
-  TeOpTensorArg tensor_arg;
-  tensor_arg.tensor.push_back(tensor_input);
-  tensor_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_input_axis_arg;
-  tensor_input_axis_arg.tensor.push_back(tensor_input_axis);
-  tensor_input_axis_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_arg_out;
-  tensor_arg_out.tensor.push_back(tensor_output);
-  tensor_arg_out.arg_type = TA_SINGLE;
-  TeOpParas opParas;
-  opParas.inputs.push_back(tensor_arg);
-  ge::TensorDesc tensorDesc1;
-  tensorDesc1.SetDataType(ge::DT_INT64);
-  auto ge_tensor = ge::Tensor(tensorDesc1);
-  opParas.const_inputs["axes"] =
-    std::tuple<const uint8_t*, size_t, ge::Tensor>((const uint8_t*)axis.data(), axis.size() * 8, ge_tensor);
-  opParas.inputs.push_back(tensor_input_axis_arg);
-  opParas.outputs.push_back(tensor_arg_out);
-  opParas.op_type = op_name;
-  OpCompileInfo op_compile_info;
-  op_compile_info.str = compileInfo;
-  op_compile_info.key = "REDUCEMEAN__COUNTER__6";
-  OpRunInfo runInfo;
+  auto opParas = op::ReduceMean("ReduceMean");
+  TENSOR_INPUT_WITH_SHAPE(opParas, x, input, in_dtype, ge::FORMAT_ND, {});
+  TENSOR_INPUT_WITH_SHAPE_AND_CONST_VALUE(opParas, axes, input_axis, ge::DT_INT64, FORMAT_ND, axis);
+  TENSOR_OUTPUT_WITH_SHAPE(opParas, y, output, in_dtype, ge::FORMAT_ND, {});
+  optiling::utils::OpCompileInfo op_compile_info(this->test_info_->name(), compileInfo.c_str());
+  optiling::utils::OpRunInfo runInfo;
   ASSERT_TRUE(iter->second(opParas, op_compile_info, runInfo));
 }
-TEST_F(ReduceMeanTiling, ReduceTiling7) {
-  using namespace optiling;
-  std::cout << "ReduceMeanTiling ReduceTiling7" << std::endl;
-  std::string op_name = "ReduceMean";
-  auto iter = optiling::OpTilingRegistryInterf::RegisteredOpInterf().find(op_name);
-  ASSERT_TRUE(iter != optiling::OpTilingRegistryInterf::RegisteredOpInterf().end());
 
+TEST_F(ReduceMeanTiling, ReduceTiling7) {
+  std::string op_name = "ReduceMean";
+  auto iter = optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().find(op_name);
+  ASSERT_TRUE(iter != optiling::utils::OpTilingRegistryInterf_V2::RegisteredOpInterf().end());
 
   std::string compileInfo = R"({"_idx_before_reduce": 0, "_pattern": "CommReduce", "_common_info": [32, 1, 8, 1, 1], "_pattern_info": [-1], "_ub_info": [32512], "_ub_info_rf": [32512], "_reduce_shape_known": true, "_const_shape_post": true, "_compile_pattern": -1, "_block_dims": {"-1": 32}, "_atomic_flags": {"-1": false},"reduce_mean_cof_dtype":"float16"})";
 
@@ -258,40 +173,14 @@ TEST_F(ReduceMeanTiling, ReduceTiling7) {
   std::vector<int64_t> output{0, 10,3};
   std::vector<int64_t> input_axis{1};
   std::vector<int64_t> axis{1};
-  std::string in_dtype = "float16";
+  ge::DataType in_dtype = ge::DT_FLOAT16;
 
-  TeOpTensor tensor_input;
-  tensor_input.shape = input;
-  tensor_input.dtype = in_dtype;
-  TeOpTensor tensor_input_axis;
-  tensor_input_axis.shape = input_axis;
-  tensor_input_axis.dtype = "int64";
-  TeOpTensor tensor_output;
-  tensor_output.shape = output;
-  tensor_output.dtype = in_dtype;
-  TeOpTensorArg tensor_arg;
-  tensor_arg.tensor.push_back(tensor_input);
-  tensor_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_input_axis_arg;
-  tensor_input_axis_arg.tensor.push_back(tensor_input_axis);
-  tensor_input_axis_arg.arg_type = TA_SINGLE;
-  TeOpTensorArg tensor_arg_out;
-  tensor_arg_out.tensor.push_back(tensor_output);
-  tensor_arg_out.arg_type = TA_SINGLE;
-  TeOpParas opParas;
-  opParas.inputs.push_back(tensor_arg);
-  ge::TensorDesc tensorDesc1;
-  tensorDesc1.SetDataType(ge::DT_INT64);
-  auto ge_tensor = ge::Tensor(tensorDesc1);
-  opParas.const_inputs["axes"] =
-    std::tuple<const uint8_t*, size_t, ge::Tensor>((const uint8_t*)axis.data(), axis.size() * 8, ge_tensor);
-  opParas.inputs.push_back(tensor_input_axis_arg);
-  opParas.outputs.push_back(tensor_arg_out);
-  opParas.op_type = op_name;
-  OpCompileInfo op_compile_info;
-  op_compile_info.str = compileInfo;
-  op_compile_info.key = "REDUCEMEAN__COUNTER__7";
-  OpRunInfo runInfo;
+  auto opParas = op::ReduceMean("ReduceMean");
+  TENSOR_INPUT_WITH_SHAPE(opParas, x, input, in_dtype, ge::FORMAT_ND, {});
+  TENSOR_INPUT_WITH_SHAPE_AND_CONST_VALUE(opParas, axes, input_axis, ge::DT_INT64, FORMAT_ND, axis);
+  TENSOR_OUTPUT_WITH_SHAPE(opParas, y, output, in_dtype, ge::FORMAT_ND, {});
+  optiling::utils::OpCompileInfo op_compile_info(this->test_info_->name(), compileInfo.c_str());
+  optiling::utils::OpRunInfo runInfo;
   ASSERT_TRUE(iter->second(opParas, op_compile_info, runInfo));
 }
 
