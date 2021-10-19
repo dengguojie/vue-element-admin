58528a31d73c68b241e0a479d372a58b721280f6
xielei <1139529313@qq.com>
2021-10-19 12:51:15 +0000

    !10891 添加ut用例 derelu_fusion_test.cpp Merge pull request !10891 from xielei/addut1

--- ops/built-in/tests/ut/graph_fusion/ai_core/derelu_fusion_test.cpp
+++ ops/built-in/tests/ut/graph_fusion/ai_core/derelu_fusion_test.cpp
@@ -55,3 +55,36 @@ TEST_F(derelu_fusion_test, derelu_fusion_test_1) {
     fe::FusionPassTestUtils::RunGraphFusionPass("DreluFusionPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
 
 }
+
+TEST_F(derelu_fusion_test, derelu_fusion_test_2) {
+  ge::Graph graph("derelu_fusion_test_2");
+  std::vector<int64_t> dims{3, 32,2,3};
+  ge::Shape shape(dims);
+  ge::TensorDesc tensorDesc(shape,FORMAT_NHWC, DT_FLOAT);
+
+  auto relu_input_data = op::Data("relu_input_data");
+  relu_input_data.update_input_desc_x(tensorDesc);
+  relu_input_data.update_output_desc_y(tensorDesc);
+
+  auto relu_op = op::Relu("relu");
+  relu_op.set_input_x(relu_input_data);
+
+  std::vector<int64_t> grad_dims{3,32,2,3 };
+  ge::Shape grad_shape(grad_dims);
+  ge::TensorDesc tensorDesc2(grad_shape);
+  auto relu_grad_data = op::Data("relu_grad_data");
+  relu_grad_data.update_input_desc_x(tensorDesc2);
+  relu_grad_data.update_output_desc_y(tensorDesc2);
+
+  auto reluGrad_op = op::ReluGrad("relugrad");
+  reluGrad_op.set_input_gradients(relu_grad_data).set_input_features(relu_op);
+
+  std::vector<Operator> inputs{relu_input_data, relu_grad_data};
+  std::vector<Operator> outputs{reluGrad_op};
+  graph.SetInputs(inputs).SetOutputs(outputs);
+
+  ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+  
+  fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+  fe::FusionPassTestUtils::RunGraphFusionPass("DreluFusionPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
+}
--- ops/built-in/tests/ut/graph_fusion/ai_core/transpose_reshape_fusion_test.cpp
+++ ops/built-in/tests/ut/graph_fusion/ai_core/transpose_reshape_fusion_test.cpp
@@ -125,3 +125,156 @@ TEST_F(transpose_reshape_fusion_test, transpose_reshape_fusion_test_2) {
   }
   EXPECT_EQ(transpose_match, true);
 }
+
+TEST_F(transpose_reshape_fusion_test, transpose_reshape_fusion_test_3) {
+  ge::Graph graph("transpose_reshape_fusion_test_3");
+  std::vector<int64_t> dims_a{1000, 5, 64, 64};
+  ge::Shape shape_a(dims_a);
+  ge::TensorDesc tensorDesc_a(shape_a, ge::FORMAT_ND, ge::DT_FLOAT);
+  tensorDesc_a.SetOriginShape(shape_a);
+  tensorDesc_a.SetOriginFormat(ge::FORMAT_ND);
+  auto input_data = op::Data("input_data");
+  input_data.update_input_desc_x(tensorDesc_a);
+  input_data.update_output_desc_y(tensorDesc_a);
+
+  auto transpose_op = op::TransposeD("TransposeD").set_input_x(input_data).set_attr_perm({0, 2, 1, 3});
+  std::vector<int64_t> transpose_dims{1000, 64, 5, 64};
+  ge::Shape transpose_shape(transpose_dims);
+  ge::TensorDesc tensor_desc_transpose(transpose_shape, ge::FORMAT_ND, ge::DT_FLOAT);
+  tensor_desc_transpose.SetOriginShape(transpose_shape);
+  tensor_desc_transpose.SetOriginFormat(ge::FORMAT_ND);
+  transpose_op.update_output_desc_y(tensor_desc_transpose);
+
+  std::vector<int64_t> dims{1000, 64, 320};
+  ge::Shape reshape_shape(dims);
+  ge::TensorDesc tensor_desc_reshape(reshape_shape, ge::FORMAT_ND, ge::DT_FLOAT);
+  tensor_desc_reshape.SetOriginShape(reshape_shape);
+  tensor_desc_reshape.SetOriginFormat(ge::FORMAT_ND);
+
+  ge::Tensor shape_tensor;
+  std::vector<int64_t> shape_vec{3};
+  ge::Shape shape_shape(shape_vec);
+  ge::TensorDesc shape_desc(shape_shape, FORMAT_ND, DT_INT32);
+  int32_t shape_size = shape_desc.GetShape().GetShapeSize();
+  shape_desc.SetSize(shape_size * sizeof(int32_t));
+  shape_tensor.SetTensorDesc(shape_desc);
+  int32_t* shape_data = nullptr;
+  shape_data = new int32_t[shape_size];
+  *(shape_data + 0) = 1000;
+  *(shape_data + 1) = 64;
+  *(shape_data + 2) = 320;
+  shape_tensor.SetData((uint8_t*)shape_data, shape_size * sizeof(int32_t));
+  delete[] shape_data;
+
+  // auto shape_op = op::Shape("shape").set_input_x(transpose_op);
+  // auto reshapeConst = op::Constant().set_attr_value(shape_op);
+  auto reshapeConst = op::Constant().set_attr_value(shape_tensor);
+  auto reshape_op = op::Reshape("reshape").set_input_x(transpose_op).set_input_shape(reshapeConst);
+  reshape_op.update_output_desc_y(tensor_desc_reshape);
+
+  std::vector<Operator> inputs{input_data};
+  std::vector<Operator> outputs{reshape_op};
+  graph.SetInputs(inputs).SetOutputs(outputs);
+  ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+
+  NodePtr node1 = nullptr;
+  NodePtr node2 = nullptr;
+  for (auto node : compute_graph_ptr->GetAllNodes()) {
+    if (node->GetType() == "TransposeD") {
+      node1 = node;
+    }
+    if (node->GetType() == "Constant") {
+      node2 = node;
+    }
+  }
+
+  GraphUtils::AddEdge(node1->GetOutControlAnchor(), node2->GetInControlAnchor());
+
+  // fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+  fe::FusionPassTestUtils::RunGraphFusionPass("TransposeReshapeFusionPass", fe::BUILT_IN_GRAPH_PASS,
+                                              *compute_graph_ptr);
+  bool transpose_match = false;
+  for (auto node : compute_graph_ptr->GetAllNodes()) {
+    std::cout << "The node is: " << node->GetType() << std::endl;
+    if (node->GetType() == "ConfusionTransposeD") {
+      transpose_match = true;
+    }
+  }
+   EXPECT_EQ(transpose_match, true);
+}
+TEST_F(transpose_reshape_fusion_test, transpose_reshape_fusion_test_4) {
+  ge::Graph graph("transpose_reshape_fusion_test_3");
+  std::vector<int64_t> dims_a{1000, 5, 64, 64};
+  ge::Shape shape_a(dims_a);
+  ge::TensorDesc tensorDesc_a(shape_a, ge::FORMAT_ND, ge::DT_FLOAT);
+  tensorDesc_a.SetOriginShape(shape_a);
+  tensorDesc_a.SetOriginFormat(ge::FORMAT_ND);
+  auto input_data = op::Data("input_data");
+  input_data.update_input_desc_x(tensorDesc_a);
+  input_data.update_output_desc_y(tensorDesc_a);
+
+  auto transpose_op = op::TransposeD("TransposeD").set_input_x(input_data).set_attr_perm({0, 2, 1, 3});
+  std::vector<int64_t> transpose_dims{1000, 64, 5, 64};
+  ge::Shape transpose_shape(transpose_dims);
+  ge::TensorDesc tensor_desc_transpose(transpose_shape, ge::FORMAT_ND, ge::DT_FLOAT);
+  tensor_desc_transpose.SetOriginShape(transpose_shape);
+  tensor_desc_transpose.SetOriginFormat(ge::FORMAT_ND);
+  transpose_op.update_output_desc_y(tensor_desc_transpose);
+
+  std::vector<int64_t> dims{1000, 64, 320};
+  ge::Shape reshape_shape(dims);
+  ge::TensorDesc tensor_desc_reshape(reshape_shape, ge::FORMAT_ND, ge::DT_FLOAT);
+  tensor_desc_reshape.SetOriginShape(reshape_shape);
+  tensor_desc_reshape.SetOriginFormat(ge::FORMAT_ND);
+
+  ge::Tensor shape_tensor;
+  std::vector<int64_t> shape_vec{3};
+  ge::Shape shape_shape(shape_vec);
+  ge::TensorDesc shape_desc(shape_shape, FORMAT_ND, DT_INT32);
+  int32_t shape_size = shape_desc.GetShape().GetShapeSize();
+  shape_desc.SetSize(shape_size * sizeof(int32_t));
+  shape_tensor.SetTensorDesc(shape_desc);
+  int32_t* shape_data = nullptr;
+  shape_data = new int32_t[shape_size];
+  *(shape_data + 0) = 1000;
+  *(shape_data + 1) = 64;
+  *(shape_data + 2) = 320;
+  shape_tensor.SetData((uint8_t*)shape_data, shape_size * sizeof(int32_t));
+  delete[] shape_data;
+
+  // auto shape_op = op::Shape("shape").set_input_x(transpose_op);
+  // auto reshapeConst = op::Constant().set_attr_value(shape_op);
+  auto reshapeConst = op::Constant().set_attr_value(shape_tensor);
+  auto reshape_op = op::Reshape("reshape").set_input_x(transpose_op).set_input_shape(reshapeConst);
+  reshape_op.update_output_desc_y(tensor_desc_reshape);
+
+  std::vector<Operator> inputs{input_data};
+  std::vector<Operator> outputs{reshape_op};
+  graph.SetInputs(inputs).SetOutputs(outputs);
+  ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+
+  NodePtr node1 = nullptr;
+  NodePtr node2 = nullptr;
+  for (auto node : compute_graph_ptr->GetAllNodes()) {
+    if (node->GetType() == "TransposeD") {
+      node1 = node;
+    }
+    if (node->GetType() == "Reshape") {
+      node2 = node;
+    }
+  }
+
+  GraphUtils::AddEdge(node1->GetOutControlAnchor(), node2->GetInControlAnchor());
+
+  // fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+  fe::FusionPassTestUtils::RunGraphFusionPass("TransposeReshapeFusionPass", fe::BUILT_IN_GRAPH_PASS,
+                                              *compute_graph_ptr);
+  bool transpose_match = false;
+  for (auto node : compute_graph_ptr->GetAllNodes()) {
+    std::cout << "The node is: " << node->GetType() << std::endl;
+    if (node->GetType() == "ConfusionTransposeD") {
+      transpose_match = true;
+    }
+  }
+   EXPECT_EQ(transpose_match, false);
+}
