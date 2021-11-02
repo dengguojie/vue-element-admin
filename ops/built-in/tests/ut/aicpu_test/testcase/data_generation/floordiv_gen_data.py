5f21a05cd641055cd73da2eaae85ed9100969ee3
孟权令 <731708398@qq.com>
2021-11-02 09:29:40 +0000

    !4649 CANN AI CPU 自定义算子FloorDiv Merge pull request !4649 from 孟权令/floordiv

---
+++ ops/built-in/tests/ut/aicpu_test/testcase/data_generation/floordiv_gen_data.py
@@ -0,0 +1,256 @@
+"""
+Copyright 2021 Huawei Technologies Co., Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import logging
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.framework import constant_op, dtypes
+
+
+# prama1: file_name: the file which store the data
+# param2: data: data which will be stored
+# param3: fmt: format
+def write_file_txt(file_name, data, fmt="%s"):
+    if (file_name is None):
+        print("file name is none, do not write data to file")
+        return
+    dir_name = os.path.dirname(file_name)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    np.savetxt(file_name, data.flatten(), fmt=fmt, delimiter='', newline='\n')
+
+
+# prama1: file_name: the file which store the data
+# param2: dtype: data type
+# param3: delim: delimiter which is used to split data
+def read_file_txt(file_name, dtype, delim=None):
+    return np.loadtxt(file_name, dtype=dtype, delimiter=delim).flatten()
+
+
+# prama1: file_name: the file which store the data
+# param2: delim: delimiter which is used to split data
+def read_file_txt_to_boll(file_name, delim=None):
+    in_data = np.loadtxt(file_name, dtype=str, delimiter=delim)
+    bool_data = []
+    for item in in_data:
+        if item == "False":
+            bool_data.append(False)
+        else:
+            bool_data.append(True)
+    return np.array(bool_data)
+
+
+# prama1: data_file: the file which store the generation data
+# param2: shape: data shape
+# param3: dtype: data type
+# param4: rand_type: the method of generate data, select from "randint, uniform"
+# param5: data lower limit
+# param6: data upper limit
+def gen_data_file(data_file, shape, dtype, rand_type, low, high):
+    if rand_type == "randint":
+        rand_data = np.random.randint(low, high, size=shape)
+    else:
+        rand_data = np.random.uniform(low, high, size=shape)
+    data = np.array(rand_data, dtype=dtype)
+    write_file_txt(data_file, data, fmt="%s")
+    return data
+
+
+def config(execute_type):
+    if execute_type == 'cpu':
+        session_config = tf.compat.v1.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    return session_config
+
+def gen_random_data_int8(data_files):
+    np.random.seed(23457)
+    shape_x1 = [3, 4, 5]
+    shape_x2 = [3, 4, 5]
+    a = gen_data_file(data_files[0], shape_x1, np.int8, "randint", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.int8, "randint", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.int8, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.int8, shape=shape_x2)
+    re = tf.math.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_int16(data_files):
+    np.random.seed(23457)
+    shape_x1 = [4]
+    shape_x2 = [3, 4]
+    a = gen_data_file(data_files[0], shape_x1, np.int16, "randint", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.int16, "randint", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.int16, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.int16, shape=shape_x2)
+    re = tf.math.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_int32(data_files):
+    np.random.seed(23457)
+    shape_x1 = [3, 4]
+    shape_x2 = [4]
+    a = gen_data_file(data_files[0], shape_x1, np.int32, "randint", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.int32, "randint", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.int32, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.int32, shape=shape_x2)
+    re = tf.math.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_int64(data_files):
+    np.random.seed(23457)
+    shape_x1 = [3, 4, 5]
+    shape_x2 = [3, 4, 5]
+    a = gen_data_file(data_files[0], shape_x1, np.int64, "randint", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.int64, "randint", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.int64, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.int64, shape=shape_x2)
+    re = tf.math.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_uint8(data_files):
+    np.random.seed(23457)
+    shape_x1 = [3, 4, 5]
+    shape_x2 = [1]
+    a = gen_data_file(data_files[0], shape_x1, np.uint8, "randint", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.uint8, "randint", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.uint8, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.uint8, shape=shape_x2)
+    re = tf.math.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_uint16(data_files):
+    np.random.seed(23457)
+    shape_x1 = [1]
+    shape_x2 = [3, 4, 5]
+    a = gen_data_file(data_files[0], shape_x1, np.uint16, "randint", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.uint16, "randint", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.uint16, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.uint16, shape=shape_x2)
+    re = tf.math.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_float16(data_files):
+    np.random.seed(23457)
+    shape_x1 = [15, 60, 30]
+    shape_x2 = [15, 60, 30]
+    a = gen_data_file(data_files[0], shape_x1, np.float16, "uniform", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.float16, "uniform", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.float16, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.float16, shape=shape_x2)
+    re = tf.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_float(data_files):
+    np.random.seed(23457)
+    shape_x1 = [15, 12, 30]
+    shape_x2 = [15, 12, 30]
+    a = gen_data_file(data_files[0], shape_x1, np.float32, "uniform", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.float32, "uniform", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.float32, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.float32, shape=shape_x2)
+    re = tf.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def gen_random_data_double(data_files):
+    np.random.seed(3457)
+    shape_x1 = [64, 48, 30]
+    shape_x2 = [64, 48, 30]
+    a = gen_data_file(data_files[0], shape_x1, np.float64, "uniform", 1, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.float64, "uniform", 1, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.float64, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.float64, shape=shape_x2)
+    re = tf.floordiv(x1, x2)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    # read_data = read_file_txt_to_boll(data_files[2])
+
+def run():
+    data_files = [
+        "floordiv/data/floordiv_data_input1_1.txt",
+        "floordiv/data/floordiv_data_input2_1.txt",
+        "floordiv/data/floordiv_data_output1_1.txt",
+        "floordiv/data/floordiv_data_input1_2.txt",
+        "floordiv/data/floordiv_data_input2_2.txt",
+        "floordiv/data/floordiv_data_output1_2.txt",
+        "floordiv/data/floordiv_data_input1_3.txt",
+        "floordiv/data/floordiv_data_input2_3.txt",
+        "floordiv/data/floordiv_data_output1_3.txt",
+        "floordiv/data/floordiv_data_input1_4.txt",
+        "floordiv/data/floordiv_data_input2_4.txt",
+        "floordiv/data/floordiv_data_output1_4.txt",
+        "floordiv/data/floordiv_data_input1_5.txt",
+        "floordiv/data/floordiv_data_input2_5.txt",
+        "floordiv/data/floordiv_data_output1_5.txt",
+        "floordiv/data/floordiv_data_input1_6.txt",
+        "floordiv/data/floordiv_data_input2_6.txt",
+        "floordiv/data/floordiv_data_output1_6.txt",
+        "floordiv/data/floordiv_data_input1_7.txt",
+        "floordiv/data/floordiv_data_input2_7.txt",
+        "floordiv/data/floordiv_data_output1_7.txt",
+        "floordiv/data/floordiv_data_input1_8.txt",
+        "floordiv/data/floordiv_data_input2_8.txt",
+        "floordiv/data/floordiv_data_output1_8.txt",
+        "floordiv/data/floordiv_data_input1_9.txt",
+        "floordiv/data/floordiv_data_input2_9.txt",
+        "floordiv/data/floordiv_data_output1_9.txt"
+    ]
+    gen_random_data_int8([data_files[0], data_files[1], data_files[2]])
+    gen_random_data_int16([data_files[3], data_files[4], data_files[5]])
+    gen_random_data_int32([data_files[6], data_files[7], data_files[8]])
+    gen_random_data_int64([data_files[9], data_files[10], data_files[11]])
+    gen_random_data_uint8([data_files[12], data_files[13], data_files[14]])
+    gen_random_data_uint16([data_files[15], data_files[16], data_files[17]])
+    gen_random_data_float16([data_files[18], data_files[19], data_files[20]])
+    gen_random_data_float([data_files[21], data_files[22], data_files[23]])
+    gen_random_data_double([data_files[24], data_files[25], data_files[26]])
---
+++ ops/built-in/tests/ut/aicpu_test/testcase/floordiv/src/floordiv_utest.cpp
@@ -0,0 +1,293 @@
+#include "gtest/gtest.h"
+#ifndef private
+#define private public
+#define protected public
+#endif
+#include "aicpu_test_utils.h"
+#include "cpu_kernel_utils.h"
+#include "node_def_builder.h"
+#include "aicpu_read_file.h"
+#include "utils/eigen_tensor.h"
+#include "utils/kernel_util.h"
+#undef private
+#undef protected
+#include "Eigen/Core"
+
+using namespace std;
+using namespace aicpu;
+
+class TEST_FLOORDIV_UT : public testing::Test {};
+
+template <typename T>
+void CalcExpectWithSameShape(const NodeDef &node_def, T expect_out[]) {
+  auto input0 = node_def.MutableInputs(0);
+  T *input0_data = (T *)input0->GetData();
+  auto input1 = node_def.MutableInputs(1);
+  DataType datatype = input1->GetDataType();
+  T *input1_data = (T *)input1->GetData();
+  int64_t input0_num = input0->NumElements();
+  int64_t input1_num = input1->NumElements();
+  if (input0_num == input1_num) {
+    for (int64_t j = 0; j < input0_num; ++j) {
+      expect_out[j] = Eigen::numext::floor(input0_data[j] / input1_data[j]);
+    }
+  }
+}
+
+template <typename T>
+void CalcExpectWithDiffShape(const NodeDef &node_def, T expect_out[]) {
+  auto input0 = node_def.MutableInputs(0);
+  T *input0_data = (T *)input0->GetData();
+  auto input1 = node_def.MutableInputs(1);
+  DataType datatype = input1->GetDataType();
+  T *input1_data = (T *)input1->GetData();
+  int64_t input0_num = input0->NumElements();
+  int64_t input1_num = input1->NumElements();
+  if (input0_num > input1_num) {
+    for (int64_t j = 0; j < input0_num; ++j) {
+      int64_t i = j % input1_num;
+      expect_out[j] = Eigen::numext::floor(input0_data[j] / input1_data[i]);
+    }
+  }
+}
+
+#define CREATE_NODEDEF(shapes, data_types, datas)                  \
+  auto node_def = CpuKernelUtils::CpuKernelUtils::CreateNodeDef(); \
+  NodeDefBuilder(node_def.get(), "FloorDiv", "FloorDiv")           \      
+      .Input({"x1", data_types[0], shapes[0], datas[0]})           \
+      .Input({"x2", data_types[1], shapes[1], datas[1]})           \
+      .Output({"y", data_types[2], shapes[2], datas[2]})
+
+// read input and output data from files which generate by your python file
+template <typename T1, typename T2, typename T3>
+void RunFloorDivKernel(vector<string> data_files, vector<DataType> data_types,
+                       vector<vector<int64_t>> &shapes) {
+  // read data from file for input1
+  string data_path = ktestcaseFilePath + data_files[0];
+  uint64_t input1_size = CalTotalElements(shapes, 0);
+  T1 *input1 = new T1[input1_size];
+  bool status = ReadFile(data_path, input1, input1_size);
+  EXPECT_EQ(status, true);
+
+  // read data from file for input2
+  data_path = ktestcaseFilePath + data_files[1];
+  uint64_t input2_size = CalTotalElements(shapes, 1);
+  T2 *input2 = new T2[input2_size];
+  status = ReadFile(data_path, input2, input2_size);
+  EXPECT_EQ(status, true);
+
+  uint64_t output_size = CalTotalElements(shapes, 2);
+  T3 *output = new T3[output_size];
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_OK);
+
+  // read data from file for expect ouput
+  data_path = ktestcaseFilePath + data_files[2];
+  T3 *output_exp = new T3[output_size];
+  status = ReadFile(data_path, output_exp, output_size);
+  EXPECT_EQ(status, true);
+
+  bool compare = CompareResult(output, output_exp, output_size);
+  EXPECT_EQ(compare, true);
+  delete[] input1;
+  delete[] input2;
+  delete[] output;
+  delete[] output_exp;
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_INT8_SUCC) {
+  vector<DataType> data_types = {DT_INT8, DT_INT8, DT_INT8};
+  vector<vector<int64_t>> shapes = {{3, 4, 5}, {3, 4, 5}, {3, 4, 5}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_1.txt",
+                       "floordiv/data/floordiv_data_input2_1.txt",
+                       "floordiv/data/floordiv_data_output1_1.txt"};
+  RunFloorDivKernel<int8_t, int8_t, int8_t>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_INT16_SUCC) {
+  vector<DataType> data_types = {DT_INT16, DT_INT16, DT_INT16};
+  vector<vector<int64_t>> shapes = {{4}, {3, 4}, {3, 4}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_2.txt",
+                       "floordiv/data/floordiv_data_input2_2.txt",
+                       "floordiv/data/floordiv_data_output1_2.txt"};
+  RunFloorDivKernel<int16_t, int16_t, int16_t>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_INT32_SUCC) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
+  vector<vector<int64_t>> shapes = {{3, 4}, {4}, {3, 4}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_3.txt",
+                       "floordiv/data/floordiv_data_input2_3.txt",
+                       "floordiv/data/floordiv_data_output1_3.txt"};
+  RunFloorDivKernel<int32_t, int32_t, int32_t>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_INT64_SUCC) {
+  vector<DataType> data_types = {DT_INT64, DT_INT64, DT_INT64};
+  vector<vector<int64_t>> shapes = {{3, 4, 5}, {3, 4, 5}, {3, 4, 5}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_4.txt",
+                       "floordiv/data/floordiv_data_input2_4.txt",
+                       "floordiv/data/floordiv_data_output1_4.txt"};
+  RunFloorDivKernel<int64_t, int64_t, int64_t>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_UINT8_SUCC) {
+  vector<DataType> data_types = {DT_UINT8, DT_UINT8, DT_UINT8};
+  vector<vector<int64_t>> shapes = {{3, 4, 5}, {1}, {3, 4, 5}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_5.txt",
+                       "floordiv/data/floordiv_data_input2_5.txt",
+                       "floordiv/data/floordiv_data_output1_5.txt"};
+  RunFloorDivKernel<uint8_t, uint8_t, uint8_t>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_UINT16_SUCC) {
+  vector<DataType> data_types = {DT_UINT16, DT_UINT16, DT_UINT16};
+  vector<vector<int64_t>> shapes = {{1}, {3, 4, 5}, {3, 4, 5}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_6.txt",
+                       "floordiv/data/floordiv_data_input2_6.txt",
+                       "floordiv/data/floordiv_data_output1_6.txt"};
+  RunFloorDivKernel<uint16_t, uint16_t, uint16_t>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_FLOAT16_SUCC) {
+  vector<DataType> data_types = {DT_FLOAT16, DT_FLOAT16, DT_FLOAT16};
+  vector<vector<int64_t>> shapes = {{15, 60, 30}, {15, 60, 30}, {15, 60, 30}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_7.txt",
+                       "floordiv/data/floordiv_data_input2_7.txt",
+                       "floordiv/data/floordiv_data_output1_7.txt"};
+  RunFloorDivKernel<Eigen::half, Eigen::half, Eigen::half>(files, data_types,
+                                                           shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_FLOAT_SUCC) {
+  vector<DataType> data_types = {DT_FLOAT, DT_FLOAT, DT_FLOAT};
+  vector<vector<int64_t>> shapes = {{15, 12, 30}, {15, 12, 30}, {15, 12, 30}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_8.txt",
+                       "floordiv/data/floordiv_data_input2_8.txt",
+                       "floordiv/data/floordiv_data_output1_8.txt"};
+  RunFloorDivKernel<float, float, float>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, FILE_DATA_TYPE_DOUBLE_SUCC) {
+  vector<DataType> data_types = {DT_DOUBLE, DT_DOUBLE, DT_DOUBLE};
+  vector<vector<int64_t>> shapes = {{64, 48, 30}, {64, 48, 30}, {64, 48, 30}};
+  vector<string> files{"floordiv/data/floordiv_data_input1_9.txt",
+                       "floordiv/data/floordiv_data_input2_9.txt",
+                       "floordiv/data/floordiv_data_output1_9.txt"};
+  RunFloorDivKernel<double, double, double>(files, data_types, shapes);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_BOOL_UNSUPPORT) {
+  vector<DataType> data_types = {DT_BOOL, DT_BOOL, DT_BOOL};
+  vector<vector<int64_t>> shapes = {{2, 11}, {2, 11}, {2, 11}};
+  bool input1[22] = {(bool)1};
+  bool input2[22] = {(bool)0};
+  bool output[22] = {(bool)0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_NULL_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
+  vector<vector<int64_t>> shapes = {{2, 11}, {2, 11}, {2, 11}};
+  int32_t output[22] = {0};
+  vector<void *> datas = {(void *)nullptr, (void *)nullptr, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_DTYPE_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT64, DT_INT32};
+  vector<vector<int64_t>> shapes = {{2, 11}, {2, 11}, {2, 11}};
+  int32_t input1[22] = {(int32_t)1};
+  int64_t input2[22] = {(int64_t)0};
+  int32_t output[22] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_SHAPE_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
+  vector<vector<int64_t>> shapes = {{2, 2, 4}, {2, 2, 3}, {2, 2, 4}};
+  int32_t input1[12] = {(int32_t)1};
+  int32_t input2[16] = {(int32_t)1};
+  int32_t output[16] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_X_ONE_ELEMENT_ZERO_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
+  vector<vector<int64_t>> shapes = {{1}, {2, 2}, {2, 2}};
+  int32_t input1[1] = {(int32_t)1};
+  int32_t input2[4] = {(int32_t)1};
+  input2[1] = (int32_t)0;
+  int32_t output[4] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_INNER_ERROR);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_Y_ONE_ELEMENT_ZERO_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
+  vector<vector<int64_t>> shapes = {{2, 2}, {1}, {2, 2}};
+  int32_t input1[4] = {(int32_t)1};
+  int32_t input2[1] = {(int32_t)0};
+  int32_t output[4] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_INNER_ERROR);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_SAME_SAHPE_ZERO_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
+  vector<vector<int64_t>> shapes = {{2, 2}, {2, 2}, {2, 2}};
+  int32_t input1[4] = {(int32_t)1};
+  int32_t input2[4] = {(int32_t)1};
+  input2[1] = (int32_t)0;
+  int32_t output[4] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_INNER_ERROR);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_BCAST_PARALLEL_ZERO_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT64, DT_INT64, DT_INT64};
+  vector<vector<int64_t>> shapes = {{16, 64, 64}, {64, 64}, {16, 64, 64}};
+  int64_t input1[65536] = {(int64_t)1};
+  int64_t input2[4096] = {(int64_t)1};
+  input2[1] = (int64_t)0;
+  int64_t output[65536] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_INNER_ERROR);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_BCAST_ZERO_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT64, DT_INT64, DT_INT64};
+  vector<vector<int64_t>> shapes = {{3, 4, 5}, {4, 5}, {3, 4, 5}};
+  int64_t input1[60] = {(int64_t)1};
+  int64_t input2[20] = {(int64_t)1};
+  input2[1] = (int64_t)0;
+  int64_t output[60] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_INNER_ERROR);
+}
+
+TEST_F(TEST_FLOORDIV_UT, INPUT_NOBCAST_PARALLEL_ZERO_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT64, DT_INT64, DT_INT64};
+  vector<vector<int64_t>> shapes = {{16, 64, 64}, {16, 64, 64}, {16, 64, 64}};
+  int64_t input1[65536] = {(int64_t)1};
+  int64_t input2[65526] = {(int64_t)1};
+  input2[1] = (int64_t)0;
+  int64_t output[65536] = {0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_INNER_ERROR);
+}
