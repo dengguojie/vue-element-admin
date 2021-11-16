66c7f97690d9c018047c8fcc8992c0c51cc2af67
jinge <jinge3@huawei.com>
2021-11-16 20:40:28 +0800

    modify

--- ops/built-in/tbe/impl/max_pool3d_with_argmax.py
+++ ops/built-in/tbe/impl/max_pool3d_with_argmax.py
@@ -178,9 +178,8 @@ class MaxPool3DWithArgmax(metaclass=ABCMeta):
                                                  remain_repeat_time * mask_value], dup_reg, 1, 1, 8)
 
     def _img2col(self, aux_l1, big_matrix_ub, l1_begin_pos, rep_times):
-        MIN_VALUE_FP16 = -65500
         if self.ub_kernel_stg is UbKernelStrategy.WHOLE_KERNEL:
-            self._vector_dup(big_matrix_ub, 0, big_matrix_ub.shape, MIN_VALUE_FP16)
+            self._vector_dup(big_matrix_ub, 0, big_matrix_ub.shape, 0)
             with self.inst.for_range(0, self.k_d) as tmp_kd:
                 # we adopt the scheme that repeat_mode = 1
                 with self.inst.for_range(0, self.k_h) as tmp_kh:
