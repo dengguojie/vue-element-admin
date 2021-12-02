8321606beb8a6a997cde888c1a9b99106480b4bc
周伟 <zhouwei188@hisilicon.com>
2021-12-02 03:25:35 +0000

    !12220 编码规范 Merge pull request !12220 from 周伟/build_check

--- tools/op_test_frame/python/op_test_frame/runtime/__init__.py
+++ tools/op_test_frame/python/op_test_frame/runtime/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2010-2018. All rights reserved.
-# coding=utf-8
 """init"""
 from .rts_api import AscendRTSApi
