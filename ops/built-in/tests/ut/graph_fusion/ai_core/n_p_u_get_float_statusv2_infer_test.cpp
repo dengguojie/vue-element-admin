5b65480bcc093a2c266ba00083f7a2241bbce772
y_xie <xieyu11@huawei.com>
2021-12-09 11:51:26 +0800

    溢出状态算子新方案

---
+++ ops/built-in/tests/ut/graph_fusion/ai_core/n_p_u_get_float_statusv2_infer_test.cpp
@@ -0,0 +1,37 @@
+#include "nn_batch_norm_ops.h"
+#include "all_ops.h"
+#include "fusion_pass_test_utils.h"
+#include "graph/compute_graph.h"
+#include "graph/graph.h"
+#include "graph/utils/graph_utils.h"
+#include "graph/utils/op_desc_utils.h"
+#include "gtest/gtest.h"
+
+using namespace ge;
+using namespace op;
+
+class n_p_u_get_float_status_v2_infer_test : public testing::Test {
+ protected:
+  static void SetUpTestCase() { std::cout << "n_p_u_get_float_status_v2 SetUp" << std::endl; }
+
+  static void TearDownTestCase() {
+    std::cout << "n_p_u_get_float_status_v2 TearDown" << std::endl;
+  }
+};
+
+TEST_F(n_p_u_get_float_status_v2_infer_test, n_p_u_get_float_status_v2_infer_test_1) {
+  auto graph = std::make_shared<ge::ComputeGraph>("n_p_u_get_float_status_v2_infer_test_1");
+
+  ge::GeShape output_shape({8});
+  ge::GeTensorDesc output_desc(output_shape, ge::FORMAT_ND, ge::DT_FLOAT);
+  bias_desc.SetOriginFormat(ge::FORMAT_ND);
+  bias_desc.SetOriginDataType(ge::DT_FLOAT);
+  bias_desc.SetOriginShape(output_shape);
+
+  ge::OpDescPtr n_p_u_get_float_status_v2= std::make_shared<ge::OpDesc>("NPUGetFloatStatusV2", "NPUGetFloatStatusV2");
+  n_p_u_get_float_status->AddOutputDesc(output_desc);
+  n_p_u_get_float_status->AddInputDesc(output_desc);
+  ge::NodePtr n_p_u_get_float_status_node = graph->AddNode(n_p_u_get_float_status);
+
+  fe::FusionPassTestUtils::InferShapeAndType(graph);
+}
