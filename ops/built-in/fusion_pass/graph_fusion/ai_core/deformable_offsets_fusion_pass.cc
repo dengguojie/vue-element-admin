c234f1986a046673b81239d51685d4ea48138d87
简轶 <jianyi3@hisilicon.com>
2021-12-09 11:17:28 +0000

    !12481 deformable_offsets 算子后冒烟测试 Merge pull request !12481 from 简轶/master

--- ops/built-in/fusion_pass/graph_fusion/ai_core/deformable_offsets_fusion_pass.cc
+++ ops/built-in/fusion_pass/graph_fusion/ai_core/deformable_offsets_fusion_pass.cc
@@ -40,7 +40,6 @@
 
 using namespace ge;
 namespace fe {
-static const uint16_t UINT_NUM_ZERO = 0;
 static const std::string CONSTANTOP = "Constant";
 static const char* FUSED_NODE = "DeformableOffsets";
 static const std::string PATTERN_FUSEDNODE = "DeformableOffsets";
@@ -56,14 +55,6 @@ vector<FusionPattern*> DeformableOffsetsFusionPass::DefinePatterns() {
   patterns.push_back(pattern);
 
   return patterns;
-}
-// transfer nchw format to nhwc 
-void trans_format_shape(vector<int64_t>& format_shape) {
-    int64_t temp_val = 0;
-    temp_val = format_shape[1];
-    format_shape[1] = format_shape[2];
-    format_shape[2] = format_shape[3];
-    format_shape[3] = temp_val;
 }
 
 Status MakeHelperTensorDFM(const vector<int64_t>& offsets_shape, const vector<int64_t>& kernel_sizes,
@@ -108,54 +99,6 @@ Status MakeHelperTensorDFM(const vector<int64_t>& offsets_shape, const vector<in
   return SUCCESS;
 }
 
-Status MakeHelperTensorFP16(const vector<int64_t>& offsets_shape, const vector<int64_t>& kernel_sizes,
-                           const vector<int64_t>& strides, const vector<int64_t>& pads,
-                           const vector<int64_t>& dilations, uint16_t& output1) {
-  uint16_t* output = &output1;
-  fp16_t h_t;
-  h_t.val = 0;
-  fp16_t w_t;
-  w_t.val = 0;
-
-  int64_t H_OUT = offsets_shape[1];
-  int64_t W_OUT = offsets_shape[2];
-  int64_t K_H = kernel_sizes[0];
-  int64_t K_W = kernel_sizes[1];
-
-  int64_t stride_h = strides[1];
-  int64_t stride_w = strides[2];
-  int64_t dilation_h = dilations[1];
-  int64_t dilation_w = dilations[2];
-  int64_t group = offsets_shape[3] / (3 * K_H * K_W);
-  int64_t pad_top = pads[0];
-  int64_t pad_left = pads[2];
-  int64_t h_index = 0;
-  int64_t w_index = 0;
-
-  for (int64_t h = 0; h < H_OUT; h++) {
-    for (int64_t w = 0; w < W_OUT; w++) {
-      for (int64_t g = 0; g < group; g++) {
-        for (int64_t k_h = 0; k_h < K_H; k_h++) {
-          for (int64_t k_w = 0; k_w < K_W; k_w++) {
-          
-            w_index = h * W_OUT * 3 * group * K_H * K_W + w * 3 * group * K_H * K_W + 0 * group * K_H * K_W +
-                      g * K_H * K_W + k_h * K_W + k_w;
-            h_index = h * W_OUT * 3 * group * K_H * K_W + w * 3 * group * K_H * K_W + 1 * group * K_H * K_W +
-                      g * K_H * K_W + k_h * K_W + k_w;
-            // get value to temp value
-            w_t = (float)(w * stride_w - pad_left + k_w * dilation_w);
-            h_t = (float)(h * stride_h - pad_top + k_h * dilation_h);
-            output[w_index] = w_t.val;
-            output[h_index] = h_t.val;
-          }
-        }
-      }
-    }
-  }
-
-  return SUCCESS;
-}
-
 Status DeformableOffsetsFusionPass::Fusion(ge::ComputeGraph& graph, Mapping& mapping, std::vector<NodePtr>& newNodes) {
   // get node
   ge::NodePtr deformableOffsetsNode = GetNodeFromMapping(PATTERN_FUSEDNODE, mapping);
@@ -178,6 +121,7 @@ Status DeformableOffsetsFusionPass::Fusion(ge::ComputeGraph& graph, Mapping& map
   auto offsets_desc = deformableOffsetsDesc->GetInputDesc(1);
   vector<int64_t> x_shape = x_desc.GetShape().GetDims();
   vector<int64_t> offsets_shape = offsets_desc.GetShape().GetDims();
+  vector<int64_t> helper_shape = offsets_desc.GetShape().GetDims();
   // get attrs
   int64_t dfm_groups;
   FUSION_PASS_CHECK(!AttrUtils::GetInt(deformableOffsetsDesc, "deformable_groups", dfm_groups),
@@ -195,66 +139,34 @@ Status DeformableOffsetsFusionPass::Fusion(ge::ComputeGraph& graph, Mapping& map
   FUSION_PASS_CHECK(!AttrUtils::GetListInt(deformableOffsetsDesc, "dilations", dilations),
                     OP_LOGI(FUSED_OP_TYPE.c_str(), "get dilations attr failed."), return PARAM_INVALID);
 
-  Format offsetFormat = x_desc.GetFormat();
-  if(offsetFormat == FORMAT_NCHW){
-    trans_format_shape(offsets_shape);
-    trans_format_shape(strides);
-    trans_format_shape(pads);
-    trans_format_shape(dilations);
-  }
-  vector<int64_t> helper_shape;
-  for (size_t i = 0; i < offsets_shape.size(); i++)
-  {
-    helper_shape.push_back(offsets_shape[i]);
-  }
+  Format helperMatrixFormat = x_desc.GetFormat();
+  FUSION_PASS_CHECK(
+      helperMatrixFormat != FORMAT_NHWC,
+      OP_LOGI(FUSED_OP_TYPE.c_str(), "The format of input is %d, not NHWC, cannot do fusion.", helperMatrixFormat),
+      return NOT_CHANGED);
+
   helper_shape[0] = 1;
-  // get input dtype
-  DataType helper_dtype = x_desc.GetDataType();
   ge::GeTensorPtr helpMatrixPtr = nullptr;
   int64_t matrixSize = offsets_shape[1] * offsets_shape[2] * offsets_shape[3];
+  unique_ptr<float[]> inputAssit(new (std::nothrow) float[matrixSize]());
+  auto retMem = memset_s(inputAssit.get(), matrixSize, 0, matrixSize);
+  FUSION_PASS_CHECK(retMem != EOK, OP_LOGI(FUSED_OP_TYPE.c_str(), "Failed to operate memset_s."), return NOT_CHANGED);
+  Status ret = MakeHelperTensorDFM(offsets_shape, ksize, strides, pads, dilations, *inputAssit.get());
+  FUSION_PASS_CHECK(ret != SUCCESS, OP_LOGI(FUSED_OP_TYPE.c_str(), "MakeHelperTensorDFM failed."), return NOT_CHANGED);
 
   // define the shape of auxiliary matrix
   ge::GeTensorDesc tensorDesc;
   ge::GeShape helperShape(helper_shape);
   tensorDesc.SetShape(helperShape);
   tensorDesc.SetOriginShape(helperShape);
-  tensorDesc.SetFormat(FORMAT_NHWC);
-  tensorDesc.SetOriginFormat(FORMAT_NHWC);
-  tensorDesc.SetDataType(helper_dtype);
-  tensorDesc.SetOriginDataType(helper_dtype);
-  // create const graph node 
-  if (helper_dtype == ge::DT_FLOAT) {
-      unique_ptr<float[]> inputAssit(new (std::nothrow) float[matrixSize]());
-      auto retMem = memset_s(inputAssit.get(), matrixSize, 0, matrixSize);
-      FUSION_PASS_CHECK(retMem != EOK, 
-                        OP_LOGI(FUSED_OP_TYPE.c_str(),
-                        "Failed to operate memset_s."),
-                        return NOT_CHANGED);
-      // get assist node to matirx
-      Status ret = MakeHelperTensorDFM(offsets_shape, ksize, strides, pads, dilations, *inputAssit.get());
-      FUSION_PASS_CHECK(ret != SUCCESS, OP_LOGI(FUSED_OP_TYPE.c_str(), "MakeHelperTensorDFM failed."), return NOT_CHANGED);
-      FUSION_PASS_MAKE_SHARED((helpMatrixPtr = std::make_shared<ge::GeTensor>(
-                               tensorDesc,
-                               reinterpret_cast<uint8_t*>(inputAssit.get()), matrixSize * sizeof(float))),
-                               helpMatrixPtr = nullptr;
-                               return PARAM_INVALID);
-  } else if (helper_dtype == ge::DT_FLOAT16) {
-      unique_ptr<uint16_t[]> inputAssit(new (std::nothrow) uint16_t[matrixSize]());
-      FUSION_PASS_CHECK(inputAssit.get() == nullptr, OP_LOGE(FUSED_OP_TYPE.c_str(), "inputAssit is NULL"),
-                        return PARAM_INVALID);
-      Status ret = NnSet(matrixSize, UINT_NUM_ZERO, *reinterpret_cast<uint16_t*>(inputAssit.get()));
-      FUSION_PASS_CHECK(ret != SUCCESS, OP_LOGE(FUSED_OP_TYPE.c_str(), "NnSet failed."), return ret);
-      ret = MakeHelperTensorFP16(offsets_shape, ksize, strides, pads, dilations, *inputAssit.get());
-      FUSION_PASS_CHECK(ret != SUCCESS, OP_LOGE(FUSED_OP_TYPE.c_str(), "MakeHelperTensorFP16 failed."), return ret);
-      FUSION_PASS_MAKE_SHARED(
-          (helpMatrixPtr = std::make_shared<ge::GeTensor>(tensorDesc, reinterpret_cast<uint8_t*>(inputAssit.get()),
-                                                     matrixSize * sizeof(uint16_t))),
-          helpMatrixPtr = nullptr;
-          return PARAM_INVALID);
-  } else {
-      OP_LOGE(FUSED_OP_TYPE.c_str(), "unsupport dtype, please check!");
-      return PARAM_INVALID;
-  }
+  tensorDesc.SetFormat(helperMatrixFormat);
+  tensorDesc.SetOriginFormat(helperMatrixFormat);
+  tensorDesc.SetDataType(ge::DT_FLOAT);
+  tensorDesc.SetOriginDataType(ge::DT_FLOAT);
+  FUSION_PASS_MAKE_SHARED((helpMatrixPtr = std::make_shared<ge::GeTensor>(
+                               tensorDesc, reinterpret_cast<uint8_t*>(inputAssit.get()), matrixSize * sizeof(float))),
+                          helpMatrixPtr = nullptr;
+                          return PARAM_INVALID);
   // add const node
   ge::OpDescPtr const_ptr = ge::OpDescUtils::CreateConstOp(helpMatrixPtr);
   ge::NodePtr const_node = graph.AddNode(const_ptr);
