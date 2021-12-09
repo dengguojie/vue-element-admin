09aa2df57d7276c53607447fa5e3a2155e30a7fc
y_xie <xieyu11@huawei.com>
2021-12-09 10:47:44 +0800

    溢出状态算子新方案

--- ops/built-in/tests/ut/ops_test/NPUGetFloatStatusV2/test_NPUGetFloatStatusV2_impl.py
+++ ops/built-in/tests/ut/ops_test/NPUGetFloatStatusV2/test_NPUGetFloatStatusV2_impl.py
@@ -3,31 +3,36 @@
 from op_test_frame.ut import OpUT
 ut_case = OpUT("NPUGetFloatStatusV2", None, None)
 
-case1 = {"params": [{"shape": (1,2,4), "dtype": "float32", "format": "ND", "ori_shape": (1,2,4),"ori_format": "ND"},
+case1 = {"params": [[{"shape": (1,2,4), "dtype": "float32", "format": "ND", "ori_shape": (1,2,4),"ori_format": "ND"},
+                     {"shape": (1,2,4), "dtype": "float32", "format": "ND", "ori_shape": (1,2,4),"ori_format": "ND"}],
                     {"shape": (1,2,4), "dtype": "float32", "format": "ND", "ori_shape": (1,2,4),"ori_format": "ND"}],
          "case_name": "n_p_u_get_float_status_v2_1",
          "expect": "success",
          "format_expect": [],
          "support_expect": True}
-case2 = {"params": [{"shape": (16,16), "dtype": "float32", "format": "ND", "ori_shape": (16,16),"ori_format": "ND"},
+case2 = {"params": [[{"shape": (16,16), "dtype": "float32", "format": "ND", "ori_shape": (16,16),"ori_format": "ND"},
+                     {"shape": (16,16), "dtype": "float32", "format": "ND", "ori_shape": (16,16),"ori_format": "ND"}],
                     {"shape": (16,16), "dtype": "float32", "format": "ND", "ori_shape": (16,16),"ori_format": "ND"}],
          "case_name": "n_p_u_get_float_status_v2_2",
          "expect": "success",
          "format_expect": [],
          "support_expect": True}
-case3 = {"params": [{"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"},
+case3 = {"params": [[{"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"},
+                     {"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"}],
                     {"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"}],
          "case_name": "n_p_u_get_float_status_v2_3",
          "expect": "success",
          "format_expect": [],
          "support_expect": True}
-case4 = {"params": [{"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"},
+case4 = {"params": [[{"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"},
+                     {"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"}],
                     {"shape": (32, 2, 4, 16), "dtype": "float32", "format": "ND", "ori_shape": (32, 2, 4, 16),"ori_format": "ND"}],
          "case_name": "n_p_u_get_float_status_v2_4",
          "expect": "success",
          "format_expect": [],
          "support_expect": True}
-case5 = {"params": [{"shape": (1, 2), "dtype": "float32", "format": "ND", "ori_shape": (1, 2),"ori_format": "ND"},
+case5 = {"params": [[{"shape": (1, 2), "dtype": "float32", "format": "ND", "ori_shape": (1, 2),"ori_format": "ND"},
+                     {"shape": (1, 2), "dtype": "float32", "format": "ND", "ori_shape": (1, 2),"ori_format": "ND"}],
                     {"shape": (1, 2), "dtype": "float32", "format": "ND", "ori_shape": (1, 2),"ori_format": "ND"}],
          "case_name": "n_p_u_get_float_status_v2_5",
          "expect": "success",
