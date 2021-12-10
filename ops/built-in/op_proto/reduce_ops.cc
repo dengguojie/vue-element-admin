d8d78ccd4955797bfa9b73d296d896041b893cb7
shilingling <1921776352@qq.com>
2021-12-10 12:30:14 +0000

    !12521 Operator ReduceLogSumExp Merge pull request !12521 from shilingling/master

--- ops/built-in/op_proto/reduce_ops.cc
+++ ops/built-in/op_proto/reduce_ops.cc
@@ -2037,4 +2037,25 @@ INFER_FUNC_REG(ReduceStdV2Update, ReduceStdV2UpdateInferShape);
 VERIFY_FUNC_REG(ReduceStdV2Update, ReduceStdV2UpdateVerify);
 // ----------------ReduceStdV2Update END---------------------
 
+// ----------------ReduceLogSumExp Op-------------------
+IMPLEMT_COMMON_INFERFUNC(ReduceLogSumExpInferShape) {
+    std::chrono::time_point<std::chrono::steady_clock> before_infer, after_infer;
+    if (prof_switch) {
+        before_infer = std::chrono::steady_clock::now();
+    }
+    OP_LOGD(op.GetName().c_str(), "Enter Start ReduceLogSumExpInferShape");
+    if (InferReduceShapeProcess(op, "x", "axes", "keep_dims")) {
+        if (prof_switch) {
+            after_infer = std::chrono::steady_clock::now();
+            auto t0 = std::chrono::duration_cast<std::chrono::microseconds>(after_infer - before_infer).count();
+            GEEVENT("[REDUCE_INFER_PROF] opname[%s]: total: %d(us)", op.GetName().c_str(), static_cast<int>(t0));
+        }
+        return GRAPH_SUCCESS;
+    }
+    return GRAPH_FAILED;
+}
+
+COMMON_INFER_FUNC_REG(ReduceLogSumExp, ReduceLogSumExpInferShape);
+// ----------------ReduceLogSumExp END-------------------
+
 }  // namespace ge
---
+++ ops/built-in/tests/ut/ops_test/ReduceLogSumExp/test_dynamic_ReduceLogSumExp_impl.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from op_test_frame.ut import OpUT
+
+ut_case = OpUT("ReduceLogSumExp", "impl.dynamic.reduce_log_sum_exp", "reduce_log_sum_exp")
+
+def gen_dynamic_reduce_log_sum_exp_case(shape_x, range_x, 
+                                        shape_axes, range_axes, 
+                                        shape_y, range_y, 
+                                        dtype_val, data_format, 
+                                        ori_shape_x, ori_shape_axes, ori_shape_y, 
+                                        keepdims, 
+                                        kernel_name_val, 
+                                        expect):
+
+    return {"params": [{"shape": shape_x, "dtype": dtype_val,
+                        "range": range_x, "format": data_format,
+                        "ori_shape": ori_shape_x, "ori_format": data_format},
+                       {"shape": shape_axes, "dtype": "int32",
+                        "range": range_axes, "format": data_format,
+                        "ori_shape": ori_shape_axes, "ori_format": data_format},
+                       {"shape": shape_y, "dtype": dtype_val,
+                        "range": range_y, "format": data_format,
+                        "ori_shape": ori_shape_y, "ori_format": data_format}, keepdims],
+            "case_name": kernel_name_val,
+            "expect": expect,
+            "format_expect": [],
+            "support_expect": True}
+
+ut_case.add_case("all", gen_dynamic_reduce_log_sum_exp_case((-1,3,-1,2), [(1,None), (3,3), (1,None), (2,2)],
+                                       (1,), [(1,1),], 
+                                       (-1,1,-1,2), [(1,None), (1,1), (1,None), (2,2)], 
+                                       "float16", "ND", 
+                                       (4,3,4,2), (1, ), (4,1,4,2),
+                                       True, 
+                                       "dynamic_reduce_log_sum_exp_float16_true", 
+                                       "success"))
+ut_case.add_case("all", gen_dynamic_reduce_log_sum_exp_case((4,3,4,2), [(4,4), (3,3), (4,4), (2,2)],
+                                       (1,), [(1,1),], 
+                                       (4,4,2), [(4,4), (4,4), (2,2)], 
+                                       "float32", "ND", 
+                                       (4,3,4,2), (1, ), (4,4,2),
+                                       False, 
+                                       "static_reduce_log_sum_exp_float32_false", 
+                                       "success"))
+
+if __name__ == "__main__":
+    ut_case.run("Ascend910A")
+    ut_case.run("Ascend310")
+
