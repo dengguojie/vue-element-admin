6ce1381fc7a9aed2154df7484421d8e66f5a76fa
Fu Jingguo <fujingguo1@hisilicon.com>
2022-01-04 01:20:26 +0000

    !13135 clean up code warning Merge pull request !13135 from Fu Jingguo/master

--- ops/built-in/framework/tf_plugin/npu_onnx_graph_op_plugin.cc
+++ ops/built-in/framework/tf_plugin/npu_onnx_graph_op_plugin.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Huawei Technologies Co., Ltd
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,16 +21,29 @@
 #include "register/register.h"
 
 namespace domi {
+const int64_t npuOnnxGraphOpInputPortNameLen = 4;
+const int64_t npuOnnxGraphOpInputAttrNameLen = 10;
+const int64_t npuOnnxGraphOpOutputPortNameLen = 6;
+const int64_t npuOnnxGraphOpOutputAttrNameLen = 11;
 
-Status AutoMappingNpuOnnxGraphOpPartitionedCall(const ge::Operator &op_src, ge::Operator &op_dest) {
-  std::vector<DynamicInputOutputInfo> value;
-  DynamicInputOutputInfo input(kInput, "args", 4, "_input_num", 10);
-  value.push_back(input);
-  DynamicInputOutputInfo output(kOutput, "output", 6, "_output_num", 11);
-  value.push_back(output);
-  return AutoMappingByOpFnDynamic(op_src, op_dest, value);
+Status AutoMappingNpuOnnxGraphOpPartitionedCall(const ge::Operator &op_src,
+                                                ge::Operator &op_dest)
+{
+    std::vector<DynamicInputOutputInfo> value;
+    DynamicInputOutputInfo input(kInput, "args",
+                                 npuOnnxGraphOpInputPortNameLen,
+                                 "_input_num",
+                                 npuOnnxGraphOpInputAttrNameLen);
+    value.push_back(input);
+    DynamicInputOutputInfo output(kOutput, "output",
+                                  npuOnnxGraphOpOutputPortNameLen,
+                                  "_output_num",
+                                  npuOnnxGraphOpOutputAttrNameLen);
+    value.push_back(output);
+    return AutoMappingByOpFnDynamic(op_src, op_dest, value);
 }
 
+// Register NpuOnnxGraphOp PartitionedCall to GE
 REGISTER_CUSTOM_OP("PartitionedCall")
     .FrameworkType(TENSORFLOW)
     .OriginOpType("NpuOnnxGraphOp")
