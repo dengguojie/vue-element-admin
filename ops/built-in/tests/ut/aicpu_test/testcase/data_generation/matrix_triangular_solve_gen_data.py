21e76fca723f8c4e9755b7ef697286d0890b3c2f
胡家豪 <631572878@qq.com>
2022-01-22 01:28:23 +0000

    !11286 提交MatrixTriangularSolve算子 Merge pull request !11286 from 胡家豪/MatrixTriangularSolve

--- ops/built-in/tests/ut/aicpu_test/testcase/data_generation/matrix_triangular_solve_gen_data.py
+++ ops/built-in/tests/ut/aicpu_test/testcase/data_generation/matrix_triangular_solve_gen_data.py
@@ -1,5 +1,5 @@
 """
-Copyright 2020 Huawei Technologies Co., Ltd
+Copyright 2021 Huawei Technologies Co., Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@ http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.       \
 See the License for the specific language governing permissions and
 limitations under the License.
 """
@@ -25,27 +25,45 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.python.framework import constant_op, dtypes
 
-# prama1: file_name: the file which store the data
-# param2: data: data which will be stored
-# param3: fmt: format
+
 def write_file_txt(file_name, data, fmt="%s"):
+    # prama1: file_name: the file which store the data
+    # param2: data: data which will be stored
+    # param3: fmt: format
     if (file_name is None):
         print("file name is none, do not write data to file")
         return
     dir_name = os.path.dirname(file_name)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
-    np.savetxt(file_name, data.flatten(), fmt=fmt, delimiter='', newline='\n')
+    data = data.flatten()
+    np.savetxt(file_name, data, fmt=fmt, delimiter=' ', newline='\n')
 
-# prama1: file_name: the file which store the data
-# param2: dtype: data type
-# param3: delim: delimiter which is used to split data
+
+def gen_data_file(data_file, shape, dtype, rand_type, low, high):
+    if rand_type == "randcomplex":
+        rand_data_real = np.random.uniform(low, high, size=shape)
+        rand_data_imag = np.random.uniform(low, high, size=shape)
+        rand_data = []
+        for i in range(len(rand_data_real)):
+            rand_data.append(rand_data_real[i] + rand_data_imag[i] * 1j)
+    else:
+        rand_data = np.random.uniform(low, high, size=shape)
+    data = np.array(rand_data, dtype=dtype)
+    write_file_txt(data_file, data, fmt="%s")
+    return data
+
+
 def read_file_txt(file_name, dtype, delim=None):
+    # prama1: file_name: the file which store the data
+    # param2: dtype: data type
+    # param3: delim: delimiter which is used to split data
     return np.loadtxt(file_name, dtype=dtype, delimiter=delim).flatten()
 
-# prama1: file_name: the file which store the data
-# param2: delim: delimiter which is used to split data
-def read_file_txt_to_bool(file_name, delim=None):
+
+def read_file_txt_to_boll(file_name, delim=None):
+    # prama1: file_name: the file which store the data
+    # param2: delim: delimiter which is used to split data
     in_data = np.loadtxt(file_name, dtype=str, delimiter=delim)
     bool_data = []
     for item in in_data:
@@ -55,25 +73,6 @@ def read_file_txt_to_bool(file_name, delim=None):
             bool_data.append(True)
     return np.array(bool_data)
 
-# prama1: data_file: the file which store the generation data
-# param2: shape: data shape
-# param3: dtype: data type
-# param4: rand_type: the method of generate data, select from "randint, uniform"
-# param5: data lower limit
-# param6: data upper limit
-def gen_data_file(data_file, shape, dtype, rand_type, low, high):
-    if rand_type == "randint":
-        rand_data = np.random.randint(low, high, size=shape)
-    else:
-        rand_data = np.random.uniform(low, high, size=shape)
-    if dtype == np.float16:
-        data = np.array(rand_data, dtype=np.float32)
-        write_file_txt(data_file, data, fmt="%s")
-        data = np.array(data, dtype=np.float16)
-    else:
-        data = np.array(rand_data, dtype=dtype)
-        write_file_txt(data_file, data, fmt="%s")
-    return data
 
 def config(execute_type):
     if execute_type == 'cpu':
@@ -83,42 +82,83 @@ def config(execute_type):
         )
     return session_config
 
+
 def gen_random_data_float():
-    data_files=["matrix_triangular_solve/data/matrix_triangular_solve_data_input1_1.txt",
-                "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_1.txt",
-                "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_1.txt"]
+    data_files = ["matrix_triangular_solve/data/matrix_triangular_solve_data_input1_1.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_1.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_1.txt"]
     np.random.seed(23457)
-    shape_x1 = [4,4]
-    shape_x2 = [4,1]
-    a = gen_data_file(data_files[0], shape_x1, np.float32, "uniform", 0, 100)
-    b = gen_data_file(data_files[1], shape_x2, np.float32, "uniform", 0, 100)
+    shape_x1 = [2, 2, 3, 3]
+    shape_x2 = [2, 2, 3, 2]
+    a = gen_data_file(data_files[0], shape_x1, np.float32, "uniform", -1000, 1000)
+    b = gen_data_file(data_files[1], shape_x2, np.float32, "uniform", -1000, 1000)
 
     x1 = tf.compat.v1.placeholder(tf.float32, shape=shape_x1)
     x2 = tf.compat.v1.placeholder(tf.float32, shape=shape_x2)
-    re = tf.linalg.triangular_solve(x1, x2, lower = True)
+    re = tf.linalg.triangular_solve(x1, x2, True, True)
     with tf.compat.v1.Session(config=config('cpu')) as session:
-        data = session.run(re, feed_dict={x1:a, x2:b})
+        data = session.run(re, feed_dict={x1: a, x2: b})
     write_file_txt(data_files[2], data, fmt="%s")
 
 
 def gen_random_data_double():
-    data_files=["matrix_triangular_solve/data/matrix_triangular_solve_data_input1_2.txt",
-                "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_2.txt",
-                "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_2.txt"]
+    data_files = ["matrix_triangular_solve/data/matrix_triangular_solve_data_input1_2.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_2.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_2.txt"]
     np.random.seed(23457)
-    shape_x1 = [4,4]
-    shape_x2 = [4,1]
+    shape_x1 = [5, 5]
+    shape_x2 = [5, 2]
     a = gen_data_file(data_files[0], shape_x1, np.float64, "uniform", 0, 100)
     b = gen_data_file(data_files[1], shape_x2, np.float64, "uniform", 0, 100)
 
-    x1 = tf.compat.v1.placeholder(tf.float64, shape=shape_x1)
-    x2 = tf.compat.v1.placeholder(tf.float64, shape=shape_x2)
-    re = tf.linalg.triangular_solve(x1, x2, lower = True)
+    x1 = tf.compat.v1.placeholder(tf.double, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.double, shape=shape_x2)
+    re = tf.linalg.triangular_solve(x1, x2, False, False)
     with tf.compat.v1.Session(config=config('cpu')) as session:
-        data = session.run(re, feed_dict={x1:a, x2:b})
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+
+
+def gen_random_data_complex64():
+    data_files = ["matrix_triangular_solve/data/matrix_triangular_solve_data_input1_3.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_3.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_3.txt"]
+    np.random.seed(23457)
+    shape_x1 = [10, 10, 10]
+    shape_x2 = [10, 10, 10]
+    a = gen_data_file(data_files[0], shape_x1, np.complex64, "randcomplex", -100, 0)
+    b = gen_data_file(data_files[1], shape_x2, np.complex64, "randcomplex", -100, 0)
+
+    x1 = tf.compat.v1.placeholder(tf.complex64, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.complex64, shape=shape_x2)
+    re = tf.linalg.triangular_solve(x1, x2, False, True)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
+    write_file_txt(data_files[2], data, fmt="%s")
+    data = data[0]
+
+
+def gen_random_data_complex128():
+    data_files = ["matrix_triangular_solve/data/matrix_triangular_solve_data_input1_4.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_4.txt",
+                  "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_4.txt"]
+    np.random.seed(23457)
+    shape_x1 = [1, 10, 10]
+    shape_x2 = [1, 10, 1]
+
+    a = gen_data_file(data_files[0], shape_x1, np.complex128, "randcomplex", -100, 100)
+    b = gen_data_file(data_files[1], shape_x2, np.complex128, "randcomplex", -100, 100)
+
+    x1 = tf.compat.v1.placeholder(tf.complex128, shape=shape_x1)
+    x2 = tf.compat.v1.placeholder(tf.complex128, shape=shape_x2)
+    re = tf.linalg.triangular_solve(x1, x2, True, False)
+    with tf.compat.v1.Session(config=config('cpu')) as session:
+        data = session.run(re, feed_dict={x1: a, x2: b})
     write_file_txt(data_files[2], data, fmt="%s")
 
 
 def run():
     gen_random_data_float()
     gen_random_data_double()
+    gen_random_data_complex64()
+    gen_random_data_complex128()
--- ops/built-in/tests/ut/aicpu_test/testcase/matrix_triangular_solve/src/matrix_triangular_solve_utest.cpp
+++ ops/built-in/tests/ut/aicpu_test/testcase/matrix_triangular_solve/src/matrix_triangular_solve_utest.cpp
@@ -3,96 +3,197 @@
 #define private public
 #define protected public
 #endif
-#include "aicpu_read_file.h"
 #include "aicpu_test_utils.h"
 #include "cpu_kernel_utils.h"
 #include "node_def_builder.h"
+#include "aicpu_read_file.h"
 #undef private
 #undef protected
 #include "Eigen/Core"
+#include "Eigen/Dense"
+#include "complex"
+#include <cmath>
 
 using namespace std;
+using namespace Eigen;
 using namespace aicpu;
 
-class TEST_MATRIX_TRIANFULAR_SOLVE_UT : public testing::Test {};
+class TEST_MATRIXTRIANGULARSOLVE_UT : public testing::Test {};
 
-#define CREATE_NODEDEF(shapes, data_types, datas, lower, adjoint)  \
-  auto node_def = CpuKernelUtils::CpuKernelUtils::CreateNodeDef(); \
-  NodeDefBuilder(node_def.get(), "MatrixTriangularSolve",          \
-                 "MatrixTriangularSolve")                          \
-      .Input({"x1", data_types[0], shapes[0], datas[0]})           \
-      .Input({"x2", data_types[1], shapes[1], datas[1]})           \
-      .Output({"y", data_types[2], shapes[2], datas[2]})           \
-      .Attr("lower", lower)                                        \
-      .Attr("adjoint", adjoint)
+#define CREATE_NODEDEF(shapes, data_types, datas, lowerattr, adjointattr)          \
+  auto node_def = CpuKernelUtils::CpuKernelUtils::CreateNodeDef();                 \
+  NodeDefBuilder(node_def.get(), "MatrixTriangularSolve", "MatrixTriangularSolve") \
+      .Input({"matrix", (data_types)[0], (shapes)[0], (datas)[0]})                 \
+      .Input({"rhs", (data_types)[1], (shapes)[1], (datas)[1]})                    \
+      .Attr("lower", lowerattr)                                                    \
+      .Attr("adjoint", adjointattr)                                                \
+      .Output({"y", (data_types)[2], (shapes)[2], (datas)[2]})
+
+template <typename T>
+bool FloatCompareResult(T output[], T expect_output[], uint64_t num) {
+  bool result = true;
+  for (uint64_t i = 0; i < num; ++i) {
+    if (fabs(output[i] - expect_output[i]) > 0.001) {
+      std::cout << "output[" << i << "] = ";
+      std::cout << output[i];
+      std::cout << ", expect_output[" << i << "] = ";
+      std::cout << expect_output[i] << std::endl;
+      result = false;
+    }
+  }
+  return result;
+}
 
 // read input and output data from files which generate by your python file
-template<typename T1, typename T2, typename T3>
-void RunMatrixTriangularSolveKernel(vector<string> data_files,
-               vector<DataType> data_types,
-               vector<vector<int64_t>> &shapes) {
+template <typename T1, typename T2, typename T3>
+void RunMatrixTriangularSolveKernel(vector<string> data_files, 
+  vector<DataType> data_types,
+  vector<vector<int64_t>>& shapes, 
+  bool lowerattr, 
+  bool adjointattr) {
   // read data from file for input1
-  string data_path = ktestcaseFilePath + data_files[0];
-  uint64_t input1_size = CalTotalElements(shapes, 0);
-  T1 *input1 = new T1[input1_size];
+  string data_path = ktestcaseFilePath + data_files[0]; 
+  uint64_t input1_size = CalTotalElements(shapes, 0);  
+  T1 input1[input1_size];
   bool status = ReadFile(data_path, input1, input1_size);
   EXPECT_EQ(status, true);
 
   // read data from file for input2
   data_path = ktestcaseFilePath + data_files[1];
   uint64_t input2_size = CalTotalElements(shapes, 1);
-  T2 *input2 = new T2[input2_size];
+  T2 input2[input2_size];
   status = ReadFile(data_path, input2, input2_size);
   EXPECT_EQ(status, true);
 
+  //Count how many outputs there are, and create an array.
   uint64_t output_size = CalTotalElements(shapes, 2);
-  T3 *output = new T3[output_size];
-  vector<void *> datas = {(void *)input1,
-                          (void *)input2,
-                          (void *)output};
-
-  CREATE_NODEDEF(shapes, data_types, datas, true, false);
+  T3 output[output_size];
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  //Create an operator instance 
+  CREATE_NODEDEF(shapes, data_types, datas, lowerattr, adjointattr);
   RUN_KERNEL(node_def, HOST, KERNEL_STATUS_OK);
 
   // read data from file for expect ouput
   data_path = ktestcaseFilePath + data_files[2];
-  T3 *output_exp = new T3[output_size];
+  T3 output_exp[output_size];
   status = ReadFile(data_path, output_exp, output_size);
   EXPECT_EQ(status, true);
 
-  bool compare = CompareResult(output, output_exp, output_size);
+  bool compare = FloatCompareResult(output, output_exp, output_size);  //check
   EXPECT_EQ(compare, true);
-  delete [] input1;
-  delete [] input2;
-  delete [] output;
-  delete [] output_exp;
 }
 
-TEST_F(TEST_MATRIX_TRIANFULAR_SOLVE_UT, SUCCESS) {
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, DATA_TYPE_FLOAT_SUCC) {
   vector<DataType> data_types = {DT_FLOAT, DT_FLOAT, DT_FLOAT};
-  vector<vector<int64_t>> shapes = {{4, 4}, {4, 1}, {4, 1}};
-  vector<string> files{"matrix_triangular_solve/data/matrix_triangular_solve_data_input1_1.txt",
-                       "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_1.txt",
-                       "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_1.txt"};
-  RunMatrixTriangularSolveKernel<float, float, float>(files, data_types, shapes);
+  vector<vector<int64_t>> shapes = {{2, 2, 3, 3}, {2, 2, 3, 2}, {2, 2, 3, 2}};
+  vector<string> files{
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input1_1.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_1.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_1.txt"};
+  RunMatrixTriangularSolveKernel<float, float, float>(files, data_types, shapes, 
+   true, true);
 }
 
-TEST_F(TEST_MATRIX_TRIANFULAR_SOLVE_UT, Double_SUCCESS) {
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, DATA_TYPE_DOUBLE_SUCC) {
   vector<DataType> data_types = {DT_DOUBLE, DT_DOUBLE, DT_DOUBLE};
-  vector<vector<int64_t>> shapes = {{4, 4}, {4, 1}, {4, 1}};
-  vector<string> files{"matrix_triangular_solve/data/matrix_triangular_solve_data_input1_2.txt",
-                       "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_2.txt",
-                       "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_2.txt"};
-  RunMatrixTriangularSolveKernel<double, double, double>(files, data_types, shapes);
+  vector<vector<int64_t>> shapes = {{5, 5}, {5, 2}, {5, 2}};
+  vector<string> files{
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input1_2.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_2.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_2.txt"};
+  RunMatrixTriangularSolveKernel<double, double, double>(files, data_types, shapes,
+   false, false);
 }
 
-TEST_F(TEST_MATRIX_TRIANFULAR_SOLVE_UT, Int32_Failed) {
-  vector<DataType> data_types = {DT_INT32, DT_INT32, DT_INT32};
-  vector<vector<int64_t>> shapes = {{4, 4}, {4, 1}, {4, 1}};
-  int32_t input0[16] = {3, 0, 0, 0, 2, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1};
-  int32_t input1[4] = {4, 2, 4, 2};
-  int32_t output[40] = {0};
-  vector<void *> datas = {(void *)input0, (void *)input1, (void *)output};
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, DATA_TYPE_COMPLEX64_SUCC) {
+  vector<DataType> data_types = {DT_COMPLEX64, DT_COMPLEX64, DT_COMPLEX64};
+  vector<vector<int64_t>> shapes = {{10, 10, 10}, {10, 10, 10}, {10, 10, 10}};
+  vector<string> files{
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input1_3.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_3.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_3.txt"};
+  RunMatrixTriangularSolveKernel<std::complex<float>, std::complex<float>, 
+   std::complex<float>>(files, data_types,shapes, false, true);
+}
+
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, DATA_TYPE_COMPLEX128_SUCC) {
+  vector<DataType> data_types = {DT_COMPLEX128, DT_COMPLEX128, DT_COMPLEX128};
+  vector<vector<int64_t>> shapes = {{1, 10, 10}, {1, 10, 1}, {1, 10, 1}};
+  vector<string> files{
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input1_4.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_input2_4.txt",
+    "matrix_triangular_solve/data/matrix_triangular_solve_data_output1_4.txt"};
+  RunMatrixTriangularSolveKernel<std::complex<double>, std::complex<double>, 
+  std::complex<double>>(files, data_types, shapes, true, false);
+}
+
+// exception instance  
+//not square
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, INPUT_SHAPE_EXCEPTION) {
+  vector<DataType> data_types = {DT_FLOAT, DT_FLOAT, DT_FLOAT};
+  vector<vector<int64_t>> shapes = {{2, 2, 4}, {2, 2, 4}, {2, 2, 4}};
+  float input1[16] = {(float)1};
+  float input2[16] = {(float)1};
+  float output[16] = {(float)0};
+  vector<void *> datas = {(void *)input1,(void *)input2,(void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas, true, false);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+//unsupported type
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, INPUT_DTYPE_EXCEPTION) {
+  vector<DataType> data_types = {DT_INT32, DT_INT32,DT_INT32};
+  vector<vector<int64_t>> shapes = {{2, 2}, {2, 2}, {2, 2}};
+  int32_t input1[4] = {(int32_t)1};
+  int32_t input2[4] = {(int32_t)1};
+  int32_t output[4] = {(int32_t)0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas, true, false);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+//input0 null
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, INPUT0_NULL_EXCEPTION) {
+  vector<DataType> data_types = {DT_FLOAT, DT_FLOAT, DT_FLOAT};
+  vector<vector<int64_t>> shapes = {{7, 7}, {7, 7}, {7}};
+  float input2[49] = {(float)1};
+  float output[7] = {(float)0};
+  vector<void *> datas = {(void *)nullptr,(void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas, true, false);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+//input1 null
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, INPUT1_NULL_EXCEPTION) {
+  vector<DataType> data_types = {DT_FLOAT, DT_FLOAT, DT_FLOAT};
+  vector<vector<int64_t>> shapes = {{8, 8}, {8, 8}, {8}};
+  float input1[64] = {(float)1};
+  float output[8] = {(float)0};
+  vector<void *> datas = {(void *)input1,(void *)nullptr, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas, true, false);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+//mismatched shape
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, MISMATCHED_SHAPE_EXCEPTION) {
+  vector<DataType> data_types = {DT_FLOAT, DT_FLOAT, DT_FLOAT};
+  vector<vector<int64_t>> shapes = {{7, 7}, {6, 7}, {3, 2}};
+  float input1[49] = {(float)1};
+  float input2[42] = {(float)1};
+  float output[6] = {(float)0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
+  CREATE_NODEDEF(shapes, data_types, datas, true, false);
+  RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
+}
+
+//mismatched type
+TEST_F(TEST_MATRIXTRIANGULARSOLVE_UT, MISMATCHED_DTYPE_EXCEPTION) {
+  vector<DataType> data_types = {DT_FLOAT, DT_DOUBLE, DT_FLOAT};
+  vector<vector<int64_t>> shapes = {{3, 3}, {3, 3}, {3, 3}};
+  int32_t input1[9] = {(int32_t)1};
+  int32_t input2[9] = {(int32_t)1};
+  int32_t output[9] = {(int32_t)0};
+  vector<void *> datas = {(void *)input1, (void *)input2, (void *)output};
   CREATE_NODEDEF(shapes, data_types, datas, true, false);
   RUN_KERNEL(node_def, HOST, KERNEL_STATUS_PARAM_INVALID);
 }
