69040cbbe6e02ebfa5a3973313e633bd77128729
张靖 <zhangjing298@huawei.com>
2022-03-10 08:53:36 +0000

    !15204 【swish/silu】解决输入-65504在fp16计算时存在误差 Merge pull request !15204 from 张靖/master

--- ops/built-in/tbe/impl/dynamic/swish.py
+++ ops/built-in/tbe/impl/dynamic/swish.py
@@ -37,6 +37,56 @@ class Constant:
     CONST_FP16_MAX = 65504
 
 
+def swish_normal(data_input, scale, dtype, exp_support):
+    tmp_negative = tbe.vmuls(data_input, tvm.const(-scale, dtype=dtype))
+
+    # avoid data overflow
+    if dtype == "float32" and exp_support:
+        ln_res = math.log(Constant.CONST_FP32_MAX)
+        ln_res = int(ln_res * 10) / 10
+    else:
+        ln_res = math.log(Constant.CONST_FP16_MAX)
+        ln_res = int(ln_res * 1000) / 1000
+    tmp_negative = tbe.vmins(tmp_negative, ln_res)
+
+    if dtype == "float32" and not exp_support:
+        tmp_negative = tbe.cast_to(tmp_negative, "float16")
+    if dtype == "float16" and exp_support:
+        tmp_negative = tbe.cast_to(tmp_negative, "float32")
+    tmp_exp = tbe.vexp(tmp_negative)
+    if not exp_support:
+        tmp_exp = tbe.cast_to(tmp_exp, "float32")
+    tmp_sum = tbe.vadds(tmp_exp, tvm.const(1.0, dtype="float32"))
+    if tmp_sum.dtype == "float16":
+        tmp_sum = tbe.cast_to(tmp_sum, "float32")
+    if dtype == "float16":
+        data_input = tbe.cast_to(data_input, "float32")
+        res = tbe.vdiv(data_input, tmp_sum)
+        return tbe.cast_to(res, dtype)
+    else:
+        return tbe.vdiv(data_input, tmp_sum)
+
+
+def swish_overflow(data_input, scale, dtype):
+    data_input = tbe.cast_to(data_input, "float32")
+    scale_input = tbe.vmuls(data_input, tvm.const(scale, dtype="float32"))
+    abs_scale_input = tbe.vabs(scale_input)
+    minus_abs = tbe.vmuls(abs_scale_input, tvm.const(-1.0, dtype="float32"))
+    sign_diff = tbe.vadd(scale_input, minus_abs)
+    half_sign_diff = tbe.vmuls(sign_diff, tvm.const(0.5, dtype="float32"))
+
+    exp_top = tbe.vexp(half_sign_diff)
+    exp_bottom = tbe.vexp(minus_abs)
+    one_plus_exp = tbe.vadds(exp_bottom, tvm.const(1.0, dtype="float32"))
+
+    input_mul_exp = tbe.vmul(data_input, exp_top)
+    res = tbe.vdiv(input_mul_exp, one_plus_exp)
+    if dtype == "float16":
+        return tbe.cast_to(res, "float16")
+    else:
+        return res
+
+
 # 'pylint: disable=unused-argument,too-many-locals,invalid-name
 @register_operator_compute("Swish", op_mode="dynamic", support_fusion=True)
 def swish_compute(data_input, y, scale, kernel_name="swish"):
@@ -57,32 +107,12 @@ def swish_compute(data_input, y, scale, kernel_name="swish"):
     -------
     output tensor
     """
-    dtype = data_input.dtype
+    dtype = data_input.dtype.lower()
     exp_support = tbe_platform.api_check_support("te.lang.cce.vexp", "float32")
-    tmp_negative = tbe.vmuls(data_input, tvm.const(-scale, dtype=dtype))
-    # avoid data overflow
     if tbe_platform.get_soc_spec("SOC_VERSION") == "Ascend910":
-        if dtype == "float32" and exp_support:
-            ln_res = math.log(Constant.CONST_FP32_MAX)
-            ln_res = int(ln_res * 10) / 10
-        else:
-            ln_res = math.log(Constant.CONST_FP16_MAX)
-            ln_res = int(ln_res * 1000) / 1000
-        tmp_negative = tbe.vmins(tmp_negative, ln_res)
-
-    if dtype == "float32" and not exp_support:
-        tmp_negative = tbe.cast_to(tmp_negative, "float16")
-    elif dtype == "float16" and exp_support:
-        tmp_negative = tbe.cast_to(tmp_negative, "float32")
-    tmp_exp = tbe.vexp(tmp_negative)
-    if not exp_support:
-        tmp_exp = tbe.cast_to(tmp_exp, "float32")
-    tmp_sum = tbe.vadds(tmp_exp, tvm.const(1, dtype="float32"))
-    if dtype == "float16":
-        data_input_fp32 = tbe.cast_to(data_input, "float32")
-        res_fp32 = tbe.vdiv(data_input_fp32, tmp_sum)
-        return tbe.cast_to(res_fp32, "float16")
-    return tbe.vdiv(data_input, tmp_sum)
+        return swish_overflow(data_input, scale, dtype)
+    else:
+        return swish_normal(data_input, scale, dtype, exp_support)
 
 
 @register_operator("Swish")
