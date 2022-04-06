ea98875c94c7967b740b11b259f32498231aa1c6
简轶 <jianyi3@hisilicon.com>
2022-04-06 09:47:23 +0000

    !16079 修复conv_transpose算子插件 Merge pull request !16079 from 简轶/master

--- ops/built-in/tests/ut/plugin_test/onnx/test_conv_transpose_onnx_gen.py
+++ ops/built-in/tests/ut/plugin_test/onnx/test_conv_transpose_onnx_gen.py
@@ -490,6 +490,31 @@ def make_conv_dims_3():
     onnx.save(model, "./test_conv_dims_3.onnx")
     onnx.checker.check_model(model)
 
+def make_conv_transpose_1_strides():
+    """v11"""
+    x = helper.make_tensor_value_info('x', TensorProto.FLOAT, [1, 1, 3, 3])
+    w = helper.make_tensor_value_info('w', TensorProto.FLOAT, [1, 2, 3, 3])
+    y = helper.make_tensor_value_info('y', TensorProto.FLOAT, [1, 2, 3, 5])
+    node_def = helper.make_node(
+        'ConvTranspose',
+        inputs=['x', 'w'],
+        outputs=['y'],
+        strides=[2, ],
+        kernel_shape=[3, 3],
+    )
+
+    graph = helper.make_graph(
+        [node_def],
+        'ConvTranspose',
+        [x, w],
+        [y],
+    )
+
+    model = helper.make_model(graph, producer_name="onnx-parser_test")
+    model.opset_import[0].version = 11
+    onnx.save(model, "./test_conv_transpose_case_1_strides.onnx")
+    onnx.checker.check_model(model)
+
 
 if __name__ == '__main__':
     make_conv_transpose_v8()
@@ -510,3 +535,4 @@ if __name__ == '__main__':
     make_conv3d_transpose_output_padding()
     make_conv3d_fail()
     make_conv_dims_3()
+    make_conv_transpose_1_strides()
--- ops/built-in/tests/ut/plugin_test/onnx/test_conv_transpose_onnx_plugin.cpp
+++ ops/built-in/tests/ut/plugin_test/onnx/test_conv_transpose_onnx_plugin.cpp
@@ -356,3 +356,22 @@ TEST_F(conv_transpose_onnx_plugin_test, conv_transpose_onnx_plugin_test_case_18)
   std::vector<ge::GNode> nodes = graph.GetAllNodes();
   EXPECT_EQ(nodes.size(), 7);
 }
+
+TEST_F(conv_transpose_onnx_plugin_test, conv_transpose_onnx_plugin_test_case_19) {
+  CleanGlobal();
+  ge::Graph graph;
+
+  std::cout << __FILE__ << std::endl;
+  std::string caseDir = __FILE__;
+  std::size_t idx = caseDir.find_last_of("/");
+  caseDir = caseDir.substr(0, idx);
+  std::string modelFile = caseDir + "/test_conv_transpose_case_1_strides.onnx";
+  std::map<ge::AscendString, ge::AscendString> parser_params;
+
+  auto status = aclgrphParseONNX(modelFile.c_str(), parser_params, graph);
+
+  EXPECT_EQ(status, ge::GRAPH_SUCCESS);
+  // check op count, some op need check op attr, op input count.
+  std::vector<ge::GNode> nodes = graph.GetAllNodes();
+  EXPECT_EQ(nodes.size(), 4);
+}
