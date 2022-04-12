1edddafaa1e3acb8091bddc1f9607919987e33fd
王婷 <wangting104@huawei.com>
2022-04-12 12:16:19 +0000

    !16246 修改AArgMaxV2FusionPass日志 Merge pull request !16246 from 王婷/master

--- ops/built-in/fusion_pass/graph_fusion/ai_core/a_arg_max_v2_fusion_pass.cc
+++ ops/built-in/fusion_pass/graph_fusion/ai_core/a_arg_max_v2_fusion_pass.cc
@@ -150,11 +150,11 @@ Status AArgMaxV2FusionPass::Fusion(ge::ComputeGraph& graph, Mapping& mapping, ve
     }
     FUSION_PASS_CHECK((aixs_const_vala > static_cast<int64_t>(x_dima) - 1 || aixs_const_vala < 0),
                        OP_LOGW(FUSED_OP_TYPE.c_str(),
-                               "Node:%s's dimension is invalid.shape dim is [%ld], axis is [%ld]",
+                               "dimension is invalid.shape dim is [%ld], axis is [%ld]",
                                x_dima, aixs_const_vala), return NOT_CHANGED);
     FUSION_PASS_CHECK((aixs_const_valb > static_cast<int64_t>(x_dimb) - 1 || aixs_const_valb < 0),
                        OP_LOGW(FUSED_OP_TYPE.c_str(),
-                               "Node:%s's dimension is invalid.shape dim is [%ld], axis is [%ld]",
+                               "dimension is invalid.shape dim is [%ld], axis is [%ld]",
                                x_dimb, aixs_const_valb), return NOT_CHANGED);
     if (x_input_shapea.at(aixs_const_vala) != -1 || x_input_shapeb.at(aixs_const_valb) != -1) {
       OP_LOGD(FUSED_OP_TYPE.c_str(), "input[0]'s shape is static shape.");
