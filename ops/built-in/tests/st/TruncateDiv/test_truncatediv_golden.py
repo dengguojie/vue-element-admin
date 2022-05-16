e1368985ec8100f199a47ff030e4abea4befe34f
Wwangdecong <wangdecong@hisilicon.com>
2022-05-16 08:45:58 +0000

    !16882 算子支持- 2 shape Merge pull request !16882 from Wwangdecong/tx

---
+++ ops/built-in/tests/st/TruncateDiv/test_truncatediv_golden.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''
+Special golden data generation function for convolution pattern
+'''
+# Third-Party Packages
+import tensorflow as tf
+
+
+def calc_expect_func(input_x, input_y, output_x):
+
+    x = input_x.get('value')
+    y = input_y.get('value')
+
+    tensor_x = tf.placeholder(x.dtype, shape=x.shape)
+    tensor_y = tf.placeholder(y.dtype, shape=y.shape)
+
+    out = tf.truncatediv(tensor_x, tensor_y)
+
+    with tf.Session() as sess:
+        res = sess.run(out, feed_dict={tensor_x: x, tensor_y: y})
+    return [res]
---
+++ ops/built-in/tests/st/Xlogy/test_xlogy_golden.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''
+Special golden data generation function for convolution pattern
+'''
+# Third-Party Packages
+import tensorflow as tf
+
+
+def calc_expect_func(x1, x2, y):
+
+    x1 = x1.get('value')
+    x2 = x2.get('value')
+
+    tensor_x1 = tf.placeholder(x1.dtype, shape=x1.shape)
+    tensor_x2 = tf.placeholder(x2.dtype, shape=x2.shape)
+
+    out = tf.math.xlogy(tensor_x1, tensor_x2)
+
+    with tf.Session() as sess:
+        res = sess.run(out, feed_dict={tensor_x1: x1, tensor_x2: x2})
+    return [res]
