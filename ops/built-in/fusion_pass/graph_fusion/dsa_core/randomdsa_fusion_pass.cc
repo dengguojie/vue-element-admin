94b2ede4ab4d03b7546a227b7c200e0667ed5172
周伟 <zhouwei188@hisilicon.com>
2022-05-21 06:16:19 +0000

    !16978 增加DSA开关控制算子融合 Merge pull request !16978 from 周伟/dsa_part2

--- ops/built-in/fusion_pass/graph_fusion/dsa_core/randomdsa_fusion_pass.cc
+++ ops/built-in/fusion_pass/graph_fusion/dsa_core/randomdsa_fusion_pass.cc
@@ -29,6 +29,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <cstdlib>
 
 #include "graph/utils/op_desc_utils.h"
 #include "graph/utils/graph_utils.h"
@@ -49,6 +50,14 @@ static const std::string PATTERN_FUSEDNODE_DSAGEN = "DSAGenBitMask";
 // define op RandomUniformInt------>DSARandomUniform
 static const std::string PATTERN_FUSEDNODE_UNIFORM = "RandomUniformInt";
 static const std::string PATTERN_FUSEDNODE_DSAUNIFORM = "DSARandomUniform";
+
+// define op TruncatedNormal------------------->DSARandomTruncatedNormal
+static const std::string PATTERN_FUSEDNODE_TRUNCATE = "TruncatedNormal";
+static const std::string PATTERN_FUSEDNODE_DSATRUNCATE = "DSARandomTruncatedNormal";
+
+// define op RandomStandardNormal--------------->DSARandomNormal
+static const std::string PATTERN_FUSEDNODE_STANDARDNORM = "RandomStandardNormal";
+static const std::string PATTERN_FUSEDNODE_DSANORMAL = "DSARandomNormal";
 
 // define op reduceprod
 static const string kPatternDSA = "RandomDSA";
@@ -74,8 +83,27 @@ vector<FusionPattern*> RandomDsaFusionPass::DefinePatterns() {
                     return patterns);
   pattern2->AddOpDesc(PATTERN_FUSEDNODE_UNIFORM, {"RandomUniformInt"}).SetOutput(PATTERN_FUSEDNODE_UNIFORM);
 
+  FusionPattern* pattern3 = new (std::nothrow) FusionPattern("RandomDsaFusionPass3");
+  FUSION_PASS_CHECK(pattern3 == nullptr, VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "new a pattern object failed."),
+                    return patterns);
+  pattern3->AddOpDesc(PATTERN_FUSEDNODE_TRUNCATE, {"TruncatedNormal"}).SetOutput(PATTERN_FUSEDNODE_TRUNCATE);
+
+  FusionPattern* pattern4 = new (std::nothrow) FusionPattern("RandomDsaFusionPass4");
+  FUSION_PASS_CHECK(pattern4 == nullptr, VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "new a pattern object failed."),
+                    return patterns);
+  pattern4->AddOpDesc(PATTERN_FUSEDNODE_STANDARDNORM, {"RandomStandardNormal"})
+      .SetOutput(PATTERN_FUSEDNODE_STANDARDNORM);
   patterns.push_back(pattern1);
   patterns.push_back(pattern2);
+  // get env to decide whether open
+  const char* env_dsa = getenv("DSA_ENABLE");
+  if (env_dsa != nullptr){
+    std::string s(env_dsa);
+    if (s == "True") {
+      patterns.push_back(pattern3);
+      patterns.push_back(pattern4);
+    }
+  }
   return patterns;
 }
 
@@ -149,7 +177,7 @@ Status RandomDsaFusionPass::CreateConstOperator(ge::ComputeGraph& graph, ge::Nod
     return GRAPH_FAILED;
   }
   uint64_t seed_tmp = seed0;
-  seed_tmp=(seed_tmp<<MOVE_32_NUM) | seed1;
+  seed_tmp = (seed_tmp << MOVE_32_NUM) | seed1;
   const uint64_t seed = seed_tmp;
   // set seed
   ge::GeTensorDesc tmp_desc;
@@ -174,6 +202,42 @@ Status RandomDsaFusionPass::CreateConstOperator(ge::ComputeGraph& graph, ge::Nod
   return SUCCESS;
 }
 
+Status RandomDsaFusionPass::CreateConstOperatorNormal(ge::ComputeGraph& graph, const float mean, const float stddev,
+                                                      ge::NodePtr& const_node_mean, ge::NodePtr& const_node_stddev) {
+  // set mean and stddev value
+  ge::GeTensorDesc tmp_desc_mean;
+  ge::GeTensorPtr mean_ptr = nullptr;
+  ge::GeTensorDesc tmp_desc_stddev;
+  ge::GeTensorPtr stddev_ptr = nullptr;
+  std::unique_ptr<float> mean_data_tmp(new (std::nothrow) float(mean));
+  std::unique_ptr<float> stddev_data_tmp(new (std::nothrow) float(stddev));
+  FUSION_PASS_MAKE_SHARED((mean_ptr = std::make_shared<ge::GeTensor>(
+                               tmp_desc_mean, reinterpret_cast<uint8_t*>(mean_data_tmp.get()), sizeof(float))),
+                          mean_ptr = nullptr;
+                          return PARAM_INVALID);
+  FUSION_PASS_MAKE_SHARED((stddev_ptr = std::make_shared<ge::GeTensor>(
+                               tmp_desc_stddev, reinterpret_cast<uint8_t*>(stddev_data_tmp.get()), sizeof(float))),
+                          stddev_ptr = nullptr;
+                          return PARAM_INVALID);
+  Status ret_mean = mean_ptr->SetData(reinterpret_cast<uint8_t*>(mean_data_tmp.get()), sizeof(float));
+  Status ret_stddev = stddev_ptr->SetData(reinterpret_cast<uint8_t*>(stddev_data_tmp.get()), sizeof(float));
+  if (ret_mean != SUCCESS || ret_stddev != SUCCESS) {
+    OP_LOGW("set mean/stddev data failed.");
+    return FAILED;
+  }
+  mean_ptr->MutableTensorDesc().SetDataType(ge::DT_FLOAT);
+  stddev_ptr->MutableTensorDesc().SetDataType(ge::DT_FLOAT);
+  ge::OpDescPtr const_opdesc_mean = ge::OpDescUtils::CreateConstOp(mean_ptr);
+  FUSION_PASS_CHECK(const_opdesc_mean == nullptr, VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(),"new a const object failed."), return FAILED);
+  ge::OpDescPtr const_opdesc_stddev = ge::OpDescUtils::CreateConstOp(stddev_ptr);
+  FUSION_PASS_CHECK(const_opdesc_stddev == nullptr, VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(),"new a const object failed."), return FAILED);
+  const_opdesc_mean->SetName("constop_mean");
+  const_opdesc_stddev->SetName("constop_stddev");
+  const_node_mean = graph.AddNode(const_opdesc_mean);
+  const_node_stddev = graph.AddNode(const_opdesc_stddev);
+  return SUCCESS;
+}
+
 Status RandomDsaFusionPass::Fusion(ge::ComputeGraph& graph, Mapping& mapping, vector<ge::NodePtr>& fusionNodes) {
   OP_LOGD("DSA OP Start Fusion");
   PlatformInfo platformInfo;
@@ -189,7 +253,10 @@ Status RandomDsaFusionPass::Fusion(ge::ComputeGraph& graph, Mapping& mapping, ve
   }
   ge::NodePtr fusedNode_Drop = GetNodeFromMapping(PATTERN_FUSEDNODE_DROP, mapping);
   ge::NodePtr fusedNode_Uniform = GetNodeFromMapping(PATTERN_FUSEDNODE_UNIFORM, mapping);
-  if (fusedNode_Drop == nullptr && fusedNode_Uniform == nullptr) {
+  ge::NodePtr fusedNode_normal = GetNodeFromMapping(PATTERN_FUSEDNODE_STANDARDNORM, mapping);
+  ge::NodePtr fusedNode_Truncate = GetNodeFromMapping(PATTERN_FUSEDNODE_TRUNCATE, mapping);
+  if (fusedNode_Drop == nullptr && fusedNode_Uniform == nullptr && fusedNode_normal == nullptr &&
+      fusedNode_Truncate == nullptr) {
     OP_LOGW(FUSED_OP_TYPE.c_str(), "all related fusedNode randomdsa  is null, fusion failed.");
     return NOT_CHANGED;
   }
@@ -385,6 +452,227 @@ Status RandomDsaFusionPass::Fusion(ge::ComputeGraph& graph, Mapping& mapping, ve
                                                      fusedNode_Uniform->GetName().c_str()),
                       return FAILED);
     fusionNodes.push_back(dsauniform_node);
+    return SUCCESS;
+  }
+  if (fusedNode_Truncate != nullptr) {
+    ge::OpDescPtr fusedDesc_Truncate = fusedNode_Truncate->GetOpDesc();
+
+    // Get Param Truncate Input Info
+    ge::GeTensorDesc truncate_input_desc0 = fusedNode_Truncate->GetOpDesc()->GetInputDesc(0);
+    ge::GeTensorDesc truncate_output_desc0 = fusedNode_Truncate->GetOpDesc()->GetOutputDesc(0);
+    DataType datatype_truncate = truncate_input_desc0.GetDataType();
+    DataType datatype_truncate_out = truncate_output_desc0.GetDataType();
+
+    // define support dtype
+    std::set<DataType> supported_dtypes = {ge::DT_INT64, ge::DT_INT32};
+    if (supported_dtypes.count(datatype_truncate) == 0 || datatype_truncate_out == ge::DT_DOUBLE) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "input dtype not supported, fusion failed.");
+      return NOT_CHANGED;
+    }
+    // define const mean/stddev value to constop
+    const float mean = 0;
+    const float stddev = 1;
+    ge::NodePtr const_node = nullptr;
+    ge::NodePtr const_node_mean = nullptr;
+    ge::NodePtr const_node_stddev = nullptr;
+    // create const op seed
+    CreateConstOperator(graph, fusedNode_Truncate, const_node);
+    CreateConstOperatorNormal(graph, mean, stddev, const_node_mean, const_node_stddev);
+
+    // Get Param Truncate Output Info
+    ge::GeTensorDesc output_desc0 = fusedNode_Truncate->GetOpDesc()->GetOutputDesc(0);
+
+    // set seed desc info
+    ge::GeTensorDesc truncate_seed_desc;
+    truncate_seed_desc.SetDataType(ge::DT_UINT64);
+    truncate_seed_desc.SetOriginDataType(ge::DT_UINT64);
+
+    // set mean desc info
+    ge::GeTensorDesc truncate_mean_desc;
+    truncate_mean_desc.SetDataType(ge::DT_FLOAT);
+    truncate_mean_desc.SetOriginDataType(ge::DT_FLOAT);
+
+    // set seed desc info
+    ge::GeTensorDesc truncate_stddev_desc;
+    truncate_stddev_desc.SetDataType(ge::DT_FLOAT);
+    truncate_stddev_desc.SetOriginDataType(ge::DT_FLOAT);
+    ge::OpDescPtr dsatruncateop = nullptr;
+    FUSION_PASS_MAKE_SHARED((dsatruncateop = std::make_shared<ge::OpDesc>(
+                                 fusedDesc_Truncate->GetName() + "/" + "DSAPARAMTRUNCATE", "DSARandomTruncatedNormal")),
+                            return INTERNAL_ERROR);
+    dsatruncateop->AddInputDesc("count", truncate_input_desc0);
+    dsatruncateop->AddInputDesc("seed", truncate_seed_desc);
+    dsatruncateop->AddInputDesc("mean", truncate_mean_desc);
+    dsatruncateop->AddInputDesc("stdev", truncate_stddev_desc);
+    dsatruncateop->AddOutputDesc("out", output_desc0);
+    ge::NodePtr dsatruncate_node = graph.AddNode(dsatruncateop);
+    // add truncate in edge to dsatruncate
+    FUSION_PASS_CHECK(
+        SUCCESS != ge::GraphUtils::AddEdge(fusedNode_Truncate->GetInDataAnchor(0)->GetPeerOutAnchor(),
+                                           dsatruncate_node->GetInDataAnchor(0)),
+        VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                fusedNode_Truncate->GetName().c_str(), 0, dsatruncate_node->GetName().c_str(), 0),
+        return FAILED);
+
+    if (dsatruncate_node->AddLinkFrom(1, const_node) != SUCCESS) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                                     const_node->GetName().c_str(), 1, dsatruncate_node->GetName().c_str(), 1);
+      return FAILED;
+    }
+
+    if (dsatruncate_node->AddLinkFrom(MEAN_NODE_EDGE, const_node_mean) != SUCCESS) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                                     const_node_mean->GetName().c_str(), 1, dsatruncate_node->GetName().c_str(), 1);
+      return FAILED;
+    }
+
+    if (dsatruncate_node->AddLinkFrom(STDDEV_NODE_EDGE, const_node_stddev) != SUCCESS) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                                     const_node_stddev->GetName().c_str(), 1, dsatruncate_node->GetName().c_str(), 1);
+      return FAILED;
+    }
+
+    for (auto in_ctl_anchor : fusedNode_Truncate->GetInControlAnchor()->GetPeerOutControlAnchors()) {
+      (void)ge::GraphUtils::AddEdge(in_ctl_anchor, dsatruncate_node->GetInControlAnchor());
+    }
+
+    // unlink dropout
+    if (fusedNode_Truncate->GetInControlAnchor() != nullptr) {
+      fusedNode_Truncate->GetInControlAnchor()->UnlinkAll();
+    }
+    for (auto& inAnchor : fusedNode_Truncate->GetAllInDataAnchors()) {
+      if (inAnchor != nullptr) {
+        inAnchor->UnlinkAll();
+      }
+    }
+    for (auto& out_anchor : fusedNode_Truncate->GetOutDataAnchor(0)->GetPeerInDataAnchors()) {
+      (void)ge::GraphUtils::RemoveEdge(fusedNode_Truncate->GetOutDataAnchor(0), out_anchor);
+      (void)ge::GraphUtils::AddEdge(dsatruncate_node->GetOutDataAnchor(0), out_anchor);
+    }
+    // flattern shape to count by insert reduceprod
+    FlatternShape(graph, dsatruncate_node, kCountIndex);
+    // set count dtype
+    dsatruncate_node->GetOpDesc()->MutableInputDesc(0)->SetDataType(ge::DT_INT64);
+    dsatruncate_node->GetOpDesc()->MutableInputDesc(0)->SetOriginDataType(ge::DT_INT64);
+
+    // remove dropout node
+    FUSION_PASS_CHECK(
+        ge::GRAPH_SUCCESS != graph.RemoveNode(fusedNode_Truncate),
+        VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "remove fusedNode node[%s] failed", fusedNode_Truncate->GetName().c_str()),
+        return FAILED);
+    fusionNodes.push_back(dsatruncate_node);
+    return SUCCESS;
+  }
+
+  if (fusedNode_normal != nullptr) {
+    ge::OpDescPtr fusedDesc_Normal = fusedNode_normal->GetOpDesc();
+
+    // Get Param Truncate Input Info
+    ge::GeTensorDesc normal_input_desc0 = fusedNode_normal->GetOpDesc()->GetInputDesc(0);
+    ge::GeTensorDesc normal_output_desc0 = fusedNode_normal->GetOpDesc()->GetOutputDesc(0);
+    DataType datatype_normal = normal_input_desc0.GetDataType();
+    DataType datatype_normal_output = normal_output_desc0.GetDataType();
+
+    // define support dtype
+    std::set<DataType> supported_dtypes = {ge::DT_INT64, ge::DT_INT32};
+    if (supported_dtypes.count(datatype_normal) == 0 || datatype_normal_output == ge::DT_DOUBLE) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "input dtype not supported, fusion failed.");
+      return NOT_CHANGED;
+    }
+
+    // define const mean/stddev value to constop
+    const float mean = 0;
+    const float stddev = 1;
+    ge::NodePtr const_node = nullptr;
+    ge::NodePtr const_node_mean = nullptr;
+    ge::NodePtr const_node_stddev = nullptr;
+    // create const op seed
+    CreateConstOperator(graph, fusedNode_normal, const_node);
+    CreateConstOperatorNormal(graph, mean, stddev, const_node_mean, const_node_stddev);
+
+    // Get Param Truncate Output Info
+    ge::GeTensorDesc output_desc0 = fusedNode_normal->GetOpDesc()->GetOutputDesc(0);
+
+    // set seed desc info
+    ge::GeTensorDesc normal_seed_desc;
+    normal_seed_desc.SetDataType(ge::DT_UINT64);
+    normal_seed_desc.SetOriginDataType(ge::DT_UINT64);
+
+    // set mean desc info
+    ge::GeTensorDesc normal_mean_desc;
+    normal_mean_desc.SetDataType(ge::DT_FLOAT);
+    normal_mean_desc.SetOriginDataType(ge::DT_FLOAT);
+
+    // set seed desc info
+    ge::GeTensorDesc normal_stddev_desc;
+    normal_stddev_desc.SetDataType(ge::DT_FLOAT);
+    normal_stddev_desc.SetOriginDataType(ge::DT_FLOAT);
+    ge::OpDescPtr dsanormalop = nullptr;
+    FUSION_PASS_MAKE_SHARED((dsanormalop = std::make_shared<ge::OpDesc>(
+                                 fusedDesc_Normal->GetName() + "/" + "DSARANDOMNORMAL", "DSARandomNormal")),
+                            return INTERNAL_ERROR);
+    dsanormalop->AddInputDesc("count", normal_input_desc0);
+    dsanormalop->AddInputDesc("seed", normal_seed_desc);
+    dsanormalop->AddInputDesc("mean", normal_mean_desc);
+    dsanormalop->AddInputDesc("stdev", normal_stddev_desc);
+    dsanormalop->AddOutputDesc("out", output_desc0);
+    ge::NodePtr dsanormal_node = graph.AddNode(dsanormalop);
+    // add truncate in edge to dsatruncate
+    FUSION_PASS_CHECK(
+        SUCCESS != ge::GraphUtils::AddEdge(fusedNode_normal->GetInDataAnchor(0)->GetPeerOutAnchor(),
+                                           dsanormal_node->GetInDataAnchor(0)),
+        VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                fusedNode_normal->GetName().c_str(), 0, dsanormal_node->GetName().c_str(), 0),
+        return FAILED);
+
+    if (dsanormal_node->AddLinkFrom(1, const_node) != SUCCESS) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                                     const_node->GetName().c_str(), 1, dsanormal_node->GetName().c_str(), 1);
+      return FAILED;
+    }
+
+    if (dsanormal_node->AddLinkFrom(MEAN_NODE_EDGE, const_node_mean) != SUCCESS) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                                     const_node_mean->GetName().c_str(), 1, dsanormal_node->GetName().c_str(), 2);
+      return FAILED;
+    }
+
+    if (dsanormal_node->AddLinkFrom(STDDEV_NODE_EDGE, const_node_stddev) != SUCCESS) {
+      VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "Add edge from fused node:%s's index[%d] to fusion node:%s's index[%d] failed.",
+                                     const_node_stddev->GetName().c_str(), 1, dsanormal_node->GetName().c_str(), 3);
+      return FAILED;
+    }
+
+    for (auto in_ctl_anchor : fusedNode_normal->GetInControlAnchor()->GetPeerOutControlAnchors()) {
+      (void)ge::GraphUtils::AddEdge(in_ctl_anchor, dsanormal_node->GetInControlAnchor());
+    }
+
+    // unlink dropout
+    if (fusedNode_normal->GetInControlAnchor() != nullptr) {
+      fusedNode_normal->GetInControlAnchor()->UnlinkAll();
+    }
+    for (auto& inAnchor : fusedNode_normal->GetAllInDataAnchors()) {
+      if (inAnchor != nullptr) {
+        inAnchor->UnlinkAll();
+      }
+    }
+    for (auto& out_anchor : fusedNode_normal->GetOutDataAnchor(0)->GetPeerInDataAnchors()) {
+      (void)ge::GraphUtils::RemoveEdge(fusedNode_normal->GetOutDataAnchor(0), out_anchor);
+      (void)ge::GraphUtils::AddEdge(dsanormal_node->GetOutDataAnchor(0), out_anchor);
+    }
+    // flattern shape to count by insert reduceprod
+    FlatternShape(graph, dsanormal_node, kCountIndex);
+
+    // set count dtype
+    dsanormal_node->GetOpDesc()->MutableInputDesc(0)->SetDataType(ge::DT_INT64);
+    dsanormal_node->GetOpDesc()->MutableInputDesc(0)->SetOriginDataType(ge::DT_INT64);
+
+    // remove dropout node
+    FUSION_PASS_CHECK(
+        ge::GRAPH_SUCCESS != graph.RemoveNode(fusedNode_normal),
+        VECTOR_FUSION_INNER_ERR_REPORT(FUSED_OP_TYPE.c_str(), "remove fusedNode node[%s] failed", fusedNode_normal->GetName().c_str()),
+        return FAILED);
+    fusionNodes.push_back(dsanormal_node);
     return SUCCESS;
   }
   return NOT_CHANGED;
--- ops/built-in/tests/ut/graph_fusion/ai_core/randomdsa_fusion_test.cpp
+++ ops/built-in/tests/ut/graph_fusion/ai_core/randomdsa_fusion_test.cpp
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include "gtest/gtest.h"
 #include "graph/compute_graph.h"
 #include "graph/graph.h"
@@ -117,3 +118,73 @@ TEST_F(randomdsa_fusion_test, randomdsa_fusion_test_2) {
     EXPECT_EQ(finddsa, true);
 }
 
+TEST_F(randomdsa_fusion_test, randomdsa_fusion_test_3) {
+    ge::Graph graph("randomdsa_fusion_test_3");
+    fe::PlatformInfo platform_info;
+    fe::OptionalInfo opti_info;
+    opti_info.soc_version = "Ascend920A";
+    fe::PlatformInfoManager::Instance().platform_info_map_["Ascend920A"] = platform_info;
+    fe::PlatformInfoManager::Instance().SetOptionalCompilationInfo(opti_info);
+    setenv("DSA_ENABLE","True",1);
+
+    // DropOutGenMask
+    auto xData = op::Data("xData");
+    std::vector<int64_t> dims_x{1024};
+    ge::Shape shape_x(dims_x);
+    ge::TensorDesc tensorDescX(shape_x, FORMAT_ND,  DT_INT64);
+    xData.update_input_desc_x(tensorDescX);
+    xData.update_output_desc_y(tensorDescX);
+
+    auto randomstandard = op::RandomStandardNormal("RandomStandardNormal1");
+    randomstandard.set_input_shape(xData);
+    std::vector<Operator> inputs{xData};
+    std::vector<Operator> outputs{randomstandard};
+
+    graph.SetInputs(inputs).SetOutputs(outputs);
+    ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+    //fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+    fe::FusionPassTestUtils::RunGraphFusionPass("RandomDsaFusionPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
+
+    bool finddsa = false;
+    for (auto node: compute_graph_ptr->GetAllNodes()) {
+        if (node->GetType() == "DSARandomNormal") {
+            finddsa = true;
+        }
+    }
+    EXPECT_EQ(finddsa, true);
+}
+
+TEST_F(randomdsa_fusion_test, randomdsa_fusion_test_4) {
+    ge::Graph graph("randomdsa_fusion_test_4");
+    fe::PlatformInfo platform_info;
+    fe::OptionalInfo opti_info;
+    opti_info.soc_version = "Ascend920A";
+    fe::PlatformInfoManager::Instance().platform_info_map_["Ascend920A"] = platform_info;
+    fe::PlatformInfoManager::Instance().SetOptionalCompilationInfo(opti_info);
+    setenv("DSA_ENABLE","True",1);
+
+    // DropOutGenMask
+    auto xData = op::Data("xData");
+    std::vector<int64_t> dims_x{1024};
+    ge::Shape shape_x(dims_x);
+    ge::TensorDesc tensorDescX(shape_x, FORMAT_ND,  DT_INT32);
+    xData.update_input_desc_x(tensorDescX);
+    xData.update_output_desc_y(tensorDescX);
+
+    auto truncatenormal = op::TruncatedNormal("TruncatedNormal1");
+    truncatenormal.set_input_shape(xData);
+    std::vector<Operator> inputs{xData};
+    std::vector<Operator> outputs{truncatenormal};
+
+    graph.SetInputs(inputs).SetOutputs(outputs);
+    ge::ComputeGraphPtr compute_graph_ptr = ge::GraphUtils::GetComputeGraph(graph);
+    //fe::FusionPassTestUtils::InferShapeAndType(compute_graph_ptr);
+    fe::FusionPassTestUtils::RunGraphFusionPass("RandomDsaFusionPass", fe::BUILT_IN_GRAPH_PASS, *compute_graph_ptr);
+    bool finddsa = false;
+    for (auto node: compute_graph_ptr->GetAllNodes()) {
+        if (node->GetType() == "DSARandomTruncatedNormal") {
+            finddsa = true;
+        }
+    }
+    EXPECT_EQ(finddsa, true);
+}
