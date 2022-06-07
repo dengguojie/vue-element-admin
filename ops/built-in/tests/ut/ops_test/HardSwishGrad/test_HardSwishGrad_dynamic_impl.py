28592e7f02673f95a39e386c71694e9680b95dd3
张继昌 <zhangjichang1@hisilicon.com>
2022-06-07 12:14:23 +0000

    !17672 HardSwish、HardSwishGrad算子支持二进制 Merge pull request !17672 from 张继昌/master

--- ops/built-in/tests/ut/ops_test/HardSwishGrad/test_HardSwishGrad_dynamic_impl.py
+++ ops/built-in/tests/ut/ops_test/HardSwishGrad/test_HardSwishGrad_dynamic_impl.py
@@ -44,9 +44,16 @@ case2 = {"params": [{"shape": (-1, 8, 375), "dtype": "float32", "format": "ND",
          "expect": "success",
          "support_expect": True}
 
+# 'pylint: disable=unused-argument
+def test_import_lib(test_arg):
+    import sys
+    import importlib
+    importlib.reload(sys.modules.get("impl.dynamic.binary_query_register"))
+
 
 ut_case.add_case(["Ascend910A"], case1)
 ut_case.add_case(["Ascend910A"], case2)
+ut_case.add_cust_test_func(test_func=test_import_lib)
 
 
 if __name__ == "__main__":
