17388818e301d68c26c3af5fba5039452b18c26d
ckirchhoff <chenxiang143@huawei.com>
2022-06-20 04:42:08 +0000

    !18170 YoloxBoundingBoxDecode BUG修复1 Merge pull request !18170 from ckirchhoff/bbox_fix

--- ops/built-in/tests/ut/ops_test/YoloxBoundingBoxDecode/test_yolox_bbox_decode_dynamic_impl.py
+++ ops/built-in/tests/ut/ops_test/YoloxBoundingBoxDecode/test_yolox_bbox_decode_dynamic_impl.py
@@ -80,13 +80,13 @@ case5 = {"params": [{"shape": (8400, 4), "dtype": "float32", "format": "ND", "or
 
 
 
-ut_case.add_case(["Ascend910","Ascend310","Ascend710"], case1)
-ut_case.add_case(["Ascend910","Ascend310","Ascend710"], case2)
-ut_case.add_case(["Ascend910","Ascend310","Ascend710"], case3)
-ut_case.add_case(["Ascend910","Ascend310","Ascend710"], case4)
-ut_case.add_case(["Ascend910","Ascend310","Ascend710"], case5)
+ut_case.add_case(["Ascend910","Ascend310","Ascend310P3"], case1)
+ut_case.add_case(["Ascend910","Ascend310","Ascend310P3"], case2)
+ut_case.add_case(["Ascend910","Ascend310","Ascend310P3"], case3)
+ut_case.add_case(["Ascend910","Ascend310","Ascend310P3"], case4)
+ut_case.add_case(["Ascend910","Ascend310","Ascend310P3"], case5)
 
 
 if __name__ == '__main__':
-    ut_case.run(["Ascend910","Ascend310","Ascend710"])
+    ut_case.run(["Ascend910","Ascend310","Ascend310P3"])
     exit(0)
