820308a7859d983a873b01843d618fe71a276080
张超凡 <zhangchaofan8@huawei.com>
2022-06-22 12:15:47 +0000

    !18101 MaxPool3DWithArgMax 适配1981 Merge pull request !18101 from 张超凡/mp3dwa_0615

--- ops/built-in/tests/ut/ops_test/MaxPool3DWithArgmax/test_MaxPool3DWithArgmax_impl.py
+++ ops/built-in/tests/ut/ops_test/MaxPool3DWithArgmax/test_MaxPool3DWithArgmax_impl.py
@@ -16,6 +16,9 @@ MaxPool3DWithArgmax ut case
 import numpy as np
 from op_test_frame.common import precision_info
 from op_test_frame.ut import OpUT
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 ut_case = OpUT("MaxPool3DWithArgmax", "impl.max_pool3d_with_argmax", "max_pool3d_with_argmax")
 
 case1 = {"params": [{"shape": (1, 10, 1, 6, 6, 16), "dtype": "float16"},
@@ -26,11 +29,26 @@ case3 = {"params": [{"shape": (10, 225, 1, 103, 176, 16), "dtype": "float16"},
                     {}, {}, [1, 1, 1, 1, 1], [1, 1, 5, 3, 2]]}
 case4 = {"params": [{"shape": (4, 25, 1, 70, 30, 16), "dtype": "float16"},
                     {}, {}, [1, 1, 2, 2, 2], [1, 1, 5, 4, 2]]}
+case5 = {"params": [{"shape": (1, 3, 2, 4, 4, 16), "dtype": "float16"},
+                    {}, {}, [1, 1, 2, 2, 2], [1, 1, 1, 2, 2]]}  # WholeKernel
+case6 = {"params": [{"shape": (1, 2, 3, 189, 21, 16), "dtype": "float16"},
+                    {}, {}, [1, 1, 2, 3, 3], [1, 1, 1, 2, 2]]}  # CUT_H
+case7 = {"params": [{"shape": (1, 3, 2, 7, 419, 16), "dtype": "float16"},
+                    {}, {}, [1, 1, 2, 5, 5], [1, 1, 1, 2, 2]]}  # CUT_H_AND_W
 
-ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910"], case1)
-ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910"], case2)
-ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910"], case3)
-ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910"], case4)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case1)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case2)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case3)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case4)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case5)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case6)
+ut_case.add_case(["Ascend310", "Ascend310P3", "Ascend910A"], case7)
+
+vals = {("tik.load3dv1", ): False}
+def side_effects(*args):
+    return vals[args]
+with patch("impl.util.platform_adapter.tbe_platform.api_check_support", MagicMock(side_effect=side_effects)):
+    ut_case.run("Ascend910A")
 
 if __name__ == '__main__':
     ut_case.run()
